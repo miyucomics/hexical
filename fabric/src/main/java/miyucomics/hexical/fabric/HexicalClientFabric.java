0ff13b86d0fc3e5bedb4675ffe4f4b3188e3d522
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-02-13 17:03:28 -0500

    Clean up

--- fabric/src/main/java/miyucomics/hexical/fabric/HexicalClientFabric.java
+++ fabric/src/main/java/miyucomics/hexical/fabric/HexicalClientFabric.java
@@ -3,9 +3,6 @@ package miyucomics.hexical.fabric;
 import net.fabricmc.api.ClientModInitializer;
 import miyucomics.hexical.HexicalClient;
 
-/**
- * Fabric client loading entrypoint.
- */
 public class HexicalClientFabric implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
