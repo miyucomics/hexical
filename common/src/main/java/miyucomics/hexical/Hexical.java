8f078fce46e9e5d0d8579780435213dd0aaaa19e
Miyu Nguyen <nbriannyc@gmail.com>
2024-02-13 15:25:01 -0500

    Comfort features

--- common/src/main/java/miyucomics/hexical/Hexical.java
+++ common/src/main/java/miyucomics/hexical/Hexical.java
@@ -7,30 +7,19 @@ import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * This is effectively the loading entrypoint for most of your code, at least
- * if you are using Architectury as intended.
- */
 public class Hexical {
-    public static final String MOD_ID = "hexical";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final String MOD_ID = "hexical";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
+	public static void init() {
+		HexicalAbstractions.initPlatformSpecific();
+		HexicalItemRegistry.init();
+		HexicalIotaTypeRegistry.init();
+		HexicalPatternRegistry.init();
+		LOGGER.info(HexicalAbstractions.getConfigDirectory().toAbsolutePath().normalize().toString());
+	}
 
-    public static void init() {
-        LOGGER.info("Hexical says hello!");
-
-        HexicalAbstractions.initPlatformSpecific();
-        HexicalItemRegistry.init();
-        HexicalIotaTypeRegistry.init();
-        HexicalPatternRegistry.init();
-
-        LOGGER.info(HexicalAbstractions.getConfigDirectory().toAbsolutePath().normalize().toString());
-    }
-
-    /**
-     * Shortcut for identifiers specific to this mod.
-     */
-    public static Identifier id(String string) {
-        return new Identifier(MOD_ID, string);
-    }
+	public static Identifier id(String string) {
+		return new Identifier(MOD_ID, string);
+	}
 }
--- common/src/main/java/miyucomics/hexical/api/config/HexicalConfig.java
+++ common/src/main/java/miyucomics/hexical/api/config/HexicalConfig.java
@@ -2,89 +2,87 @@ package miyucomics.hexical.api.config;
 
 import miyucomics.hexical.Hexical;
 
-import java.util.Collection;
-
 /**
  * Platform-agnostic class for statically accessing current config values.
  * If any of the config types (common, client, server) are not needed in your mod,
  * feel free to remove anything related to them in this class and platform-specific config implementations.
  */
 public class HexicalConfig {
-    private static final CommonConfigAccess dummyCommon = new CommonConfigAccess() {
-    };
-    private static final ClientConfigAccess dummyClient = new ClientConfigAccess() {
-    };
-    private static final ServerConfigAccess dummyServer = new ServerConfigAccess() {
-        @Override
-        public int getCongratsCost() {
-            throw new IllegalStateException("Attempted to access property of Dummy Config Object");
-        }
+	private static final CommonConfigAccess dummyCommon = new CommonConfigAccess() {
+	};
+	private static final ClientConfigAccess dummyClient = new ClientConfigAccess() {
+	};
+	private static final ServerConfigAccess dummyServer = new ServerConfigAccess() {
+		@Override
+		public int getCongratsCost() {
+			throw new IllegalStateException("Attempted to access property of Dummy Config Object");
+		}
 
-        @Override
-        public int getSignumCost() {
-            throw new IllegalStateException("Attempted to access property of Dummy Config Object");
-        }
-    };
-    private static CommonConfigAccess common = dummyCommon;
-    private static ClientConfigAccess client = dummyClient;
-    private static ServerConfigAccess server = dummyServer;
+		@Override
+		public int getSignumCost() {
+			throw new IllegalStateException("Attempted to access property of Dummy Config Object");
+		}
+	};
+	private static CommonConfigAccess common = dummyCommon;
+	private static ClientConfigAccess client = dummyClient;
+	private static ServerConfigAccess server = dummyServer;
 
-    public static CommonConfigAccess getCommon() {
-        return common;
-    }
+	public static CommonConfigAccess getCommon() {
+		return common;
+	}
 
-    public static void setCommon(CommonConfigAccess common) {
-        if (HexicalConfig.common != dummyCommon) {
-            Hexical.LOGGER.warn("CommonConfigAccess was replaced! Old {} New {}", HexicalConfig.common.getClass().getName(), common.getClass().getName());
-        }
-        HexicalConfig.common = common;
-    }
+	public static void setCommon(CommonConfigAccess common) {
+		if (HexicalConfig.common != dummyCommon) {
+			Hexical.LOGGER.warn("CommonConfigAccess was replaced! Old {} New {}", HexicalConfig.common.getClass().getName(), common.getClass().getName());
+		}
+		HexicalConfig.common = common;
+	}
 
-    public static ClientConfigAccess getClient() {
-        return client;
-    }
+	public static ClientConfigAccess getClient() {
+		return client;
+	}
 
-    public static void setClient(ClientConfigAccess client) {
-        if (HexicalConfig.client != dummyClient) {
-            Hexical.LOGGER.warn("ClientConfigAccess was replaced! Old {} New {}", HexicalConfig.client.getClass().getName(), client.getClass().getName());
-        }
-        HexicalConfig.client = client;
-    }
+	public static void setClient(ClientConfigAccess client) {
+		if (HexicalConfig.client != dummyClient) {
+			Hexical.LOGGER.warn("ClientConfigAccess was replaced! Old {} New {}", HexicalConfig.client.getClass().getName(), client.getClass().getName());
+		}
+		HexicalConfig.client = client;
+	}
 
-    public static ServerConfigAccess getServer() {
-        return server;
-    }
+	public static ServerConfigAccess getServer() {
+		return server;
+	}
 
-    public static void setServer(ServerConfigAccess server) {
+	public static void setServer(ServerConfigAccess server) {
 
-        if (HexicalConfig.server != dummyServer) {
-            Hexical.LOGGER.warn("ServerConfigAccess was replaced! Old {} New {}", HexicalConfig.server.getClass().getName(), server.getClass().getName());
-        }
-        HexicalConfig.server = server;
-    }
+		if (HexicalConfig.server != dummyServer) {
+			Hexical.LOGGER.warn("ServerConfigAccess was replaced! Old {} New {}", HexicalConfig.server.getClass().getName(), server.getClass().getName());
+		}
+		HexicalConfig.server = server;
+	}
 
-    public static int bound(int toBind, int lower, int upper) {
-        return Math.min(Math.max(toBind, lower), upper);
-    }
+	public static int bound(int toBind, int lower, int upper) {
+		return Math.min(Math.max(toBind, lower), upper);
+	}
 
-    public static double bound(double toBind, double lower, double upper) {
-        return Math.min(Math.max(toBind, lower), upper);
-    }
+	public static double bound(double toBind, double lower, double upper) {
+		return Math.min(Math.max(toBind, lower), upper);
+	}
 
-    public interface CommonConfigAccess {
-    }
+	public interface CommonConfigAccess {
+	}
 
-    public interface ClientConfigAccess {
-    }
+	public interface ClientConfigAccess {
+	}
 
-    public interface ServerConfigAccess {
-        double DEF_MIN_COST = 0.0001;
-        double DEF_MAX_COST = 10_000.0;
-        double DEFAULT_CONGRATS_COST = 1.5;
-        double DEFAULT_SIGNUM_COST = 1.0;
+	public interface ServerConfigAccess {
+		double DEF_MIN_COST = 0.0001;
+		double DEF_MAX_COST = 10_000.0;
+		double DEFAULT_CONGRATS_COST = 1.5;
+		double DEFAULT_SIGNUM_COST = 1.0;
 
-        int getCongratsCost();
+		int getCongratsCost();
 
-        int getSignumCost();
-    }
+		int getSignumCost();
+	}
 }
