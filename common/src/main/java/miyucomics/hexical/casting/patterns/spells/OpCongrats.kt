dad706e82a650e41e195502c940fbca793799a73
Miyu Nguyen <nbriannyc@gmail.com>
2024-02-13 15:48:27 -0500

    Chorus blink spell

--- common/src/main/java/miyucomics/hexical/casting/patterns/spells/OpCongrats.kt
+++ common/src/main/java/miyucomics/hexical/casting/patterns/spells/OpCongrats.kt
@@ -13,42 +13,15 @@ import net.minecraft.text.Text
 
 class OpCongrats : SpellAction {
 	override val argc = 1
-	private val cost = 2 * MediaConstants.DUST_UNIT
 
-	/**
-	 * The method called when this Action is actually executed. Accepts the [args]
-	 * that were on the stack (there will be [argc] of them), and the [ctx],
-	 * which contains things like references to the caster, the ServerLevel,
-	 * methods to determine whether locations and entities are in ambit, etc.
-	 * Returns a triple of things. The [RenderedSpell] is responsible for the spell actually
-	 * doing things in the world, the [Int] is how much media the spell should cost,
-	 * and the [List] of [ParticleSpray] renders particle effects for the result of the SpellAction.
-	 *
-	 * The [execute] method should only contain code to find the targets of the spell and validate
-	 * them. All the code that actually makes changes to the world (breaking blocks, teleporting things,
-	 * etc.) should be in the private [Spell] data class below.
-	 */
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
 		val target = args.getEntity(0, argc)
 		ctx.assertEntityInRange(target)
-
-		if (target !is ServerPlayerEntity) throw MishapBadEntity(
-				target,
-				Text.translatable("text.hexical.congrats.player")
-		)
-
-		return Triple(
-				Spell(target),
-				cost,
-				listOf(ParticleSpray.burst(target.pos, 1.0))
-		)
+		if (target !is ServerPlayerEntity)
+			throw MishapBadEntity(target, Text.translatable("text.hexical.congrats.player"))
+		return Triple(Spell(target), 2 * MediaConstants.DUST_UNIT, listOf(ParticleSpray.burst(target.pos, 1.0)))
 	}
 
-	/**
-	 * This class is responsible for actually making changes to the world. It accepts parameters to
-	 * define where/what it should affect (for this example the parameter is [player]), and the
-	 * [cast] method within is responsible for using that data to alter the world.
-	 */
 	private data class Spell(val player: ServerPlayerEntity) : RenderedSpell {
 		override fun cast(ctx: CastingContext) {
 			ctx.caster.sendMessage(Text.translatable("text.hexical.congrats", player.displayName));
