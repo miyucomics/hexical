8b8a1def03efc4f0e0f5222360a60cef4656f1d4
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-02-17 20:23:04 -0500

    Simplify config

--- fabric/src/main/java/miyucomics/hexical/fabric/HexicalConfigFabric.java
+++ fabric/src/main/java/miyucomics/hexical/fabric/HexicalConfigFabric.java
@@ -1,6 +1,5 @@
 package miyucomics.hexical.fabric;
 
-import at.petrak.hexcasting.api.misc.MediaConstants;
 import dev.architectury.platform.Platform;
 import miyucomics.hexical.Hexical;
 import miyucomics.hexical.api.config.HexicalConfig;
@@ -28,52 +27,19 @@ public class HexicalConfigFabric extends PartitioningSerializer.GlobalData {
 	public static void init() {
 		AutoConfig.register(HexicalConfigFabric.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new));
 		var instance = AutoConfig.getConfigHolder(HexicalConfigFabric.class).getConfig();
-
 		HexicalConfig.setCommon(instance.common);
-
-		if (Platform.getEnv().equals(EnvType.CLIENT)) {
+		if (Platform.getEnv().equals(EnvType.CLIENT))
 			HexicalConfig.setClient(instance.client);
-		}
-
 		HexicalConfig.setServer(instance.server);
 	}
 
-
 	@Config(name = "common")
-	private static class Common implements ConfigData, HexicalConfig.CommonConfigAccess {
-	}
-
+	private static class Common implements ConfigData, HexicalConfig.CommonConfigAccess {}
 	@Config(name = "client")
-	private static class Client implements ConfigData, HexicalConfig.ClientConfigAccess {
-	}
-
-
+	private static class Client implements ConfigData, HexicalConfig.ClientConfigAccess {}
 	@Config(name = "server")
 	private static class Server implements ConfigData, HexicalConfig.ServerConfigAccess {
-
-		@ConfigEntry.Gui.CollapsibleObject
-		private Costs costs = new Costs();
-
 		@Override
-		public void validatePostLoad() {
-			this.costs.signumCost = HexicalConfig.bound(this.costs.signumCost, DEF_MIN_COST, DEF_MAX_COST);
-			this.costs.congratsCost = HexicalConfig.bound(this.costs.congratsCost, DEF_MIN_COST, DEF_MAX_COST);
-		}
-
-		@Override
-		public int getSignumCost() {
-			return (int) (costs.signumCost * MediaConstants.DUST_UNIT);
-		}
-
-		@Override
-		public int getCongratsCost() {
-			return (int) (costs.congratsCost * MediaConstants.DUST_UNIT);
-		}
-
-		static class Costs {
-			// costs of actions
-			double signumCost = DEFAULT_SIGNUM_COST;
-			double congratsCost = DEFAULT_CONGRATS_COST;
-		}
+		public void validatePostLoad() {}
 	}
 }
