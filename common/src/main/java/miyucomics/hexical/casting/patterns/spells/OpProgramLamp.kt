ea79ce31d9ec5369cef05ce56e644d7417711d1d
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-02-21 12:45:06 -0500

    Finished documentation, added comments

--- common/src/main/java/miyucomics/hexical/casting/patterns/spells/OpProgramLamp.kt
+++ common/src/main/java/miyucomics/hexical/casting/patterns/spells/OpProgramLamp.kt
@@ -9,15 +9,19 @@ import at.petrak.hexcasting.xplat.IXplatAbstractions
 class OpProgramLamp : SpellAction {
 	override val argc = 1
 
+	// Right now, this pattern is extremely overpowered
+	// Later it'll be specialized to program only the lamp
+	// But for debug purposes, it lets you program any casting item for free
+	// And even gives you ten thousand crystals worth of media as a bonus
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
 		val patterns = args.getList(0, argc).toList()
-		return Triple(Spell(patterns), 3 * MediaConstants.DUST_UNIT, listOf())
+		return Triple(Spell(patterns), 0, listOf())
 	}
 
 	private data class Spell(val patterns: List<Iota>) : RenderedSpell {
 		override fun cast(ctx: CastingContext) {
 			val (handStack, _) = ctx.getHeldItemToOperateOn { IXplatAbstractions.INSTANCE.findHexHolder(it) != null }
-			IXplatAbstractions.INSTANCE.findHexHolder(handStack)?.writeHex(patterns, 100000)
+			IXplatAbstractions.INSTANCE.findHexHolder(handStack)?.writeHex(patterns, MediaConstants.CRYSTAL_UNIT * 10000)
 		}
 	}
 }
