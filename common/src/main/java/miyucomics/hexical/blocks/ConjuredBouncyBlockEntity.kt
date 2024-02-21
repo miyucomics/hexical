dbfedf7f91bdfba36c98b98de1c33ff811b9588b
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-02-21 16:22:59 -0500

    Refactored code to make conjuring blocks easier

--- common/src/main/java/miyucomics/hexical/blocks/ConjuredBouncyBlockEntity.kt
+++ common/src/main/java/miyucomics/hexical/blocks/ConjuredBouncyBlockEntity.kt
@@ -1,67 +1,15 @@
 package miyucomics.hexical.blocks
 
-import at.petrak.hexcasting.api.block.HexBlockEntity
-import at.petrak.hexcasting.api.misc.FrozenColorizer
-import at.petrak.hexcasting.common.blocks.BlockConjured
-import at.petrak.hexcasting.common.particles.ConjureParticleOptions
+import miyucomics.hexical.generics.GenericConjuredBlockEntity
 import miyucomics.hexical.registry.HexicalBlocks
 import net.minecraft.block.BlockState
-import net.minecraft.entity.Entity
-import net.minecraft.nbt.NbtCompound
+import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Vec3d
-import java.util.*
 
-class ConjuredBouncyBlockEntity(pos: BlockPos?, state: BlockState?) : HexBlockEntity(HexicalBlocks.CONJURED_BOUNCY_BLOCK_ENTITY, pos, state) {
-	private var colorizer: FrozenColorizer = FrozenColorizer.DEFAULT.get()
-
-	fun walkParticle(entity: Entity) {
-		for (i in 0..2) {
-			val color = colorizer.getColor(entity.age.toFloat(), entity.pos.add(Vec3d(RANDOM.nextDouble(), RANDOM.nextDouble(), RANDOM.nextDouble()).multiply(RANDOM.nextDouble() * 3)))
-			assert(world != null)
-			world!!.addParticle(
-				ConjureParticleOptions(color, false),
-				entity.x + (RANDOM.nextFloat() * 0.6) - 0.3,
-				getPos().y + (RANDOM.nextFloat() * 0.05) + 0.95,
-				entity.z + (RANDOM.nextFloat() * 0.6) - 0.3,
-				RANDOM.nextFloat(-0.02f, 0.02f).toDouble(),
-				RANDOM.nextFloat(0.02f).toDouble(),
-				RANDOM.nextFloat(-0.02f, 0.02f).toDouble()
-			)
-		}
-	}
-
-	fun particleEffect() {
-		val color = colorizer.getColor(RANDOM.nextFloat() * 16384, Vec3d(RANDOM.nextDouble(), RANDOM.nextDouble(), RANDOM.nextDouble()).multiply(RANDOM.nextDouble() * 3))
-		assert(world != null)
-		if (RANDOM.nextFloat() < 0.2) {
-			world!!.addParticle(
-				ConjureParticleOptions(color, false),
-				getPos().x.toDouble() + RANDOM.nextFloat(),
-				getPos().y.toDouble() + RANDOM.nextFloat(),
-				getPos().z.toDouble() + RANDOM.nextFloat(),
-				RANDOM.nextFloat(-0.02f, 0.02f).toDouble(),
-				RANDOM.nextFloat(-0.02f, 0.02f).toDouble(),
-				RANDOM.nextFloat(-0.02f, 0.02f).toDouble()
-			)
-		}
-	}
-
-	override fun saveModData(tag: NbtCompound) {
-		tag.put(TAG_COLORIZER, colorizer.serializeToNBT())
-	}
-
-	override fun loadModData(tag: NbtCompound) {
-		this.colorizer = FrozenColorizer.fromNBT(tag.getCompound(TAG_COLORIZER))
-	}
-
-	fun setColorizer(colorizer: FrozenColorizer) {
-		this.colorizer = colorizer
-		this.sync()
-	}
-
+class ConjuredBouncyBlockEntity(type: BlockEntityType<ConjuredBouncyBlockEntity>, pos: BlockPos?, state: BlockState?) : GenericConjuredBlockEntity<ConjuredBouncyBlockEntity>(pos, state, type) {
 	companion object {
-		private val RANDOM = Random()
-		const val TAG_COLORIZER: String = "tag_colorizer"
+		fun init (pos: BlockPos?, state: BlockState?): ConjuredBouncyBlockEntity {
+			return ConjuredBouncyBlockEntity(HexicalBlocks.CONJURED_BOUNCY_BLOCK_ENTITY, pos, state)
+		}
 	}
 }
