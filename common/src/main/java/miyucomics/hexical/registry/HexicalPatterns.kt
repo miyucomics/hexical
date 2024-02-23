c07f9088ef17216cfec796c248735ded9f8e9c51
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-02-23 12:37:08 -0500

    Added more modifiers to conjured blocks

--- common/src/main/java/miyucomics/hexical/registry/HexicalPatterns.kt
+++ common/src/main/java/miyucomics/hexical/registry/HexicalPatterns.kt
@@ -17,7 +17,9 @@ object HexicalPatterns {
 	var NEPHTHYS_GAMBIT = register(HexPattern.fromAngles("deaqqdq", HexDir.SOUTH_EAST), "nephthys", OpNephthys);
 
 	var CONJURE_ADVANCED_BLOCK: HexPattern = register(HexPattern.fromAngles("dee", HexDir.NORTH_WEST), "conjure_advanced_block", OpConjureAdvancedBlock())
-	var CONFIGURE_BLOCK_VOLATILE: HexPattern = register(HexPattern.fromAngles("deew", HexDir.NORTH_WEST), "modify_block_volatile", OpConfigureBlock("volatile"))
+	var CONFIGURE_BLOCK_BOUNCY: HexPattern = register(HexPattern.fromAngles("deeqa", HexDir.NORTH_WEST), "modify_block_bouncy", OpConfigureBlock("bouncy"))
+	var CONFIGURE_BLOCK_EPHEMERAL: HexPattern = register(HexPattern.fromAngles("deewwaawd", HexDir.NORTH_WEST), "modify_block_ephemeral", OpConfigureBlock("ephemeral", 1))
+	var CONFIGURE_BLOCK_VOLATILE: HexPattern = register(HexPattern.fromAngles("deewedeeeee", HexDir.NORTH_WEST), "modify_block_volatile", OpConfigureBlock("volatile"))
 
 	var CHORUS_BLINK: HexPattern = register(HexPattern.fromAngles("aawqqqq", HexDir.SOUTH_EAST), "chorus_blink", OpChorusBlink())
 	var PROGRAM_LAMP: HexPattern = register(HexPattern.fromAngles("wwqqqqq", HexDir.EAST), "program_lamp", OpProgramLamp())
