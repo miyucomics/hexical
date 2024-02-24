a93c0156a9d205d9944298b5c150eb3495fd0ed9
Miyu Nguyen <nbriannyc@gmail.com>
2024-02-24 08:44:53 -0500

    Created helper function for casting silently and particlelessly

---
+++ common/src/main/java/miyucomics/hexical/utils/CastingUtils.kt
@@ -0,0 +1,20 @@
+package miyucomics.hexical.utils
+
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.casting.CastingHarness
+import at.petrak.hexcasting.api.spell.iota.Iota
+import miyucomics.hexical.interfaces.CastingContextMixinInterface
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
+
+class CastingUtils {
+	companion object {
+		@Suppress("CAST_NEVER_SUCCEEDS")
+		fun castInvisibly(world: ServerWorld, user: ServerPlayerEntity, hex: List<Iota>) {
+			val context = CastingContext(user, user.activeHand, CastingContext.CastSource.PACKAGED_HEX)
+			(context as CastingContextMixinInterface).setCastByLamp(true)
+			val harness = CastingHarness(context)
+			harness.executeIotas(hex, world)
+		}
+	}
+}
