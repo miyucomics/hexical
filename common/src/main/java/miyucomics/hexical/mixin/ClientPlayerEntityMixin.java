2f2a3c9eb47e4f8564cff50c3cd2ba348819916a
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-03-01 18:56:12 -0500

    Added movement in casting screen

---
+++ common/src/main/java/miyucomics/hexical/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,36 @@
+package miyucomics.hexical.mixin;
+
+import at.petrak.hexcasting.client.gui.GuiSpellcasting;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+	@Shadow public Input input;
+
+	@Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/tutorial/TutorialManager;onMovement(Lnet/minecraft/client/input/Input;)V"))
+	private void onInputUpdate(CallbackInfo info) {
+		MinecraftClient client = MinecraftClient.getInstance();
+		if (!(client.currentScreen instanceof GuiSpellcasting))
+			return;
+		KeyBinding.updatePressedStates();
+		GameOptions keys = client.options;
+		keys.dropKey.setPressed(false);
+		input.pressingForward = keys.forwardKey.isPressed();
+		input.pressingBack = keys.backKey.isPressed();
+		input.pressingLeft = keys.leftKey.isPressed();
+		input.pressingRight = keys.rightKey.isPressed();
+		input.jumping = keys.jumpKey.isPressed();
+		input.sneaking = keys.sneakKey.isPressed();
+		if (!client.player.isSprinting())
+			client.player.setSprinting(keys.sprintKey.isPressed());
+	}
+}
