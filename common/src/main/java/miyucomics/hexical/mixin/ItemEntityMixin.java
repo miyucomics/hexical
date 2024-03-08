52050212cb0f47f254e64823fb7c4147936f0d58
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-03-07 19:34:38 -0500

    Deactivate lamps upon being dropped

---
+++ common/src/main/java/miyucomics/hexical/mixin/ItemEntityMixin.java
@@ -0,0 +1,21 @@
+package miyucomics.hexical.mixin;
+
+import miyucomics.hexical.registry.HexicalItems;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin {
+	@Shadow public abstract ItemStack getStack();
+
+	@Inject(method = "tick", at = @At("HEAD"))
+	void deactivateLamp(CallbackInfo ci) {
+		if (this.getStack().getItem() == HexicalItems.INSTANCE.getARCH_LAMP_ITEM() && this.getStack().getNbt() != null)
+			this.getStack().getNbt().putBoolean("active", false);
+	}
+}
