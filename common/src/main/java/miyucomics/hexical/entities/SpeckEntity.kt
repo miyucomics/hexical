95c19967065c57c55d7fa69b1d16bc0c5409ae88
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-10 12:16:04 -0400

    More work on specks

--- common/src/main/java/miyucomics/hexical/entities/SpeckEntity.kt
+++ common/src/main/java/miyucomics/hexical/entities/SpeckEntity.kt
@@ -1,5 +1,6 @@
 package miyucomics.hexical.entities
 
+import at.petrak.hexcasting.api.misc.FrozenColorizer
 import at.petrak.hexcasting.api.spell.math.HexDir
 import at.petrak.hexcasting.api.spell.math.HexPattern
 import at.petrak.hexcasting.api.utils.putCompound
@@ -16,32 +17,46 @@ import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket
 import net.minecraft.util.math.Vec2f
 import net.minecraft.world.World
 
-val patternNbtDataTracker: TrackedData<NbtCompound> = DataTracker.registerData(SpeckEntity::class.java, TrackedDataHandlerRegistry.NBT_COMPOUND)
+val patternDataTracker: TrackedData<NbtCompound> = DataTracker.registerData(SpeckEntity::class.java, TrackedDataHandlerRegistry.NBT_COMPOUND)
+val pigmentDataTracker: TrackedData<NbtCompound> = DataTracker.registerData(SpeckEntity::class.java, TrackedDataHandlerRegistry.NBT_COMPOUND)
 
 class SpeckEntity(entityType: EntityType<SpeckEntity?>?, world: World?) : Entity(entityType, world) {
 	private var pattern: HexPattern = HexPattern.fromAngles("w", HexDir.WEST)
+	private var pigment: FrozenColorizer = FrozenColorizer.DEFAULT.get()
 
 	override fun initDataTracker() {
-		dataTracker.startTracking(patternNbtDataTracker, NbtCompound())
+		dataTracker.startTracking(patternDataTracker, NbtCompound())
+		dataTracker.startTracking(pigmentDataTracker, NbtCompound())
 	}
 
 	override fun readCustomDataFromNbt(nbt: NbtCompound) {
 		pattern = HexPattern.fromNBT(nbt.getCompound("pattern"))
-		dataTracker.set(patternNbtDataTracker, nbt.getCompound("pattern"))
+		pigment = FrozenColorizer.fromNBT(nbt.getCompound("pigment"))
+		dataTracker.set(patternDataTracker, nbt.getCompound("pattern"))
+		dataTracker.set(pigmentDataTracker, nbt.getCompound("pattern"))
 	}
 
 	override fun writeCustomDataToNbt(nbt: NbtCompound) {
 		nbt.putCompound("pattern", pattern.serializeToNBT())
+		nbt.putCompound("pigment", pigment.serializeToNBT())
 	}
 
 	fun setPattern(pattern: HexPattern) {
 		this.pattern = pattern
-		dataTracker.set(patternNbtDataTracker, pattern.serializeToNBT())
+		dataTracker.set(patternDataTracker, pattern.serializeToNBT())
 	}
 
-	fun getPattern(): List<Vec2f> {
-		val pattern = HexPattern.fromNBT(dataTracker.get(patternNbtDataTracker))
-		return pattern.toLines(0.25f, pattern.getCenter(1f).multiply(-1f))
+	fun setPigment(pigment: FrozenColorizer) {
+		this.pigment = pigment
+		dataTracker.set(pigmentDataTracker, pigment.serializeToNBT())
+	}
+
+	fun getPattern(): HexPattern {
+		return HexPattern.fromNBT(dataTracker.get(patternDataTracker))
+	}
+
+	fun getPigment(): FrozenColorizer {
+		return FrozenColorizer.fromNBT(dataTracker.get(pigmentDataTracker))
 	}
 
 	override fun createSpawnPacket(): Packet<*> {
