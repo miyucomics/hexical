1c975254e9c3e746d80ff832c9eda5bc344dd9fe
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-11 19:52:28 -0400

    Clean up and started work on telepathy

--- common/src/main/java/miyucomics/hexical/HexicalClient.java
+++ common/src/main/java/miyucomics/hexical/HexicalClient.java
@@ -1,11 +1,23 @@
 package miyucomics.hexical;
 
+import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import miyucomics.hexical.entities.SpeckEntityRenderer;
 import miyucomics.hexical.registry.HexicalEntities;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 
 public class HexicalClient {
+	private static final KeyBinding TELEPATHY_KEYBIND = new KeyBinding("key.hexical.telepathy", GLFW.GLFW_KEY_G, "key.categories.hexical");
+
 	public static void init() {
+		ClientTickEvent.CLIENT_POST.register(client -> {
+			if (client.player == null)
+				return;
+			while (TELEPATHY_KEYBIND.wasPressed())
+				client.player.sendMessage(Text.literal("using mind powers!!"), false);
+		});
 		EntityRendererRegistry.register(HexicalEntities.INSTANCE::getSPECK_ENTITY, SpeckEntityRenderer::new);
 	}
 }
