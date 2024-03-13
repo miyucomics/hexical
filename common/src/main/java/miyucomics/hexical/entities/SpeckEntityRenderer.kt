3fe6305d6fab11197cd8c25298a3f9339db57de5
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-13 19:02:39 -0400

    Finally finished specks

--- common/src/main/java/miyucomics/hexical/entities/SpeckEntityRenderer.kt
+++ common/src/main/java/miyucomics/hexical/entities/SpeckEntityRenderer.kt
@@ -9,11 +9,9 @@ import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.entity.EntityRenderer
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.entity.Entity
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.Quaternion
-import net.minecraft.util.math.Vec2f
-import net.minecraft.util.math.Vec3d
-import net.minecraft.util.math.Vec3f
+import net.minecraft.util.math.*
 import kotlin.math.asin
 import kotlin.math.atan2
 
@@ -32,27 +30,15 @@ class SpeckEntityRenderer(ctx: EntityRendererFactory.Context?) : EntityRenderer<
 			lines[i] = Vec2f(lines[i].x, -lines[i].y)
 		val zappy = makeZappy(lines, findDupIndices(pattern.positions()), 10, 1.5f, 0.1f, 0.2f, 0f, 1f, hashCode().toDouble())
 		val outer = entity.getPigment().getColor(entity.age.toFloat(), entity.pos)
-
-		val rotation = Vec3f.NEGATIVE_Y.getRadialQuaternion(entity.yaw)
-		rotation.hamiltonProduct(Vec3f.POSITIVE_X.getRadialQuaternion((entity.pitch - Math.PI / 2).toFloat()))
-		matrices.multiply(rotation)
+		if (entity.yaw > 90 || entity.yaw < -90) {
+			matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(-entity.pitch))
+		} else {
+			matrices.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(entity.pitch))
+		}
+		matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-entity.yaw))
 		drawLineSeq(matrices.peek().positionMatrix, zappy, 0.05f, 0.01f, outer, outer)
 		matrices.pop()
 		RenderSystem.setShader { oldShader }
-	}
-
-	fun getQuaternion(direction: Vec3d, forward: Boolean): Quaternion {
-		var copiedVector = direction
-		copiedVector = copiedVector.normalize()
-		val pitch = asin(-copiedVector.y).toFloat()
-		val yaw = (-atan2(copiedVector.x, copiedVector.z)).toFloat()
-		val rotation = Vec3f.NEGATIVE_Y.getRadialQuaternion(yaw)
-		if (forward) {
-			rotation.hamiltonProduct(Vec3f.POSITIVE_X.getRadialQuaternion((pitch + Math.PI / 2).toFloat()))
-		} else {
-			rotation.hamiltonProduct(Vec3f.POSITIVE_X.getRadialQuaternion((pitch - Math.PI / 2).toFloat()))
-		}
-		return rotation
 	}
 
 	override fun getTexture(entity: SpeckEntity?): Identifier? { return null }
