57fef2d1913c265de10c2c35464039a0559a3a48
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-15 21:05:36 -0400

    Added more telepathy groundwork

---
+++ common/src/main/java/miyucomics/hexical/persistent_state/TelepathyData.kt
@@ -0,0 +1,5 @@
+package miyucomics.hexical.persistent_state
+
+class TelepathyData {
+
+}
--- common/src/main/java/miyucomics/hexical/registry/HexicalKeybinds.kt
+++ common/src/main/java/miyucomics/hexical/registry/HexicalKeybinds.kt
@@ -1,14 +1,18 @@
 package miyucomics.hexical.registry
 
 import dev.architectury.event.events.client.ClientTickEvent
+import dev.architectury.networking.NetworkManager
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry
+import io.netty.buffer.Unpooled
+import miyucomics.hexical.Hexical
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.option.KeyBinding
-import net.minecraft.text.Text
+import net.minecraft.network.PacketByteBuf
 import org.lwjgl.glfw.GLFW
 
 object HexicalKeybinds {
 	private val TELEPATHY_KEYBIND = KeyBinding("key.hexical.telepathy", GLFW.GLFW_KEY_G, "key.categories.hexical")
+	private var previouslyHeld = false
 
 	@JvmStatic
 	fun init() {
@@ -16,8 +20,14 @@ object HexicalKeybinds {
 		ClientTickEvent.CLIENT_POST.register(ClientTickEvent.Client { client: MinecraftClient ->
 			if (client.player == null)
 				return@Client
-			while (TELEPATHY_KEYBIND.wasPressed())
-				client.player!!.sendMessage(Text.literal("using mind powers!!"), false)
+			if (TELEPATHY_KEYBIND.isPressed) {
+				if (!previouslyHeld)
+					NetworkManager.sendToServer(Hexical.START_TELEPATHY_PACKET, PacketByteBuf(Unpooled.buffer()))
+				previouslyHeld = true
+			} else if (previouslyHeld) {
+				NetworkManager.sendToServer(Hexical.STOP_TELEPATHY_PACKET, PacketByteBuf(Unpooled.buffer()))
+				previouslyHeld = false
+			}
 		})
 	}
 }
