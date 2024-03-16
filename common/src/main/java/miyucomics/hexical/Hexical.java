56b2bbdf875ac25120e3b57a66d80f8ad055f98d
miyucomics <nbriannyc@gmail.com>
2024-03-15 21:05:36 -0400

    Added more telepathy groundwork

--- common/src/main/java/miyucomics/hexical/Hexical.java
+++ common/src/main/java/miyucomics/hexical/Hexical.java
@@ -6,6 +6,8 @@ import net.minecraft.util.Identifier;
 public class Hexical {
 	public static final String MOD_ID = "hexical";
 	public static final Identifier CAST_CONJURED_STAFF_PACKET = new Identifier(MOD_ID, "cast_conjured_staff");
+	public static final Identifier START_TELEPATHY_PACKET = new Identifier(MOD_ID, "start_telepathy");
+	public static final Identifier STOP_TELEPATHY_PACKET = new Identifier(MOD_ID, "stop_telepathy");
 
 	public static void init() {
 		HexicalAbstractions.initPlatformSpecific();
