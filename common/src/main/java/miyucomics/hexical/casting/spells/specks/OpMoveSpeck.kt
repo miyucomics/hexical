675f2ae66a75391a550f65860501b702f1a110f3
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-03-22 22:19:26 -0400

    Specks!

---
+++ common/src/main/java/miyucomics/hexical/casting/spells/specks/OpMoveSpeck.kt
@@ -0,0 +1,25 @@
+package miyucomics.hexical.casting.spells.specks
+
+import at.petrak.hexcasting.api.spell.*
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapBadEntity
+import miyucomics.hexical.entities.SpeckEntity
+import net.minecraft.util.math.Vec3d
+
+class OpMoveSpeck : SpellAction {
+	override val argc = 2
+	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
+		val entity = args.getEntity(0, argc)
+		val position = args.getVec3(1, argc)
+		ctx.assertEntityInRange(entity)
+		ctx.assertVecInRange(position)
+		if (entity !is SpeckEntity)
+			throw MishapBadEntity.of(entity, "speck")
+		return Triple(Spell(entity, position), 0, listOf())
+	}
+
+	private data class Spell(val speck: SpeckEntity, val position: Vec3d) : RenderedSpell {
+		override fun cast(ctx: CastingContext) = speck.setPosition(position)
+	}
+}
