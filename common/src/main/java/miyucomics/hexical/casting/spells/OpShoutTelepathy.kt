37f2c7bd42db979e0438974ffea99e4c5b0778f8
miyucomics <nbriannyc@gmail.com>
2024-03-23 16:29:10 -0400

    Way more documentation

---
+++ common/src/main/java/miyucomics/hexical/casting/spells/OpShoutTelepathy.kt
@@ -0,0 +1,14 @@
+package miyucomics.hexical.casting.spells
+
+import at.petrak.hexcasting.api.spell.*
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.iota.Iota
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket
+
+class OpShoutTelepathy : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		ctx.caster.networkHandler.sendPacket(TitleS2CPacket(args[0].display()))
+		return listOf()
+	}
+}
