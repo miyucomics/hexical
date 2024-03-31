fb2be220c7237246d840ad096f76fd4de91cd5a4
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-30 21:00:21 -0400

    Started identifier iota

---
+++ common/src/main/java/miyucomics/hexical/iota/IdentifierIota.kt
@@ -0,0 +1,40 @@
+package miyucomics.hexical.iota
+
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.iota.IotaType
+import at.petrak.hexcasting.api.utils.styledWith
+import miyucomics.hexical.registry.HexicalIota
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.util.Identifier
+
+class IdentifierIota(identifier: Identifier) : Iota(HexicalIota.IDENTIFIER_IOTA, identifier) {
+	val identifier = this.payload as Identifier
+
+	override fun isTruthy() = true
+	override fun toleratesOther(that: Iota) = (typesMatch(this, that) && that is IdentifierIota) && this.identifier == that.identifier
+
+	override fun serialize(): NbtElement {
+		val compound = NbtCompound()
+		compound.putString("namespace", identifier.namespace)
+		compound.putString("path", identifier.path)
+		return compound
+	}
+
+	companion object {
+		var TYPE: IotaType<IdentifierIota> = object : IotaType<IdentifierIota>() {
+			override fun color() = -0xaaaa01
+			override fun display(tag: NbtElement): Text {
+				val compound = (tag as NbtCompound)
+				return Text.literal(compound.getString("namespace") + ":" + compound.getString("path")).styledWith(Formatting.GOLD)
+			}
+			override fun deserialize(tag: NbtElement?, world: ServerWorld?): IdentifierIota {
+				val compound = (tag!! as NbtCompound)
+				return IdentifierIota(Identifier(compound.getString("namespace"), compound.getString("path")))
+			}
+		}
+	}
+}
