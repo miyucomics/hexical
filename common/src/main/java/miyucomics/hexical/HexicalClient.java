a9ae8dfa197de24c213a0f4e3125b8de2ffe1cb2
Miyu Nguyen <nbriannyc@gmail.com>
2024-03-31 16:52:08 -0400

    Some random changes

--- common/src/main/java/miyucomics/hexical/HexicalClient.java
+++ common/src/main/java/miyucomics/hexical/HexicalClient.java
@@ -1,9 +1,11 @@
 package miyucomics.hexical;
 
 import miyucomics.hexical.registry.HexicalEntities;
+import miyucomics.hexical.registry.HexicalKeybinds;
 
 public class HexicalClient {
 	public static void init() {
 		HexicalEntities.clientInit();
+		HexicalKeybinds.init();
 	}
 }
