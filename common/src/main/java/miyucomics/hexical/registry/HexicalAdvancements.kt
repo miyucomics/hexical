ce7652409a48499f685ad8b881f1e3033667d4d5
miyucomics <nbriannyc@gmail.com>
2024-04-02 21:09:04 -0400

    More advancements!

--- common/src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
+++ common/src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
@@ -1,11 +1,12 @@
 package miyucomics.hexical.registry
 
-import miyucomics.hexical.advancements.EducateGenieCriterion
-import miyucomics.hexical.advancements.ReloadLampCriterion
-import miyucomics.hexical.advancements.UseUpLampCriterion
+import miyucomics.hexical.advancements.*
 import net.minecraft.advancement.criterion.Criteria
 
 object HexicalAdvancements {
+	val AR: ARCriterion = Criteria.register(ARCriterion())
+	val DIY: DIYCriterion = Criteria.register(DIYCriterion())
+	val HALLUCINATE: HallucinateCriterion = Criteria.register(HallucinateCriterion())
 	val EDUCATE_GENIE: EducateGenieCriterion = Criteria.register(EducateGenieCriterion())
 	val RELOAD_LAMP: ReloadLampCriterion = Criteria.register(ReloadLampCriterion())
 	val USE_UP_LAMP: UseUpLampCriterion = Criteria.register(UseUpLampCriterion())
