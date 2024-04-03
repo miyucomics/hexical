98a81a5ceb56efeb3a0d3e6665f9ec7e670e71b2
miyucomics <nbriannyc@gmail.com>
2024-04-03 17:39:31 -0400

    Removed chameleons and made energized a modifier

--- common/src/main/java/miyucomics/hexical/blocks/MageBlockEntity.kt
+++ common/src/main/java/miyucomics/hexical/blocks/MageBlockEntity.kt
@@ -3,6 +3,7 @@ package miyucomics.hexical.blocks
 import at.petrak.hexcasting.api.block.HexBlockEntity
 import at.petrak.hexcasting.api.misc.FrozenColorizer
 import at.petrak.hexcasting.api.spell.getPositiveInt
+import at.petrak.hexcasting.api.spell.getPositiveIntUnder
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.common.particles.ConjureParticleOptions
 import miyucomics.hexical.iota.getIdentifier
@@ -20,67 +21,47 @@ class MageBlockEntity(pos: BlockPos?, state: BlockState?) : HexBlockEntity(Hexic
 	private var colorizer: FrozenColorizer = FrozenColorizer.DEFAULT.get()
 	var properties: MutableMap<String, Boolean> = mutableMapOf(
 		"bouncy" to false,
-		"chameleon" to false,
+		"energized" to false,
 		"ephemeral" to false,
 		"invisible" to false,
 		"replaceable" to false,
 		"volatile" to false
 	)
-	var appearance: Identifier? = null
+	var redstone: Int = 0
 	var lifespan: Int = 0
 
 	fun walkParticle(entity: Entity) {
 		val color = colorizer.getColor(entity.age.toFloat(), entity.pos.add(Vec3d(random.nextDouble(), random.nextDouble(), random.nextDouble()).multiply(random.nextDouble() * 3)))
 		for (i in 0..2) {
 			assert(world != null)
-			world!!.addParticle(
-				ConjureParticleOptions(color, false),
-				entity.x + (random.nextFloat() * 0.6) - 0.3,
-				getPos().y + (random.nextFloat() * 0.05) + 0.95,
-				entity.z + (random.nextFloat() * 0.6) - 0.3,
-				random.nextFloat(-0.02f, 0.02f).toDouble(),
-				random.nextFloat(0.02f).toDouble(),
-				random.nextFloat(-0.02f, 0.02f).toDouble()
-			)
+			world!!.addParticle(ConjureParticleOptions(color, false), entity.x + (random.nextFloat() * 0.6) - 0.3, getPos().y + (random.nextFloat() * 0.05) + 0.95, entity.z + (random.nextFloat() * 0.6) - 0.3, random.nextFloat(-0.02f, 0.02f).toDouble(), random.nextFloat(0.02f).toDouble(), random.nextFloat(-0.02f, 0.02f).toDouble())
 		}
 	}
 
 	fun particleEffect() {
 		val color = colorizer.getColor(random.nextFloat() * 16384, Vec3d(random.nextDouble(), random.nextDouble(), random.nextDouble()).multiply(random.nextDouble() * 3))
 		assert(world != null)
-		if (random.nextFloat() < 0.2) {
-			world!!.addParticle(
-				ConjureParticleOptions(color, false),
-				getPos().x.toDouble() + random.nextFloat(),
-				getPos().y.toDouble() + random.nextFloat(),
-				getPos().z.toDouble() + random.nextFloat(),
-				random.nextFloat(-0.02f, 0.02f).toDouble(),
-				random.nextFloat(-0.02f, 0.02f).toDouble(),
-				random.nextFloat(-0.02f, 0.02f).toDouble()
-			)
-		}
+		if (random.nextFloat() < 0.2)
+			world!!.addParticle(ConjureParticleOptions(color, false), getPos().x.toDouble() + random.nextFloat(), getPos().y.toDouble() + random.nextFloat(), getPos().z.toDouble() + random.nextFloat(), random.nextFloat(-0.02f, 0.02f).toDouble(), random.nextFloat(-0.02f, 0.02f).toDouble(), random.nextFloat(-0.02f, 0.02f).toDouble())
 	}
 
 	override fun saveModData(tag: NbtCompound) {
 		tag.put("colorizer", colorizer.serializeToNBT())
 		properties.forEach { (key, value) -> tag.putBoolean(key, value) }
-		if (this.appearance != null)
-			tag.putString("appearance", this.appearance.toString())
 		tag.putInt("lifespan", this.lifespan)
+		tag.putInt("redstone", this.redstone)
 	}
 
 	override fun loadModData(tag: NbtCompound) {
 		this.colorizer = FrozenColorizer.fromNBT(tag.getCompound("colorizer"))
 		properties.keys.forEach { key -> properties[key] = tag.getBoolean(key) }
-		this.appearance = null
-		if (tag.contains("appearance"))
-			this.appearance = Identifier(tag.getString("appearance"))
 		this.lifespan = tag.getInt("lifespan")
+		this.redstone = tag.getInt("redstone")
 	}
 
 	fun setProperty(property: String, args: List<Iota>) {
-		if (property == "chameleon")
-			this.appearance = args.getIdentifier(0, args.size)
+		if (property == "energized")
+			this.redstone = args.getPositiveIntUnder(0, 16, args.size)
 		if (property == "ephemeral")
 			this.lifespan = args.getPositiveInt(0, args.size)
 		properties[property] = !properties[property]!!
