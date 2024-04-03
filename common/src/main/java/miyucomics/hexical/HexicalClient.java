6dd83c5bdf7770ab18f8fda95a36021e0592d252
Miyu Nguyen <nbriannyc@gmail.com>
2024-04-02 20:19:03 -0400

    Chameleon mage blocks

--- common/src/main/java/miyucomics/hexical/HexicalClient.java
+++ common/src/main/java/miyucomics/hexical/HexicalClient.java
@@ -1,10 +1,12 @@
 package miyucomics.hexical;
 
+import miyucomics.hexical.registry.HexicalBlocks;
 import miyucomics.hexical.registry.HexicalEntities;
 import miyucomics.hexical.registry.HexicalKeybinds;
 
 public class HexicalClient {
 	public static void init() {
+		HexicalBlocks.clientInit();
 		HexicalEntities.clientInit();
 		HexicalKeybinds.init();
 	}
