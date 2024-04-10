74d5333fb27d40a1c9edfd7c48fdd20f5c8f2c96
Miyu Nguyen <nbriannyc@gmail.com>
2024-04-10 18:04:05 -0400

    Fix speck stuff

--- common/src/main/java/miyucomics/hexical/casting/spells/circle/OpDisplace.kt
+++ common/src/main/java/miyucomics/hexical/casting/spells/circle/OpDisplace.kt
@@ -1,4 +1,4 @@
-package miyucomics.hexical.casting.spells
+package miyucomics.hexical.casting.spells.circle
 
 import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.spell.*
