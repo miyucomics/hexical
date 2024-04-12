eb32cc8c16fd627ef1faa7e4bc8e8f15de788706
Miyu Nguyen <nbriannyc@gmail.com>
2024-04-12 19:54:37 -0400

    Cleaned up code

--- common/src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
+++ common/src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
@@ -1,17 +1,31 @@
 package miyucomics.hexical.registry
 
-import miyucomics.hexical.advancements.*
+import com.google.gson.JsonObject
+import miyucomics.hexical.Hexical
+import net.minecraft.advancement.criterion.AbstractCriterion
+import net.minecraft.advancement.criterion.AbstractCriterionConditions
 import net.minecraft.advancement.criterion.Criteria
+import net.minecraft.predicate.entity.AdvancementEntityPredicateDeserializer
+import net.minecraft.predicate.entity.EntityPredicate
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Identifier
 
 object HexicalAdvancements {
-	val AR: ARCriterion = Criteria.register(ARCriterion())
-	val DIY: DIYCriterion = Criteria.register(DIYCriterion())
-	val HALLUCINATE: HallucinateCriterion = Criteria.register(HallucinateCriterion())
-	val EDUCATE_GENIE: EducateGenieCriterion = Criteria.register(EducateGenieCriterion())
-	val RELOAD_LAMP: ReloadLampCriterion = Criteria.register(ReloadLampCriterion())
-	val USE_UP_LAMP: UseUpLampCriterion = Criteria.register(UseUpLampCriterion())
+	val AR: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("augmented_reality")))
+	val DIY: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("diy_conjuring")))
+	val HALLUCINATE: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("hallucinate")))
+	val EDUCATE_GENIE: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("educate_genie")))
+	val RELOAD_LAMP: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("reload_lamp")))
+	val USE_UP_LAMP: CriterionTemplate = Criteria.register(CriterionTemplate(Hexical.id("use_up_lamp")))
 
 	@JvmStatic
 	fun init() {
 	}
+}
+
+class CriterionTemplate(val data: Identifier) : AbstractCriterion<CriterionTemplate.Condition>() {
+	override fun conditionsFromJson(obj: JsonObject, playerPredicate: EntityPredicate.Extended, predicateDeserializer: AdvancementEntityPredicateDeserializer) = Condition()
+	fun trigger(player: ServerPlayerEntity?) = trigger(player) { true }
+	override fun getId() = data
+	inner class Condition : AbstractCriterionConditions(data, EntityPredicate.Extended.EMPTY)
 }
