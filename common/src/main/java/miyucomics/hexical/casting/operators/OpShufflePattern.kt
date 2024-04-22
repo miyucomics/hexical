96a1b5e8407140bcd4d08af66ce5fb9de5697a4e
miyucomics <nbriannyc@gmail.com>
2024-04-21 21:59:59 -0400

    Progress snapshot

--- common/src/main/java/miyucomics/hexical/casting/operators/OpShufflePattern.kt
+++ common/src/main/java/miyucomics/hexical/casting/operators/OpShufflePattern.kt
@@ -2,15 +2,11 @@ package miyucomics.hexical.casting.operators
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
-import at.petrak.hexcasting.api.spell.getDouble
 import at.petrak.hexcasting.api.spell.getInt
 import at.petrak.hexcasting.api.spell.getPattern
-import at.petrak.hexcasting.api.spell.iota.DoubleIota
 import at.petrak.hexcasting.api.spell.iota.Iota
-import at.petrak.hexcasting.api.spell.iota.NullIota
 import at.petrak.hexcasting.api.spell.iota.PatternIota
 import at.petrak.hexcasting.api.spell.math.EulerPathFinder
-import miyucomics.hexical.state.TelepathyData
 
 class OpShufflePattern : ConstMediaAction {
 	override val argc = 2
