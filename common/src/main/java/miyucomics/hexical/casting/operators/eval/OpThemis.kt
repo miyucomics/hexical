4b245649ee15e51a6b7bd99eda78ad3142c0486e
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-04-23 20:34:44 -0400

    Started work on making Themis use metaevals

--- common/src/main/java/miyucomics/hexical/casting/operators/eval/OpThemis.kt
+++ common/src/main/java/miyucomics/hexical/casting/operators/eval/OpThemis.kt
@@ -1,42 +1,64 @@
 package miyucomics.hexical.casting.operators.eval
 
-import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.Action
+import at.petrak.hexcasting.api.spell.OperationResult
+import at.petrak.hexcasting.api.spell.SpellList
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.casting.CastingHarness
-import at.petrak.hexcasting.api.spell.evaluatable
+import at.petrak.hexcasting.api.spell.casting.ResolvedPatternType
+import at.petrak.hexcasting.api.spell.casting.eval.FrameEvaluate
+import at.petrak.hexcasting.api.spell.casting.eval.FrameForEach
+import at.petrak.hexcasting.api.spell.casting.eval.FunctionalData
+import at.petrak.hexcasting.api.spell.casting.eval.SpellContinuation
 import at.petrak.hexcasting.api.spell.getList
-import at.petrak.hexcasting.api.spell.getPattern
-import at.petrak.hexcasting.api.spell.iota.DoubleIota
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.iota.ListIota
-import at.petrak.hexcasting.api.spell.iota.NullIota
-import at.petrak.hexcasting.api.spell.iota.PatternIota
-import miyucomics.hexical.casting.mishaps.ThemisMishap
+import at.petrak.hexcasting.api.spell.mishaps.MishapNotEnoughArgs
+import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
+import miyucomics.hexical.enums.InjectedGambit
+import miyucomics.hexical.interfaces.FrameForEachMinterface
 
-class OpThemis : ConstMediaAction {
-	override val argc = 2
-	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-		val toSort = args.getList(0, argc).toMutableList()
-		evaluatable(args[1], 1)
-		val executable: List<Iota> = when (args[1]) {
-			is ListIota -> args.getList(1, argc).toMutableList()
-			is PatternIota -> mutableListOf(PatternIota(args.getPattern(1, argc)))
-			else -> listOf(NullIota())
-		}
-		toSort.sortBy { judge(it, executable, ctx) }
-		return listOf(ListIota(toSort))
+object OpThemis : Action {
+	override fun operate(continuation: SpellContinuation, stack: MutableList<Iota>, ravenmind: Iota?, ctx: CastingContext): OperationResult {
+		if (stack.size < 2)
+			throw MishapNotEnoughArgs(1, 0)
+		val data = stack.getList(stack.lastIndex - 1)
+		val code = stack.getList(stack.lastIndex)
+		stack.removeLastOrNull()
+		stack.removeLastOrNull()
+		if (data.size() == 0)
+			return OperationResult(continuation, stack, ravenmind, listOf())
+		if (code.size() == 0)
+			return OperationResult(continuation, stack, ravenmind, listOf())
+		val frame = FrameForEach(data, code, null, mutableListOf())
+		(frame as FrameForEachMinterface).overwrite(InjectedGambit.THEMIS)
+		return OperationResult(continuation.pushFrame(frame), stack, ravenmind, listOf())
 	}
 
-	private fun judge(iota: Iota, judgement: List<Iota>, ctx: CastingContext): Double {
-		val tempCtx = CastingContext(ctx.caster, ctx.castingHand, CastingContext.CastSource.PACKAGED_HEX)
-		val harness = CastingHarness(tempCtx)
-		harness.stack = mutableListOf(iota)
-		harness.executeIotas(judgement, ctx.world)
-		if (harness.stack.size == 0)
-			throw ThemisMishap()
-		val top = harness.stack.last()
-		if (top !is DoubleIota)
-			throw ThemisMishap()
-		return top.double
+	fun breakDownwards(stack: List<Iota>, accumulator: MutableList<Iota>): Pair<Boolean, List<Iota>> {
+		val newStack = stack.toMutableList()
+		return true to newStack
+	}
+
+	fun evaluate(continuation: SpellContinuation, harness: CastingHarness, data: SpellList, code: SpellList, baseStack: List<Iota>?, accumulator: MutableList<Iota>): CastingHarness.CastResult {
+		val (stackTop, newContinuation) = if (data.nonEmpty) {
+			harness.ctx.incDepth()
+			val frame = FrameForEach(data.cdr, code, baseStack, accumulator)
+			(frame as FrameForEachMinterface).overwrite(InjectedGambit.THEMIS)
+			data.car to continuation.pushFrame(frame).pushFrame(FrameEvaluate(code, true))
+		} else {
+			ListIota(accumulator) to continuation
+		}
+
+		val newStack = baseStack.toMutableList()
+		newStack.add(stackTop)
+
+		return CastingHarness.CastResult(
+			newContinuation,
+			FunctionalData(newStack, 0, listOf(), false, harness.ravenmind),
+			ResolvedPatternType.EVALUATED,
+			listOf(),
+			HexEvalSounds.THOTH,
+		)
 	}
 }
