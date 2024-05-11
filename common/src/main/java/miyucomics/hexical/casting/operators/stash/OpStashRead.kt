449e136148fed52fb8e2d2672db53aabba2ffee2
Miyu Nguyen <nbriannyc@gmail.com>
2024-05-11 12:29:37 -0400

    Added library mishaps to stashing patterns

--- common/src/main/java/miyucomics/hexical/casting/operators/stash/OpStashRead.kt
+++ common/src/main/java/miyucomics/hexical/casting/operators/stash/OpStashRead.kt
@@ -5,7 +5,9 @@ import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.iota.NullIota
 import at.petrak.hexcasting.api.spell.math.HexPattern
+import at.petrak.hexcasting.api.spell.mishaps.MishapNoAkashicRecord
 import at.petrak.hexcasting.common.blocks.akashic.AkashicFloodfiller
+import at.petrak.hexcasting.common.blocks.akashic.BlockAkashicRecord
 import at.petrak.hexcasting.common.blocks.akashic.BlockEntityAkashicBookshelf
 import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
 import miyucomics.hexical.casting.mishaps.NoBoundLibraryMishap
@@ -19,6 +21,9 @@ class OpStashRead(private val key: HexPattern) : ConstMediaAction {
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val world = ctx.world
 		val record = PersistentStateHandler.getPlayerBoundLibrary(ctx.caster) ?: throw NoBoundLibraryMishap()
+		if (world.getBlockState(record).block !is BlockAkashicRecord)
+			throw MishapNoAkashicRecord(record)
+
 		val existingShelf = AkashicFloodfiller.floodFillFor(record, world) { pos: BlockPos?, _: BlockState?, world2: World -> world2.getBlockEntity(pos) is BlockEntityAkashicBookshelf && (world2.getBlockEntity(pos) as BlockEntityAkashicBookshelf).pattern == key }
 		if (existingShelf != null)
 			return listOf(HexIotaTypes.deserialize((world.getBlockEntity(existingShelf) as BlockEntityAkashicBookshelf).iotaTag!!, world))
