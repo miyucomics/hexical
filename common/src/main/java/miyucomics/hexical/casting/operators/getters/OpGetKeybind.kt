8693fda418059e5f1eec38cf124e60c6118d4b43
Miyu Nguyen <nbriannyc@gmail.com>
2024-05-14 20:56:35 -0400

    Many changes and tweaks

--- common/src/main/java/miyucomics/hexical/casting/operators/getters/OpGetKeybind.kt
+++ common/src/main/java/miyucomics/hexical/casting/operators/getters/OpGetKeybind.kt
@@ -3,14 +3,15 @@ package miyucomics.hexical.casting.operators.getters
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.BooleanIota
+import at.petrak.hexcasting.api.spell.iota.DoubleIota
 import at.petrak.hexcasting.api.spell.iota.Iota
 import miyucomics.hexical.state.KeybindData
 
 class OpGetKeybind(private val key: String) : ConstMediaAction {
 	override val argc = 0
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-		if (!KeybindData.lookup.containsKey(ctx.caster.uuid))
-			return listOf(BooleanIota(false))
-		return listOf(BooleanIota(KeybindData.lookup[ctx.caster.uuid]!!.getOrDefault(key, false)))
+		if (!KeybindData.active.containsKey(ctx.caster.uuid))
+			return listOf(DoubleIota(0.0))
+		return listOf(DoubleIota(KeybindData.duration[ctx.caster.uuid]!!.getOrDefault(key, 0.0).toDouble()))
 	}
 }
