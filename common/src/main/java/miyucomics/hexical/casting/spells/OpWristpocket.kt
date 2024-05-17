3ac481e5a8a4a48500691b367db6db0699ee0451
miyucomics <nbriannyc@gmail.com>
2024-05-16 21:33:08 -0400

    Wristpocket

--- common/src/main/java/miyucomics/hexical/casting/spells/OpWristpocket.kt
+++ common/src/main/java/miyucomics/hexical/casting/spells/OpWristpocket.kt
@@ -4,20 +4,30 @@ import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
+import miyucomics.hexical.interfaces.PlayerEntityMinterface
+import net.minecraft.item.AirBlockItem
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.util.Hand
 import net.minecraft.util.math.Vec3d
 
 class OpWristpocket : SpellAction {
 	override val argc = 0
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
-		// make this cost three dust if adding the item
-		// otherwise make it free
 		return Triple(Spell(ctx.otherHand), MediaConstants.DUST_UNIT * 3, listOf())
 	}
 
 	private data class Spell(val hand: Hand) : RenderedSpell {
 		override fun cast(ctx: CastingContext) {
-			// disappear the item
+			print(ctx.caster.getStackInHand(hand))
+			val minterface = (ctx.caster as PlayerEntityMinterface)
+			if (minterface.wristpocketItem() == ItemStack.EMPTY || minterface.wristpocketItem().item == Items.AIR) {
+				(ctx.caster as PlayerEntityMinterface).stashWristpocket(ctx.caster.getStackInHand(hand))
+				ctx.caster.setStackInHand(hand, ItemStack.EMPTY)
+			} else {
+				ctx.caster.setStackInHand(hand, (ctx.caster as PlayerEntityMinterface).wristpocketItem())
+				minterface.stashWristpocket(ItemStack.EMPTY)
+			}
 		}
 	}
 }
