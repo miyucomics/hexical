285be826e872db16fe4963aa29e1e64d3f4d4d0a
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-05-17 17:47:37 -0400

    Made wristpocket server-state

--- common/src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
+++ common/src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
@@ -14,27 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin implements PlayerEntityMinterface {
 	@Unique
-	private static final String WRISTPOCKET_PATH = "hexical:wristpocket";
-	@Unique
 	private boolean hexical$archLampCastedThisTick = false;
-	@Unique
-	private ItemStack hexical$wristpocket = ItemStack.EMPTY;
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	void tick(CallbackInfo ci) {
 		hexical$archLampCastedThisTick = false;
-	}
-
-	@Inject(method = "writeCustomDataToNbt", at = @At("HEAD"))
-	void writeData(NbtCompound nbt, CallbackInfo ci) {
-		NbtCompound tag = new NbtCompound();
-		hexical$wristpocket.writeNbt(tag);
-		nbt.put(WRISTPOCKET_PATH, tag);
-	}
-
-	@Inject(method = "readCustomDataFromNbt", at = @At("HEAD"))
-	void readData(NbtCompound nbt, CallbackInfo ci) {
-		hexical$wristpocket = ItemStack.fromNbt(nbt.getCompound(WRISTPOCKET_PATH));
 	}
 
 	public boolean getArchLampCastedThisTick() {
@@ -44,16 +28,5 @@ public class PlayerEntityMixin implements PlayerEntityMinterface {
 	@Override
 	public void lampCastedThisTick() {
 		hexical$archLampCastedThisTick = true;
-	}
-
-	@Override
-	public void stashWristpocket(@Nullable ItemStack stack) {
-		hexical$wristpocket = stack;
-	}
-
-	@Nullable
-	@Override
-	public ItemStack wristpocketItem() {
-		return hexical$wristpocket;
 	}
 }
