3cb27dc4bb3b881f5c6ea4cb5836aa1a6d323aa2
Miyu Nguyen <nbriannyc@gmail.com>
2024-05-16 21:33:08 -0400

    Wristpocket

--- common/src/main/java/miyucomics/hexical/interfaces/PlayerEntityMinterface.kt
+++ common/src/main/java/miyucomics/hexical/interfaces/PlayerEntityMinterface.kt
@@ -1,6 +1,10 @@
 package miyucomics.hexical.interfaces
 
+import net.minecraft.item.ItemStack
+
 interface PlayerEntityMinterface {
 	fun getArchLampCastedThisTick(): Boolean
 	fun lampCastedThisTick()
+	fun stashWristpocket(stack: ItemStack)
+	fun wristpocketItem(): ItemStack
 }
