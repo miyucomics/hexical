cf0e37453b889ae8ae8f825ae9ba0361ee430aad
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-05-17 22:33:11 -0400

    Mage hand

---
+++ common/src/main/java/miyucomics/hexical/casting/operators/getters/OpGetWristpocket.kt
@@ -0,0 +1,30 @@
+package miyucomics.hexical.casting.operators.getters
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.iota.DoubleIota
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.iota.NullIota
+import miyucomics.hexical.iota.IdentifierIota
+import miyucomics.hexical.state.PersistentStateHandler
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.util.registry.Registry
+
+class OpGetWristpocket(private val mode: Int) : ConstMediaAction {
+	override val argc = 0
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		val wristpocket = PersistentStateHandler.wristpocketItem(ctx.caster)
+		if (wristpocket.isOf(Items.AIR) || wristpocket == ItemStack.EMPTY)
+			return listOf(when (mode) {
+				0 -> NullIota()
+				1 -> DoubleIota(0.0)
+				else -> NullIota()
+			})
+		return listOf(when (mode) {
+			0 -> IdentifierIota(Registry.ITEM.getId(wristpocket.item))
+			1 -> DoubleIota(wristpocket.count.toDouble())
+			else -> NullIota()
+		})
+	}
+}
