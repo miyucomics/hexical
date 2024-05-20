54973f4cd2ac88106820e0a5cb300c760a4e2fe7
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-05-20 18:13:23 -0400

    Dye iota

--- common/src/main/java/miyucomics/hexical/casting/operators/OpGetDye.kt
+++ common/src/main/java/miyucomics/hexical/casting/operators/OpGetDye.kt
@@ -6,37 +6,258 @@ import at.petrak.hexcasting.api.spell.getBlockPos
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.iota.NullIota
 import miyucomics.hexical.iota.DyeIota
+import miyucomics.hexical.registry.HexicalTags
+import net.minecraft.block.Blocks
+import net.minecraft.tag.BlockTags
 import net.minecraft.util.DyeColor
-import net.minecraft.util.registry.Registry
 
 class OpGetDye : ConstMediaAction {
-	private val dyes = mapOf(
-		"white" to DyeColor.WHITE,
-		"orange" to DyeColor.ORANGE,
-		"magenta" to DyeColor.MAGENTA,
-		"light_blue" to DyeColor.LIGHT_BLUE,
-		"yellow" to DyeColor.YELLOW,
-		"lime" to DyeColor.LIME,
-		"pink" to DyeColor.PINK,
-		"gray" to DyeColor.GRAY,
-		"light_gray" to DyeColor.LIGHT_GRAY,
-		"cyan" to DyeColor.CYAN,
-		"purple" to DyeColor.PURPLE,
-		"blue" to DyeColor.BLUE,
-		"brown" to DyeColor.BROWN,
-		"green" to DyeColor.GREEN,
-		"red" to DyeColor.RED,
-		"black" to DyeColor.BLACK
+	private val beds = mapOf(
+		Blocks.WHITE_BED to DyeColor.WHITE,
+		Blocks.ORANGE_BED to DyeColor.ORANGE,
+		Blocks.MAGENTA_BED to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_BED to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_BED to DyeColor.YELLOW,
+		Blocks.LIME_BED to DyeColor.LIME,
+		Blocks.PINK_BED to DyeColor.PINK,
+		Blocks.GRAY_BED to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_BED to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_BED to DyeColor.CYAN,
+		Blocks.PURPLE_BED to DyeColor.PURPLE,
+		Blocks.BLUE_BED to DyeColor.BLUE,
+		Blocks.BROWN_BED to DyeColor.BROWN,
+		Blocks.GREEN_BED to DyeColor.GREEN,
+		Blocks.RED_BED to DyeColor.RED,
+		Blocks.BLACK_BED to DyeColor.BLACK
+	)
+	private val cakeCandles = mapOf(
+		Blocks.WHITE_CANDLE_CAKE to DyeColor.WHITE,
+		Blocks.ORANGE_CANDLE_CAKE to DyeColor.ORANGE,
+		Blocks.MAGENTA_CANDLE_CAKE to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_CANDLE_CAKE to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_CANDLE_CAKE to DyeColor.YELLOW,
+		Blocks.LIME_CANDLE_CAKE to DyeColor.LIME,
+		Blocks.PINK_CANDLE_CAKE to DyeColor.PINK,
+		Blocks.GRAY_CANDLE_CAKE to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_CANDLE_CAKE to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_CANDLE_CAKE to DyeColor.CYAN,
+		Blocks.PURPLE_CANDLE_CAKE to DyeColor.PURPLE,
+		Blocks.BLUE_CANDLE_CAKE to DyeColor.BLUE,
+		Blocks.BROWN_CANDLE_CAKE to DyeColor.BROWN,
+		Blocks.GREEN_CANDLE_CAKE to DyeColor.GREEN,
+		Blocks.RED_CANDLE_CAKE to DyeColor.RED,
+		Blocks.BLACK_CANDLE_CAKE to DyeColor.BLACK
+	)
+	private val candles = mapOf(
+		Blocks.WHITE_CANDLE to DyeColor.WHITE,
+		Blocks.ORANGE_CANDLE to DyeColor.ORANGE,
+		Blocks.MAGENTA_CANDLE to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_CANDLE to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_CANDLE to DyeColor.YELLOW,
+		Blocks.LIME_CANDLE to DyeColor.LIME,
+		Blocks.PINK_CANDLE to DyeColor.PINK,
+		Blocks.GRAY_CANDLE to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_CANDLE to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_CANDLE to DyeColor.CYAN,
+		Blocks.PURPLE_CANDLE to DyeColor.PURPLE,
+		Blocks.BLUE_CANDLE to DyeColor.BLUE,
+		Blocks.BROWN_CANDLE to DyeColor.BROWN,
+		Blocks.GREEN_CANDLE to DyeColor.GREEN,
+		Blocks.RED_CANDLE to DyeColor.RED,
+		Blocks.BLACK_CANDLE to DyeColor.BLACK
+	)
+	private val carpets = mapOf(
+		Blocks.WHITE_CARPET to DyeColor.WHITE,
+		Blocks.ORANGE_CARPET to DyeColor.ORANGE,
+		Blocks.MAGENTA_CARPET to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_CARPET to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_CARPET to DyeColor.YELLOW,
+		Blocks.LIME_CARPET to DyeColor.LIME,
+		Blocks.PINK_CARPET to DyeColor.PINK,
+		Blocks.GRAY_CARPET to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_CARPET to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_CARPET to DyeColor.CYAN,
+		Blocks.PURPLE_CARPET to DyeColor.PURPLE,
+		Blocks.BLUE_CARPET to DyeColor.BLUE,
+		Blocks.BROWN_CARPET to DyeColor.BROWN,
+		Blocks.GREEN_CARPET to DyeColor.GREEN,
+		Blocks.RED_CARPET to DyeColor.RED,
+		Blocks.BLACK_CARPET to DyeColor.BLACK
+	)
+	private val concretes = mapOf(
+		Blocks.WHITE_CONCRETE to DyeColor.WHITE,
+		Blocks.ORANGE_CONCRETE to DyeColor.ORANGE,
+		Blocks.MAGENTA_CONCRETE to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_CONCRETE to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_CONCRETE to DyeColor.YELLOW,
+		Blocks.LIME_CONCRETE to DyeColor.LIME,
+		Blocks.PINK_CONCRETE to DyeColor.PINK,
+		Blocks.GRAY_CONCRETE to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_CONCRETE to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_CONCRETE to DyeColor.CYAN,
+		Blocks.PURPLE_CONCRETE to DyeColor.PURPLE,
+		Blocks.BLUE_CONCRETE to DyeColor.BLUE,
+		Blocks.BROWN_CONCRETE to DyeColor.BROWN,
+		Blocks.GREEN_CONCRETE to DyeColor.GREEN,
+		Blocks.RED_CONCRETE to DyeColor.RED,
+		Blocks.BLACK_CONCRETE to DyeColor.BLACK
+	)
+	private val concretePowders = mapOf(
+		Blocks.WHITE_CONCRETE_POWDER to DyeColor.WHITE,
+		Blocks.ORANGE_CONCRETE_POWDER to DyeColor.ORANGE,
+		Blocks.MAGENTA_CONCRETE_POWDER to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_CONCRETE_POWDER to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_CONCRETE_POWDER to DyeColor.YELLOW,
+		Blocks.LIME_CONCRETE_POWDER to DyeColor.LIME,
+		Blocks.PINK_CONCRETE_POWDER to DyeColor.PINK,
+		Blocks.GRAY_CONCRETE_POWDER to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_CONCRETE_POWDER to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_CONCRETE_POWDER to DyeColor.CYAN,
+		Blocks.PURPLE_CONCRETE_POWDER to DyeColor.PURPLE,
+		Blocks.BLUE_CONCRETE_POWDER to DyeColor.BLUE,
+		Blocks.BROWN_CONCRETE_POWDER to DyeColor.BROWN,
+		Blocks.GREEN_CONCRETE_POWDER to DyeColor.GREEN,
+		Blocks.RED_CONCRETE_POWDER to DyeColor.RED,
+		Blocks.BLACK_CONCRETE_POWDER to DyeColor.BLACK
+	)
+	private val glazedTerracotta = mapOf(
+		Blocks.WHITE_GLAZED_TERRACOTTA to DyeColor.WHITE,
+		Blocks.ORANGE_GLAZED_TERRACOTTA to DyeColor.ORANGE,
+		Blocks.MAGENTA_GLAZED_TERRACOTTA to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_GLAZED_TERRACOTTA to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_GLAZED_TERRACOTTA to DyeColor.YELLOW,
+		Blocks.LIME_GLAZED_TERRACOTTA to DyeColor.LIME,
+		Blocks.PINK_GLAZED_TERRACOTTA to DyeColor.PINK,
+		Blocks.GRAY_GLAZED_TERRACOTTA to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_GLAZED_TERRACOTTA to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_GLAZED_TERRACOTTA to DyeColor.CYAN,
+		Blocks.PURPLE_GLAZED_TERRACOTTA to DyeColor.PURPLE,
+		Blocks.BLUE_GLAZED_TERRACOTTA to DyeColor.BLUE,
+		Blocks.BROWN_GLAZED_TERRACOTTA to DyeColor.BROWN,
+		Blocks.GREEN_GLAZED_TERRACOTTA to DyeColor.GREEN,
+		Blocks.RED_GLAZED_TERRACOTTA to DyeColor.RED,
+		Blocks.BLACK_GLAZED_TERRACOTTA to DyeColor.BLACK
+	)
+	private val shulkerBoxes = mapOf(
+		Blocks.WHITE_SHULKER_BOX to DyeColor.WHITE,
+		Blocks.ORANGE_SHULKER_BOX to DyeColor.ORANGE,
+		Blocks.MAGENTA_SHULKER_BOX to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_SHULKER_BOX to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_SHULKER_BOX to DyeColor.YELLOW,
+		Blocks.LIME_SHULKER_BOX to DyeColor.LIME,
+		Blocks.PINK_SHULKER_BOX to DyeColor.PINK,
+		Blocks.GRAY_SHULKER_BOX to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_SHULKER_BOX to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_SHULKER_BOX to DyeColor.CYAN,
+		Blocks.PURPLE_SHULKER_BOX to DyeColor.PURPLE,
+		Blocks.BLUE_SHULKER_BOX to DyeColor.BLUE,
+		Blocks.BROWN_SHULKER_BOX to DyeColor.BROWN,
+		Blocks.GREEN_SHULKER_BOX to DyeColor.GREEN,
+		Blocks.RED_SHULKER_BOX to DyeColor.RED,
+		Blocks.BLACK_SHULKER_BOX to DyeColor.BLACK
+	)
+	private val stainedGlass = mapOf(
+		Blocks.WHITE_STAINED_GLASS to DyeColor.WHITE,
+		Blocks.ORANGE_STAINED_GLASS to DyeColor.ORANGE,
+		Blocks.MAGENTA_STAINED_GLASS to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_STAINED_GLASS to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_STAINED_GLASS to DyeColor.YELLOW,
+		Blocks.LIME_STAINED_GLASS to DyeColor.LIME,
+		Blocks.PINK_STAINED_GLASS to DyeColor.PINK,
+		Blocks.GRAY_STAINED_GLASS to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_STAINED_GLASS to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_STAINED_GLASS to DyeColor.CYAN,
+		Blocks.PURPLE_STAINED_GLASS to DyeColor.PURPLE,
+		Blocks.BLUE_STAINED_GLASS to DyeColor.BLUE,
+		Blocks.BROWN_STAINED_GLASS to DyeColor.BROWN,
+		Blocks.GREEN_STAINED_GLASS to DyeColor.GREEN,
+		Blocks.RED_STAINED_GLASS to DyeColor.RED,
+		Blocks.BLACK_STAINED_GLASS to DyeColor.BLACK
+	)
+	private val stainedGlassPanes = mapOf(
+		Blocks.WHITE_STAINED_GLASS_PANE to DyeColor.WHITE,
+		Blocks.ORANGE_STAINED_GLASS_PANE to DyeColor.ORANGE,
+		Blocks.MAGENTA_STAINED_GLASS_PANE to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_STAINED_GLASS_PANE to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_STAINED_GLASS_PANE to DyeColor.YELLOW,
+		Blocks.LIME_STAINED_GLASS_PANE to DyeColor.LIME,
+		Blocks.PINK_STAINED_GLASS_PANE to DyeColor.PINK,
+		Blocks.GRAY_STAINED_GLASS_PANE to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_STAINED_GLASS_PANE to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_STAINED_GLASS_PANE to DyeColor.CYAN,
+		Blocks.PURPLE_STAINED_GLASS_PANE to DyeColor.PURPLE,
+		Blocks.BLUE_STAINED_GLASS_PANE to DyeColor.BLUE,
+		Blocks.BROWN_STAINED_GLASS_PANE to DyeColor.BROWN,
+		Blocks.GREEN_STAINED_GLASS_PANE to DyeColor.GREEN,
+		Blocks.RED_STAINED_GLASS_PANE to DyeColor.RED,
+		Blocks.BLACK_STAINED_GLASS_PANE to DyeColor.BLACK
+	)
+	private val terracotta = mapOf(
+		Blocks.WHITE_TERRACOTTA to DyeColor.WHITE,
+		Blocks.ORANGE_TERRACOTTA to DyeColor.ORANGE,
+		Blocks.MAGENTA_TERRACOTTA to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_TERRACOTTA to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_TERRACOTTA to DyeColor.YELLOW,
+		Blocks.LIME_TERRACOTTA to DyeColor.LIME,
+		Blocks.PINK_TERRACOTTA to DyeColor.PINK,
+		Blocks.GRAY_TERRACOTTA to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_TERRACOTTA to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_TERRACOTTA to DyeColor.CYAN,
+		Blocks.PURPLE_TERRACOTTA to DyeColor.PURPLE,
+		Blocks.BLUE_TERRACOTTA to DyeColor.BLUE,
+		Blocks.BROWN_TERRACOTTA to DyeColor.BROWN,
+		Blocks.GREEN_TERRACOTTA to DyeColor.GREEN,
+		Blocks.RED_TERRACOTTA to DyeColor.RED,
+		Blocks.BLACK_TERRACOTTA to DyeColor.BLACK
+	)
+	private val wools = mapOf(
+		Blocks.WHITE_WOOL to DyeColor.WHITE,
+		Blocks.ORANGE_WOOL to DyeColor.ORANGE,
+		Blocks.MAGENTA_WOOL to DyeColor.MAGENTA,
+		Blocks.LIGHT_BLUE_WOOL to DyeColor.LIGHT_BLUE,
+		Blocks.YELLOW_WOOL to DyeColor.YELLOW,
+		Blocks.LIME_WOOL to DyeColor.LIME,
+		Blocks.PINK_WOOL to DyeColor.PINK,
+		Blocks.GRAY_WOOL to DyeColor.GRAY,
+		Blocks.LIGHT_GRAY_WOOL to DyeColor.LIGHT_GRAY,
+		Blocks.CYAN_WOOL to DyeColor.CYAN,
+		Blocks.PURPLE_WOOL to DyeColor.PURPLE,
+		Blocks.BLUE_WOOL to DyeColor.BLUE,
+		Blocks.BROWN_WOOL to DyeColor.BROWN,
+		Blocks.GREEN_WOOL to DyeColor.GREEN,
+		Blocks.RED_WOOL to DyeColor.RED,
+		Blocks.BLACK_WOOL to DyeColor.BLACK
 	)
 
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val position = args.getBlockPos(0, argc)
 		ctx.assertVecInRange(position)
-		val name = Registry.BLOCK.getId(ctx.world.getBlockState(position).block).path
-		for (dye in dyes.keys)
-			if (name.startsWith(dye))
-				return listOf(DyeIota(dyes[dye]!!))
+		val state = ctx.world.getBlockState(position)
+		if (state.isIn(BlockTags.BEDS))
+			return listOf(DyeIota(beds[state.block]!!))
+		if (state.isIn(BlockTags.CANDLE_CAKES))
+			return listOf(DyeIota(cakeCandles[state.block]!!))
+		if (state.isIn(BlockTags.CANDLES))
+			return listOf(DyeIota(candles[state.block]!!))
+		if (state.isIn(HexicalTags.CONCRETES))
+			return listOf(DyeIota(concretes[state.block]!!))
+		if (state.isIn(HexicalTags.CONCRETE_POWDERS))
+			return listOf(DyeIota(concretePowders[state.block]!!))
+		if (state.isIn(HexicalTags.GLAZED_TERRACOTTA))
+			return listOf(DyeIota(glazedTerracotta[state.block]!!))
+		if (state.isIn(BlockTags.SHULKER_BOXES))
+			return listOf(DyeIota(shulkerBoxes[state.block]!!))
+		if (state.isIn(HexicalTags.STAINED_GLASS))
+			return listOf(DyeIota(stainedGlass[state.block]!!))
+		if (state.isIn(HexicalTags.STAINED_GLASS_PANES))
+			return listOf(DyeIota(stainedGlassPanes[state.block]!!))
+		if (state.isIn(HexicalTags.TERRACOTTA))
+			return listOf(DyeIota(terracotta[state.block]!!))
+		if (state.isIn(BlockTags.WOOL_CARPETS))
+			return listOf(DyeIota(carpets[state.block]!!))
+		if (state.isIn(BlockTags.WOOL))
+			return listOf(DyeIota(wools[state.block]!!))
 		return listOf(NullIota())
 	}
 }
--- common/src/main/java/miyucomics/hexical/iota/DyeIota.kt
+++ common/src/main/java/miyucomics/hexical/iota/DyeIota.kt
@@ -9,15 +9,14 @@ import miyucomics.hexical.registry.HexicalIota
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtElement
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.text.Style
 import net.minecraft.text.Text
 import net.minecraft.util.DyeColor
-import net.minecraft.util.Formatting
-import net.minecraft.util.Identifier
 import java.util.*
 
-class DyeIota(color: DyeColor) : Iota(HexicalIota.DYE_IOTA, color.name) {
+class DyeIota(color: DyeColor) : Iota(HexicalIota.DYE_IOTA, color) {
 	override fun isTruthy() = true
-	val dye: DyeColor = DyeColor.byName(this.payload as String, DyeColor.BLACK)!!
+	val dye: DyeColor = this.payload as DyeColor
 	override fun toleratesOther(that: Iota) = (typesMatch(this, that) && that is DyeIota) && this.dye == that.dye
 
 	override fun serialize(): NbtElement {
@@ -27,10 +26,32 @@ class DyeIota(color: DyeColor) : Iota(HexicalIota.DYE_IOTA, color.name) {
 	}
 
 	companion object {
+		private val MAP = mapOf(
+			"white" to 0xF9FFFE,
+			"orange" to 16351261,
+			"magenta" to 13061821,
+			"light_blue" to 3847130,
+			"yellow" to 16701501,
+			"lime" to 8439583,
+			"pink" to 15961002,
+			"gray" to 4673362,
+			"light_gray" to 0x9D9D97,
+			"cyan" to 1481884,
+			"purple" to 8991416,
+			"blue" to 3949738,
+			"brown" to 8606770,
+			"green" to 6192150,
+			"red" to 11546150,
+			"black" to 0x1D1D21
+		)
+
 		var TYPE: IotaType<DyeIota> = object : IotaType<DyeIota>() {
-			override fun color() = 0xff_e6c24c.toInt()
-			override fun deserialize(tag: NbtElement?, world: ServerWorld?) = DyeIota(DyeColor.byName((tag!! as NbtCompound).getString("color"), DyeColor.BLACK)!!)
-			override fun display(tag: NbtElement) = Text.literal((tag as NbtCompound).getString("color").replace("_", " "))
+			override fun color() = 0xff_ffffff.toInt()
+			override fun deserialize(tag: NbtElement?, world: ServerWorld?) = DyeIota(DyeColor.byName((tag!! as NbtCompound).getString("color").lowercase(Locale.getDefault()), DyeColor.BLACK)!!)
+			override fun display(tag: NbtElement): Text {
+				val color = (tag as NbtCompound).getString("color")
+				return Text.literal(color.replace("_", " ")).styledWith(Style.EMPTY.withColor(MAP[color.lowercase(Locale.getDefault())]!!))
+			}
 		}
 	}
 }
