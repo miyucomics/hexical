637be385b5b0260f399b26072fe51fd1c7a7ae4d
miyucomics <nbriannyc@gmail.com>
2024-05-25 17:24:56 -0400

    More tweaks

---
+++ src/main/java/miyucomics/hexical/casting/operators/conjured_staff/OpWriteStaff.kt
@@ -0,0 +1,26 @@
+package miyucomics.hexical.casting.operators.conjured_staff
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapOthersName
+import at.petrak.hexcasting.api.utils.putCompound
+import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
+import miyucomics.hexical.casting.mishaps.NeedsSourceMishap
+import miyucomics.hexical.enums.SpecializedSource
+import miyucomics.hexical.interfaces.CastingContextMinterface
+
+class OpWriteStaff : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		if ((ctx as CastingContextMinterface).getSpecializedSource() != SpecializedSource.CONJURED_STAFF)
+			throw NeedsSourceMishap("conjured_staff")
+		val iota = args[0]
+		val trueName = MishapOthersName.getTrueNameFromDatum(iota, ctx.caster)
+		if (trueName != null)
+			throw MishapOthersName(trueName)
+		val nbt = ctx.caster.getStackInHand(ctx.castingHand).orCreateNbt
+		nbt.putCompound("storage", HexIotaTypes.serialize(iota))
+		return listOf()
+	}
+}
