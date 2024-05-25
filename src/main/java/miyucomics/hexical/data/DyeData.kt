142a649cbed34651d01be1c411a719fd55fb4624
miyucomics <nbriannyc@gmail.com>
2024-05-25 14:14:34 -0400

    Made dye spell data-driven

---
+++ src/main/java/miyucomics/hexical/data/DyeData.kt
@@ -0,0 +1,26 @@
+package miyucomics.hexical.data
+
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.util.DyeColor
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+import java.util.*
+import kotlin.collections.HashMap
+
+object DyeData {
+	val blocks = HashMap<String, DyeColor>()
+	val families = HashMap<String, EnumMap<DyeColor, String>>()
+
+	fun getDye(block: Block): DyeColor? {
+		return blocks.getOrDefault(Registry.BLOCK.getId(block).toString(), null)
+	}
+
+	fun getNewBlock(block: Block, dye: DyeColor): BlockState {
+		families.forEach { (_, family) ->
+			if (family.containsValue(Registry.BLOCK.getId(block).toString()))
+				return Registry.BLOCK.get(Identifier(family[dye])).defaultState
+		}
+		return block.defaultState
+	}
+}
