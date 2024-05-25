0986669d6e6d06fbd15abfaf5b6e28c8c3565f8f
miyucomics <nbriannyc@gmail.com>
2024-05-24 21:10:29 -0400

    Small work

--- common/src/main/java/miyucomics/hexical/casting/spells/OpDye.kt
+++ common/src/main/java/miyucomics/hexical/casting/spells/OpDye.kt
@@ -5,15 +5,12 @@ import at.petrak.hexcasting.api.spell.*
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
 import miyucomics.hexical.iota.getDye
-import net.minecraft.block.BedBlock
+import net.minecraft.entity.mob.ShulkerEntity
+import net.minecraft.entity.passive.SheepEntity
 import net.minecraft.util.DyeColor
-import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Vec3d
-import net.minecraft.util.registry.Registry
 
 class OpDye : SpellAction {
-
 	override val argc = 2
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
 		val position = args.getBlockPos(0, argc)
@@ -22,16 +19,20 @@ class OpDye : SpellAction {
 	}
 
 	private data class BlockSpell(val position: BlockPos, val dye: DyeColor) : RenderedSpell {
-		private val suffixes = listOf("shulker_box", "bed", "stained_glass", "stained_glass_pane", "candle", "glazed_terracotta", "terracotta", "wool", "carpet", "concrete", "concrete_powder", "candle_cake")
-
 		override fun cast(ctx: CastingContext) {
-			val name = Registry.BLOCK.getId(ctx.world.getBlockState(position).block)
-			for (suffix in suffixes) {
-				if (name.path.endsWith(suffix)) {
-					ctx.world.setBlockState(position, Registry.BLOCK.get(Identifier(name.namespace, dye.name + "_" + suffix)).defaultState)
-					return
-				}
-			}
+
+		}
+	}
+
+	private data class SheepSpell(val sheep: SheepEntity, val dye: DyeColor) : RenderedSpell {
+		override fun cast(ctx: CastingContext) {
+			sheep.color = dye
+		}
+	}
+
+	private data class ShulkerSpell(val shulker: ShulkerEntity, val dye: DyeColor) : RenderedSpell {
+		override fun cast(ctx: CastingContext) {
+
 		}
 	}
 }
