5173f533be54f2407a3b5e33aace4ce948465740
Miyu Nguyen <nbriannyc@gmail.com>
2024-06-02 20:48:29 -0400

    Data driven dyed items

--- src/main/java/miyucomics/hexical/data/DyeData.kt
+++ src/main/java/miyucomics/hexical/data/DyeData.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.item.Item
 import net.minecraft.util.DyeColor
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -13,35 +14,59 @@ import java.util.*
 import kotlin.collections.HashMap
 
 object DyeData {
-	private val blocks = HashMap<String, DyeColor>()
-	private val families = HashMap<String, EnumMap<DyeColor, String>>()
+	private val flatBlockLookup = HashMap<String, DyeColor?>()
+	private val flatItemLookup = HashMap<String, DyeColor?>()
+	private val blockFamilies = HashMap<String, MutableMap<DyeColor?, String>>()
+	private val itemFamilies = HashMap<String, MutableMap<DyeColor?, String>>()
 
-	fun isDyeable(block: Block): Boolean {
-		return blocks.containsKey(Registry.BLOCK.getId(block).toString())
-	}
+	fun isDyeable(block: Block): Boolean { return flatBlockLookup.containsKey(Registry.BLOCK.getId(block).toString()) }
+	fun isDyeable(item: Item): Boolean { return flatItemLookup.containsKey(Registry.ITEM.getId(item).toString()) }
+	fun getDye(block: Block): DyeColor? { return flatBlockLookup.getOrDefault(Registry.BLOCK.getId(block).toString(), null) }
+	fun getDye(item: Item): DyeColor? { return flatItemLookup.getOrDefault(Registry.ITEM.getId(item).toString(), null) }
 
-	fun getDye(block: Block): DyeColor? {
-		return blocks.getOrDefault(Registry.BLOCK.getId(block).toString(), null)
-	}
-
-	fun getNewBlock(block: Block, dye: DyeColor): BlockState {
-		families.forEach { (_, family) ->
-			if (family.containsValue(Registry.BLOCK.getId(block).toString()))
+	fun getNewBlock(block: Block, dye: DyeColor?): BlockState {
+		blockFamilies.forEach { (_, family) ->
+			if (family.containsValue(Registry.BLOCK.getId(block).toString()) && family.containsKey(dye))
 				return Registry.BLOCK.get(Identifier(family[dye])).defaultState
 		}
 		return block.defaultState
 	}
+	fun getNewItem(item: Item, dye: DyeColor?): Item {
+		itemFamilies.forEach { (_, family) ->
+			if (family.containsValue(Registry.ITEM.getId(item).toString()) && family.containsKey(dye))
+				return Registry.ITEM.get(Identifier(family[dye]))
+		}
+		return item
+	}
 
 	fun loadData(stream: InputStream, name: String) {
 		val json = JsonParser.parseReader(InputStreamReader(stream, "UTF-8")) as JsonObject
-		json.keySet().forEach { familyKey ->
-			val family = json.get(familyKey) as JsonObject
-			family.keySet().forEach { block ->
-				val dye = DyeColor.byName(family.get(block).asString, DyeColor.WHITE)!!
-				blocks[block] = dye
-				if (!families.containsKey(familyKey))
-					families[familyKey] = EnumMap(DyeColor::class.java)
-				families[familyKey]!![dye] = block
+		when (name) {
+			"dyes/blocks.json" -> {
+				json.keySet().forEach { familyKey ->
+					val family = json.get(familyKey) as JsonObject
+					family.keySet().forEach { block ->
+						val raw = family.get(block).asString
+						val dye = if (raw == "uncolored") null else DyeColor.byName(family.get(block).asString, DyeColor.WHITE)!!
+						flatBlockLookup[block] = dye
+						if (!blockFamilies.containsKey(familyKey))
+							blockFamilies[familyKey] = mutableMapOf()
+						blockFamilies[familyKey]!![dye] = block
+					}
+				}
+			}
+			"dyes/items.json" -> {
+				json.keySet().forEach { familyKey ->
+					val family = json.get(familyKey) as JsonObject
+					family.keySet().forEach { item ->
+						val raw = family.get(item).asString
+						val dye = if (raw == "uncolored") null else DyeColor.byName(family.get(item).asString, DyeColor.WHITE)!!
+						flatItemLookup[item] = dye
+						if (!itemFamilies.containsKey(familyKey))
+							itemFamilies[familyKey] = mutableMapOf()
+						itemFamilies[familyKey]!![dye] = item
+					}
+				}
 			}
 		}
 	}
