f1e024a55cf34308cdc7c92d7221cdf7e9924c7a
Miyu Nguyen <nbriannyc@gmail.com>
2024-06-22 18:28:34 -0400

    Proper input interception

--- src/main/java/miyucomics/hexical/mixin/MouseMixin.java
+++ src/main/java/miyucomics/hexical/mixin/MouseMixin.java
@@ -2,13 +2,19 @@ package miyucomics.hexical.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import miyucomics.hexical.client.FakeCameraEntity;
+import miyucomics.hexical.interfaces.MinecraftClientMinterface;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.network.ClientPlayerEntity;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static miyucomics.hexical.items.ConjuredStaffItemKt.getConjuredStaff;
 
 @Mixin(Mouse.class)
 public class MouseMixin {
@@ -21,5 +27,22 @@ public class MouseMixin {
 		if (client.getCameraEntity() instanceof FakeCameraEntity camera)
 			return camera;
 		return original;
+	}
+
+	@Inject(method = "onMouseButton", at = @At("HEAD"), cancellable = true)
+	private void onMouseButton(long window, int button, int action, int mods, CallbackInfo ci) {
+		if (this.client.currentScreen != null || this.client.getOverlay() != null)
+			return;
+		if (client.player == null || client.player.isSpectator())
+			return;
+		if (getConjuredStaff(client.player) != null && action == 1) {
+			if (button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
+				((MinecraftClientMinterface) client).leftClick();
+				ci.cancel();
+			} else if (button == GLFW.GLFW_MOUSE_BUTTON_RIGHT) {
+				((MinecraftClientMinterface) client).rightClick();
+				ci.cancel();
+			}
+		}
 	}
 }
