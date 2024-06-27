ebd88d2f562748512317a1050030044ac1ece9a6
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-06-27 17:36:25 -0400

    Removed flecks for now

--- src/main/java/miyucomics/hexical/registry/HexicalEntities.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalEntities.kt
@@ -11,14 +11,12 @@ import net.minecraft.util.registry.Registry
 object HexicalEntities {
 	val MAGIC_MISSILE_ENTITY: EntityType<MagicMissileEntity?> = EntityType.Builder.create(::MagicMissileEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(4).trackingTickInterval(20).build(HexicalMain.MOD_ID + ":magic_missile")
 	val LIVING_SCROLL_ENTITY: EntityType<LivingScrollEntity?> = EntityType.Builder.create(::LivingScrollEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":living_scroll")
-	val FLECK_ENTITY: EntityType<FleckEntity?> = EntityType.Builder.create(::FleckEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":fleck")
 	val SPECK_ENTITY: EntityType<SpeckEntity?> = EntityType.Builder.create(::SpeckEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":speck")
 
 	@JvmStatic
 	fun init() {
 		Registry.register(Registry.ENTITY_TYPE, HexicalMain.id("living_scroll"), LIVING_SCROLL_ENTITY)
 		Registry.register(Registry.ENTITY_TYPE, HexicalMain.id("magic_missile"), MAGIC_MISSILE_ENTITY)
-		Registry.register(Registry.ENTITY_TYPE, HexicalMain.id("fleck"), FLECK_ENTITY)
 		Registry.register(Registry.ENTITY_TYPE, HexicalMain.id("speck"), SPECK_ENTITY)
 	}
 
@@ -26,7 +24,6 @@ object HexicalEntities {
 	fun clientInit() {
 		EntityRendererRegistry.register(LIVING_SCROLL_ENTITY) { ctx: EntityRendererFactory.Context? -> LivingScrollRenderer(ctx) }
 		EntityRendererRegistry.register(MAGIC_MISSILE_ENTITY) { ctx: EntityRendererFactory.Context? -> MagicMissileRenderer(ctx) }
-		EntityRendererRegistry.register(FLECK_ENTITY) { ctx: EntityRendererFactory.Context? -> FleckRenderer(ctx) }
 		EntityRendererRegistry.register(SPECK_ENTITY) { ctx: EntityRendererFactory.Context? -> SpeckRenderer(ctx) }
 	}
 }
