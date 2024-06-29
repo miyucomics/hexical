cb0dcc6c77d9390332b681b51c07b8cff4ba9844
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-06-29 19:47:44 -0400

    Speck rendering optimization

--- src/main/java/miyucomics/hexical/entities/SpeckRenderer.kt
+++ src/main/java/miyucomics/hexical/entities/SpeckRenderer.kt
@@ -12,7 +12,7 @@ import net.minecraft.util.math.*
 class SpeckRenderer(ctx: EntityRendererFactory.Context?) : EntityRenderer<SpeckEntity?>(ctx) {
 	override fun getTexture(entity: SpeckEntity?): Identifier? = null
 	override fun shouldRender(entity: SpeckEntity?, frustum: Frustum?, x: Double, y: Double, z: Double) = true
-	override fun render(entity: SpeckEntity?, yaw: Float, tickDelta: Float, matrices: MatrixStack?, vertexConsumers: VertexConsumerProvider?, light: Int) {
+	override fun render(entity: SpeckEntity?, yaw: Float, tickDelta: Float, matrices: MatrixStack, vertexConsumers: VertexConsumerProvider, light: Int) {
 		val oldShader = RenderSystem.getShader()
 		RenderSystem.setShader(GameRenderer::getPositionColorShader)
 		RenderSystem.enableDepthTest()
@@ -34,6 +34,7 @@ class SpeckRenderer(ctx: EntityRendererFactory.Context?) : EntityRenderer<SpeckE
 			val lines = pattern.toLines(0.25f, pattern.getCenter(0.25f).negate()).toMutableList()
 			for (i in lines.indices)
 				lines[i] = Vec2f(lines[i].x, -lines[i].y)
+//			RenderUtils.testRendering(matrices, lines, entity.getThickness() * 0.05f, entity.getPigment())
 			RenderUtils.drawFigure(matrices.peek().positionMatrix, lines, entity.getThickness() * 0.05f, entity.getPigment())
 		} else {
 			matrices.scale(0.1f / 3f, -0.1f / 3f, -0.1f / 3f)
