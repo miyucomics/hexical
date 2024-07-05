bfcdb7639e2f188bed4f35e2beaefe7e739fea26
miyucomics <nbriannyc@gmail.com>
2024-07-05 12:05:06 -0400

    Media jars

--- src/main/java/miyucomics/hexical/HexicalClient.java
+++ src/main/java/miyucomics/hexical/HexicalClient.java
@@ -1,14 +1,12 @@
 package miyucomics.hexical;
 
-import miyucomics.hexical.registry.HexicalEntities;
-import miyucomics.hexical.registry.HexicalItems;
-import miyucomics.hexical.registry.HexicalKeybinds;
-import miyucomics.hexical.registry.HexicalNetworking;
+import miyucomics.hexical.registry.*;
 import net.fabricmc.api.ClientModInitializer;
 
 public class HexicalClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		HexicalBlocks.clientInit();
 		HexicalEntities.clientInit();
 		HexicalItems.clientInit();
 		HexicalKeybinds.init();
