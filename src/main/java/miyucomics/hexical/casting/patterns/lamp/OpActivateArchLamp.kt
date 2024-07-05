549e4f49236d1cad4a47a5e53ee17ebbad605d02
miyucomics <nbriannyc@gmail.com>
2024-07-05 16:43:27 -0400

    Activate Genie spell

--- src/main/java/miyucomics/hexical/casting/patterns/lamp/OpActivateArchLamp.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/lamp/OpActivateArchLamp.kt
@@ -1,28 +1,37 @@
 package miyucomics.hexical.casting.patterns.lamp
 
+import at.petrak.hexcasting.api.misc.DiscoveryHandlers
+import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.spell.ParticleSpray
 import at.petrak.hexcasting.api.spell.RenderedSpell
 import at.petrak.hexcasting.api.spell.SpellAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
-import miyucomics.hexical.casting.mishaps.NeedsActiveArchLampMishap
-import miyucomics.hexical.items.hasActiveArchLamp
 import miyucomics.hexical.registry.HexicalItems
-import miyucomics.hexical.registry.HexicalSounds
+import net.minecraft.item.ItemStack
 
-class OpTerminateArchLamp : SpellAction {
+class OpActivateArchLamp : SpellAction {
 	override val argc = 0
 	override fun execute(args: List<Iota>, ctx: CastingContext): Triple<RenderedSpell, Int, List<ParticleSpray>> {
-		if (!hasActiveArchLamp(ctx.caster))
-			throw NeedsActiveArchLampMishap()
-		return Triple(Spell(), 0, listOf())
+		for (stack in DiscoveryHandlers.collectItemSlots(ctx)) {
+			if (stack.isEmpty)
+				continue
+			if (!stack.isOf(HexicalItems.ARCH_LAMP_ITEM))
+				continue
+			if (!stack.orCreateNbt.getBoolean("active"))
+				return Triple(Spell(stack), MediaConstants.DUST_UNIT, listOf())
+		}
+		return Triple(Spell(null), 0, listOf())
 	}
 
-	private class Spell : RenderedSpell {
+	private class Spell(val stack: ItemStack?) : RenderedSpell {
 		override fun cast(ctx: CastingContext) {
 			for (slot in ctx.caster.inventory.main)
 				if (slot.item == HexicalItems.ARCH_LAMP_ITEM)
 					slot.orCreateNbt.putBoolean("active", false)
+			if (stack == null)
+				return
+			stack.orCreateNbt.putBoolean("active", true)
 		}
 	}
 }
