689bdbf1fc251f72e44ef66357d3e91c8c2b016e
miyucomics <nbriannyc@gmail.com>
2024-07-07 23:02:46 -0400

    Spike spell progress

--- src/main/java/miyucomics/hexical/casting/patterns/getters/types/OpGetBlockTypeData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/types/OpGetBlockTypeData.kt
@@ -1,17 +1,20 @@
-package miyucomics.hexical.casting.patterns.getters
+package miyucomics.hexical.casting.patterns.getters.types
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
-import at.petrak.hexcasting.api.spell.getBlockPos
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
+import miyucomics.hexical.casting.iota.getIdentifier
+import net.minecraft.util.registry.Registry
 
-class OpGetBlockData(private val mode: Int) : ConstMediaAction {
+class OpGetBlockTypeData(private val mode: Int) : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-		val position = args.getBlockPos(0, argc)
-		ctx.assertVecInRange(position)
-		val block = ctx.world.getBlockState(position).block
+		val id = args.getIdentifier(0, argc)
+		if (!Registry.BLOCK.containsId(id))
+			throw MishapInvalidIota.of(args[0], 0, "block_id")
+		val block = Registry.BLOCK.get(id)
 		return when (mode) {
 			0 -> block.hardness.asActionResult
 			1 -> block.blastResistance.asActionResult
