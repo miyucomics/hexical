886c692315f7f4028ec76f9265714915f361d56a
miyucomics <nbriannyc@gmail.com>
2024-07-10 15:02:31 -0400

    Cached speck verts on client

--- src/main/java/miyucomics/hexical/utils/RenderUtils.kt
+++ src/main/java/miyucomics/hexical/utils/RenderUtils.kt
@@ -3,16 +3,8 @@ package miyucomics.hexical.utils
 import at.petrak.hexcasting.api.misc.FrozenColorizer
 import at.petrak.hexcasting.api.spell.math.HexPattern
 import at.petrak.hexcasting.client.rotate
-import com.mojang.blaze3d.systems.RenderSystem
-import net.minecraft.client.render.GameRenderer
-import net.minecraft.client.render.Tessellator
-import net.minecraft.client.render.VertexFormat
-import net.minecraft.client.render.VertexFormats
-import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.util.math.MathHelper
-import net.minecraft.util.math.Matrix4f
-import net.minecraft.util.math.Vec2f
-import net.minecraft.util.math.Vec3d
+import net.minecraft.client.render.*
+import net.minecraft.util.math.*
 import kotlin.math.abs
 import kotlin.math.atan2
 import kotlin.math.ceil
@@ -30,36 +22,6 @@ object RenderUtils {
 		for (i in lines.indices)
 			lines[i] = Vec2f(lines[i].x, -lines[i].y).multiply(1 / scaling)
 		return lines.toList()
-	}
-
-	// this rendering function draws some points similar to a sentinel: it will always be the same width regardless of distance
-	fun sentinelLike(matrices: MatrixStack, points: List<Vec3d>, width: Float, colorizer: FrozenColorizer) {
-		if (points.size <= 1) return
-		val tessellator = Tessellator.getInstance()
-		val buf = tessellator.buffer
-		RenderSystem.setShader(GameRenderer::getRenderTypeLinesShader)
-		RenderSystem.lineWidth(width)
-		val matrix = matrices.peek().positionMatrix
-		fun vertex(a: Vec3d, b: Vec3d) {
-			val aColor = colorizer.getColor(0f, a)
-			val bColor = colorizer.getColor(0f, b)
-			val normal = a.subtract(b).normalize()
-			buf.vertex(matrix, a.x.toFloat(), a.y.toFloat(), a.z.toFloat())
-				.color(aColor)
-				.normal(matrices.peek().normalMatrix, normal.x.toFloat(), normal.y.toFloat(), normal.z.toFloat())
-				.next()
-			buf.vertex(matrix, b.x.toFloat(), b.y.toFloat(), b.z.toFloat())
-				.color(bColor)
-				.normal(matrices.peek().normalMatrix, -normal.x.toFloat(), -normal.y.toFloat(), -normal.z.toFloat())
-				.next()
-		}
-		buf.begin(VertexFormat.DrawMode.LINES, VertexFormats.LINES)
-		for (i in 1 until points.size) {
-			val a = points[i - 1]
-			val b = points[i]
-			vertex(Vec3d(a.x, a.y, a.z), Vec3d(b.x, b.y, b.z))
-		}
-		tessellator.draw()
 	}
 
 	fun drawFigure(mat: Matrix4f, points: List<Vec2f>, width: Float, colorizer: FrozenColorizer, colorizerOffset: Vec3d) {
