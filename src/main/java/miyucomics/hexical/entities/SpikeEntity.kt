a7ad75cf3ad7d8322620f761c641aef89375efab
miyucomics <nbriannyc@gmail.com>
2024-07-14 17:57:53 -0400

    Spike spell mostly complete

--- src/main/java/miyucomics/hexical/entities/SpikeEntity.kt
+++ src/main/java/miyucomics/hexical/entities/SpikeEntity.kt
@@ -8,28 +8,31 @@ import net.minecraft.entity.data.TrackedData
 import net.minecraft.entity.data.TrackedDataHandlerRegistry
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
+import java.lang.Math.pow
 import kotlin.math.min
+import kotlin.math.pow
 
 class SpikeEntity(entityType: EntityType<SpikeEntity>, world: World) : Entity(entityType, world) {
-	private val animationLength = 10
-	private val lingerPeriod = 10
-	private var timer: Int = 0
+	private var timer = 0
 
-	constructor(world: World, x: Double, y: Double, z: Double, direction: Direction, timer: Int): this(HexicalEntities.SPIKE_ENTITY, world) {
+	constructor(world: World, x: Double, y: Double, z: Double, direction: Direction): this(HexicalEntities.SPIKE_ENTITY, world) {
 		this.setDirection(direction)
 		this.setPosition(x, y, z)
-		this.timer = timer
-		this.dataTracker.set(timerDataTracker, timer)
+		this.timer = -10
+		this.dataTracker.set(timerDataTracker, this.timer)
 	}
 
 	override fun tick() {
 		this.timer += 1
-		if (this.timer == 0)
+		if (this.timer == EMERGE_LENGTH) {
+			this.playSound(SoundEvents.BLOCK_GLASS_BREAK, 1f, 1f)
 			for (livingEntity in world.getNonSpectatingEntities(LivingEntity::class.java, boundingBox))
 				this.damage(livingEntity)
-		if (this.timer > animationLength + lingerPeriod)
+		}
+		if (this.timer > EMERGE_LENGTH + DISAPPEAR_LENGTH)
 			this.discard()
 	}
 
@@ -53,17 +56,19 @@ class SpikeEntity(entityType: EntityType<SpikeEntity>, world: World) : Entity(en
 	}
 
 	fun getAnimationProgress(tickDelta: Float): Float {
-		if (this.timer < -tickDelta)
-			return 0f
-		if (this.timer > -tickDelta && this.timer < this.animationLength)
-			return min(1f, (this.timer + tickDelta) / this.animationLength.toFloat() + 0.05f)
-		if (this.timer >= this.animationLength)
-			return (this.animationLength - (this.timer + tickDelta - this.animationLength) / this.lingerPeriod.toFloat())
+		val trueTime = this.timer + tickDelta
+		if (trueTime < 0)
+			return MIN_APPEARANCE
+		if (this.timer in 0..<EMERGE_LENGTH)
+			return min((trueTime / EMERGE_LENGTH.toFloat()).pow(5f) + MIN_APPEARANCE, 1f)
+		if (this.timer >= EMERGE_LENGTH)
+			return 1 - ((trueTime - EMERGE_LENGTH) / DISAPPEAR_LENGTH.toFloat())
 		return 1f
 	}
 
 	override fun readCustomDataFromNbt(nbt: NbtCompound) {
 		this.timer = nbt.getInt("timer")
+		this.dataTracker.set(timerDataTracker, this.timer)
 		this.setDirection(Direction.byId(nbt.getInt("direction")))
 	}
 
@@ -81,7 +86,15 @@ class SpikeEntity(entityType: EntityType<SpikeEntity>, world: World) : Entity(en
 		this.dataTracker.startTracking(timerDataTracker, 0)
 	}
 
+	override fun onTrackedDataSet(data: TrackedData<*>?) {
+		if (data == timerDataTracker)
+			this.timer = this.dataTracker.get(timerDataTracker)
+	}
+
 	companion object {
+		private const val EMERGE_LENGTH = 10
+		private const val DISAPPEAR_LENGTH = 5
+		private const val MIN_APPEARANCE = 0.1f
 		private val directionDataTracker: TrackedData<Int> = DataTracker.registerData(SpikeEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
 		private val timerDataTracker: TrackedData<Int> = DataTracker.registerData(SpikeEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
 	}
