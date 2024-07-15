e8249d9aa10668be3cd914b3b718bf85b101e3d1
miyucomics <nbriannyc@gmail.com>
2024-07-14 21:24:59 -0400

    Spikes documented

--- src/main/java/miyucomics/hexical/registry/HexicalDamageTypes.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalDamageTypes.kt
@@ -1,12 +1,13 @@
 package miyucomics.hexical.registry
 
+import miyucomics.hexical.entities.SpikeEntity
 import net.minecraft.entity.Entity
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.damage.ProjectileDamageSource
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.projectile.PersistentProjectileEntity
 
 object HexicalDamageTypes {
-	fun magicMissile(projectile: PersistentProjectileEntity, attacker: Entity?): DamageSource {
-		return ProjectileDamageSource("magic_missile", projectile, attacker).setProjectile().setUnblockable().setUsesMagic().setBypassesArmor().setBypassesProtection()
-	}
+	fun magicMissile(projectile: PersistentProjectileEntity, attacker: Entity?): DamageSource = ProjectileDamageSource("magic_missile", projectile, attacker).setProjectile().setUnblockable().setUsesMagic().setBypassesArmor().setBypassesProtection()
+	fun spike(spike: SpikeEntity, attacker: PlayerEntity?): DamageSource = ProjectileDamageSource("spike", spike, attacker)
 }
