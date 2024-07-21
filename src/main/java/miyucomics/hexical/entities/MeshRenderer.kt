432dd0dca83b95dc242cb5785f79c3a073e67750
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-07-21 14:04:39 -0400

    Mesh progress

--- src/main/java/miyucomics/hexical/entities/MeshRenderer.kt
+++ src/main/java/miyucomics/hexical/entities/MeshRenderer.kt
@@ -1,42 +1,49 @@
 package miyucomics.hexical.entities
 
-import com.mojang.blaze3d.platform.GlStateManager
+import at.petrak.hexcasting.api.HexAPI.modLoc
 import com.mojang.blaze3d.systems.RenderSystem
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.*
 import net.minecraft.client.render.entity.EntityRenderer
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.Vec3f
+import net.minecraft.util.math.Vec3d
 
 class MeshRenderer(ctx: EntityRendererFactory.Context) : EntityRenderer<MeshEntity>(ctx) {
+	private val layer = RenderLayer.getEntityCutoutNoCull(modLoc("textures/entity/white.png"))
+
 	override fun getTexture(entity: MeshEntity?): Identifier? = null
 	override fun shouldRender(entity: MeshEntity?, frustum: Frustum?, x: Double, y: Double, z: Double) = true
 	override fun render(entity: MeshEntity?, yaw: Float, tickDelta: Float, matrices: MatrixStack, vertexConsumers: VertexConsumerProvider, light: Int) {
 		val vertices = entity!!.clientVertices
 		if (vertices.size < 2)
 			return
-		RenderSystem.enableBlend()
-		RenderSystem.lineWidth(10f)
-		RenderSystem.disableCull()
-		RenderSystem.blendFunc(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA)
-		RenderSystem.setShader(GameRenderer::getRenderTypeLinesShader)
-		val tessellator = Tessellator.getInstance()
-		val buf = tessellator.buffer
-		buf.begin(VertexFormat.DrawMode.LINES, VertexFormats.LINES)
-		for (i in 1..<vertices.size)
-			renderLine(matrices, buf, vertices[i - 1], vertices[i], 1f, 1f, 1f, 1f)
-		tessellator.draw()
-		RenderSystem.enableCull()
+
+		val buf = vertexConsumers.getBuffer(layer)
+		for (i in 1..<vertices.size) {
+			val a = vertices[i - 1]
+			val b = vertices[i]
+			renderLine(matrices, buf, Vec3d(a.x.toDouble(), a.y.toDouble(), a.z.toDouble()), Vec3d(b.x.toDouble(), b.y.toDouble(), b.z.toDouble()), light)
+		}
 	}
 
-	private fun renderLine(matrices: MatrixStack, buffer: BufferBuilder, start: Vec3f, end: Vec3f, r: Float, g: Float, b: Float, a: Float) {
+	private fun renderLine(matrices: MatrixStack, buffer: VertexConsumer, start: Vec3d, end: Vec3d, light: Int) {
 		val position = matrices.peek().positionMatrix
-		val norm = matrices.peek().normalMatrix
-		val normal = end.copy()
-		normal.subtract(start)
-		normal.normalize()
-		buffer.vertex(position, start.x, start.y, start.z).color(r, g, b, a).normal(norm, normal.x, normal.y, normal.z).next()
-		buffer.vertex(position, end.x, end.y, end.z).color(r, g, b, a).normal(norm, -normal.x, -normal.y, -normal.z).next()
+		val normal = matrices.peek().normalMatrix
+
+		val strokeDirection = end.subtract(start).normalize()
+		val cameraDirection = MinecraftClient.getInstance().gameRenderer.camera.pos.subtract(start.add(end).multiply(0.5)).normalize()
+		val perpendicular = strokeDirection.crossProduct(cameraDirection).normalize().multiply(0.05)
+
+		val p1 = start.add(perpendicular)
+		val p2 = start.subtract(perpendicular)
+		val p3 = end.subtract(perpendicular)
+		val p4 = end.add(perpendicular)
+
+		buffer.vertex(position, p1.x.toFloat(), p1.y.toFloat(), p1.z.toFloat()).color(1f, 1f, 1f, 1f).texture(0f, 0f).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normal, 0f, 0f, 1f).next()
+		buffer.vertex(position, p2.x.toFloat(), p2.y.toFloat(), p2.z.toFloat()).color(1f, 1f, 1f, 1f).texture(0f, 0f).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normal, 0f, 0f, 1f).next()
+		buffer.vertex(position, p3.x.toFloat(), p3.y.toFloat(), p3.z.toFloat()).color(1f, 1f, 1f, 1f).texture(0f, 0f).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normal, 0f, 0f, 1f).next()
+		buffer.vertex(position, p4.x.toFloat(), p4.y.toFloat(), p4.z.toFloat()).color(1f, 1f, 1f, 1f).texture(0f, 0f).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normal, 0f, 0f, 1f).next()
 	}
 }
