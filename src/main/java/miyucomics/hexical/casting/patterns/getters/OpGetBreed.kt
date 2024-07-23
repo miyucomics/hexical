5208337e5189525ffa07069f68fadf5d1960d0c2
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-07-23 12:58:43 -0400

    Some more minor additions

---
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetBreed.kt
@@ -0,0 +1,20 @@
+package miyucomics.hexical.casting.patterns.getters
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.asActionResult
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.getEntity
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.iota.NullIota
+import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
+import net.minecraft.entity.passive.AnimalEntity
+
+class OpGetBreed : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		val entity = args.getEntity(0, argc)
+		if (entity !is AnimalEntity)
+			return listOf(NullIota())
+		return entity.isInLove.asActionResult
+	}
+}
--- src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetLivingEntityData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetLivingEntityData.kt
@@ -4,14 +4,22 @@ import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getLivingEntityButNotArmorStand
+import at.petrak.hexcasting.api.spell.iota.EntityIota
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
 import miyucomics.hexical.casting.iota.IdentifierIota
+import net.minecraft.entity.LivingEntity
 import net.minecraft.util.registry.Registry
 
 class OpGetLivingEntityData(private val mode: Int) : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-		val entity = args.getLivingEntityButNotArmorStand(0, argc)
+		val iota = args[0]
+		if (iota !is EntityIota)
+			throw MishapInvalidIota.ofType(iota, 0, "lenient_living")
+		val entity = iota.entity
+		if (entity !is LivingEntity)
+			throw MishapInvalidIota.ofType(iota, 0, "lenient_living")
 		return when (mode) {
 			0 -> entity.health.asActionResult
 			1 -> entity.maxHealth.asActionResult
@@ -19,7 +27,8 @@ class OpGetLivingEntityData(private val mode: Int) : ConstMediaAction {
 			3 -> (entity.maxAir.toDouble() / 20 + 1).asActionResult
 			4 -> entity.isSleeping.asActionResult
 			5 -> entity.isSprinting.asActionResult
-			6 -> {
+			6 -> entity.isBaby.asActionResult
+			7 -> {
 				val list = mutableListOf<Iota>()
 				for (effect in entity.statusEffects)
 					list.add(IdentifierIota(Registry.STATUS_EFFECT.getId(effect.effectType)!!))
