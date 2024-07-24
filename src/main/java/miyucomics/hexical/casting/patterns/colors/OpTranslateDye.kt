eab8e6f04511d30f7b7dfd4e2074d559e1b119ff
miyucomics <nbriannyc@gmail.com>
2024-07-24 13:10:46 -0400

    Minor stuff

---
+++ src/main/java/miyucomics/hexical/casting/patterns/colors/OpTranslateDye.kt
@@ -0,0 +1,16 @@
+package miyucomics.hexical.casting.patterns.colors
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.asActionResult
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.iota.Iota
+import miyucomics.hexical.casting.iota.getTrueDye
+import net.minecraft.util.math.Vec3d
+
+class OpTranslateDye : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		val dye = args.getTrueDye(0, argc).colorComponents
+		return Vec3d(dye[0].toDouble() / 255.0, dye[1].toDouble() / 255.0, dye[2].toDouble() / 255.0).asActionResult
+	}
+}
