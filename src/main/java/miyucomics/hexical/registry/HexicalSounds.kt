4f5f36110e80786994f1f01d4c10474f018c8275
miyucomics <nbriannyc@gmail.com>
2024-07-28 12:13:41 -0400

    Major rendering overhaul

--- src/main/java/miyucomics/hexical/registry/HexicalSounds.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalSounds.kt
@@ -5,23 +5,27 @@ import net.minecraft.sound.SoundEvent
 import net.minecraft.util.registry.Registry
 
 object HexicalSounds {
-	private val EVOKING_MURMUR_ID = HexicalMain.id("evoking_murmur")
-	val EVOKING_MURMUR_EVENT = SoundEvent(EVOKING_MURMUR_ID)
-	private val EVOKING_CAST_ID = HexicalMain.id("evoking_casts")
-	val EVOKING_CAST_EVENT = SoundEvent(EVOKING_CAST_ID)
-	private val LAMP_ACTIVATE_SOUND_ID = HexicalMain.id("lamp_activate")
-	val LAMP_ACTIVATE_SOUND_EVENT = SoundEvent(LAMP_ACTIVATE_SOUND_ID)
-	private val LAMP_DEACTIVATE_SOUND_ID = HexicalMain.id("lamp_deactivate")
-	val LAMP_DEACTIVATE_SOUND_EVENT = SoundEvent(LAMP_DEACTIVATE_SOUND_ID)
-	private val SUDDEN_REALIZATION_ID = HexicalMain.id("sudden_realization")
-	val SUDDEN_REALIZATION_EVENT = SoundEvent(SUDDEN_REALIZATION_ID)
+	lateinit var EVOKING_MURMUR_EVENT: SoundEvent
+	lateinit var EVOKING_CAST_EVENT: SoundEvent
+	lateinit var LAMP_ACTIVATE_SOUND_EVENT: SoundEvent
+	lateinit var LAMP_DEACTIVATE_SOUND_EVENT: SoundEvent
+	lateinit var SUDDEN_REALIZATION_EVENT: SoundEvent
+	lateinit var PLAYER_SLURP: SoundEvent
 
 	@JvmStatic
 	fun init() {
-		Registry.register(Registry.SOUND_EVENT, EVOKING_MURMUR_ID, EVOKING_MURMUR_EVENT)
-		Registry.register(Registry.SOUND_EVENT, EVOKING_CAST_ID, EVOKING_CAST_EVENT)
-		Registry.register(Registry.SOUND_EVENT, LAMP_ACTIVATE_SOUND_ID, LAMP_ACTIVATE_SOUND_EVENT)
-		Registry.register(Registry.SOUND_EVENT, LAMP_DEACTIVATE_SOUND_ID, LAMP_DEACTIVATE_SOUND_EVENT)
-		Registry.register(Registry.SOUND_EVENT, SUDDEN_REALIZATION_ID, SUDDEN_REALIZATION_EVENT)
+		EVOKING_MURMUR_EVENT = register("evoking_murmur")
+		EVOKING_CAST_EVENT = register("evoking_casts")
+		LAMP_ACTIVATE_SOUND_EVENT = register("lamp_activate")
+		LAMP_DEACTIVATE_SOUND_EVENT = register("lamp_deactivate")
+		SUDDEN_REALIZATION_EVENT = register("sudden_realization")
+		PLAYER_SLURP = register("player_slurp")
+	}
+
+	private fun register(name: String): SoundEvent {
+		val id = HexicalMain.id(name)
+		val event = SoundEvent(id)
+		Registry.register(Registry.SOUND_EVENT, id, event)
+		return event
 	}
 }
