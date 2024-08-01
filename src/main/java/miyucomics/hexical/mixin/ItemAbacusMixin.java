60a8cfe8192e5b92748ce63d4930df185dd1fd41
miyucomics <nbriannyc@gmail.com>
2024-08-01 18:57:23 -0400

    Documented prestidigitation

--- src/main/java/miyucomics/hexical/mixin/ItemAbacusMixin.java
+++ src/main/java/miyucomics/hexical/mixin/ItemAbacusMixin.java
@@ -5,9 +5,7 @@ import at.petrak.hexcasting.api.spell.iota.Iota;
 import at.petrak.hexcasting.api.utils.NBTHelper;
 import at.petrak.hexcasting.common.items.ItemAbacus;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,10 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemAbacus.class)
 public class ItemAbacusMixin {
-	@Shadow
-	@Final
-	public static String TAG_VALUE;
-
 	@Inject(method = "canWrite", at = @At("HEAD"), cancellable = true)
 	public void makeWriteable(ItemStack stack, Iota datum, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(datum instanceof DoubleIota);
@@ -27,6 +21,6 @@ public class ItemAbacusMixin {
 	@Inject(method = "writeDatum", at = @At("HEAD"))
 	public void makeWriteable(ItemStack stack, Iota datum, CallbackInfo ci) {
 		if (datum instanceof DoubleIota doub)
-			NBTHelper.putDouble(stack, TAG_VALUE, doub.getDouble());
+			NBTHelper.putDouble(stack, ItemAbacus.TAG_VALUE, doub.getDouble());
 	}
 }
