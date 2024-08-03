b08210c8aea344d00deb242cd89a7e2da3567853
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-08-03 14:15:08 -0400

    Added and removed

--- src/main/java/miyucomics/hexical/registry/HexicalKeybinds.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalKeybinds.kt
@@ -31,7 +31,7 @@ object HexicalKeybinds {
 			}
 			states[EVOKE_KEYBIND.translationKey] = EVOKE_KEYBIND.isPressed
 
-			for (key in listOf(client.options.forwardKey, client.options.leftKey, client.options.rightKey, client.options.backKey, TELEPATHY_KEYBIND)) {
+			for (key in listOf(client.options.forwardKey, client.options.leftKey, client.options.rightKey, client.options.backKey, client.options.jumpKey, TELEPATHY_KEYBIND)) {
 				if (states.keys.contains(key.translationKey)) {
 					if (states[key.translationKey] == true && !key.isPressed) {
 						val buf = PacketByteBufs.create()
