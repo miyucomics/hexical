4351ba4fed8758b011cf6e1879fbb2bedadc5a1d
miyucomics <nbriannyc@gmail.com>
2024-08-04 12:46:48 -0400

    Documentation reshuffling

---
+++ src/main/java/miyucomics/hexical/casting/patterns/OpBrainswept.kt
@@ -0,0 +1,19 @@
+package miyucomics.hexical.casting.patterns
+
+import at.petrak.hexcasting.api.spell.ConstMediaAction
+import at.petrak.hexcasting.api.spell.asActionResult
+import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.getEntity
+import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.xplat.IXplatAbstractions
+import net.minecraft.entity.mob.MobEntity
+
+class OpBrainswept : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		val entity = args.getEntity(0, argc)
+		if (entity is MobEntity)
+			return IXplatAbstractions.INSTANCE.isBrainswept(entity).asActionResult
+		return (false).asActionResult
+	}
+}
