6a551bd9855fcae625e41b6fb452b24584647ec0
miyucomics <nbriannyc@gmail.com>
2024-08-08 19:45:38 -0400

    Regressing

--- src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
+++ src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
@@ -1,9 +1,7 @@
 package miyucomics.hexical.blocks
 
 import miyucomics.hexical.registry.HexicalSounds
-import miyucomics.hexical.registry.HexicalStatusEffects
 import net.minecraft.block.*
-import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
@@ -27,7 +25,6 @@ class MediaJarBlock : TransparentBlock(Settings.of(Material.GLASS).emissiveLight
 			return ActionResult.SUCCESS
 		player.swingHand(hand)
 		world.playSoundFromEntity(null, player, HexicalSounds.PLAYER_SLURP, SoundCategory.PLAYERS, 1f, 1f)
-		player.addStatusEffect(StatusEffectInstance(HexicalStatusEffects.MEDIA_VISION_STATUS_EFFECT, 2000, 0, false, false, true))
 		return ActionResult.SUCCESS
 	}
 }
--- src/main/java/miyucomics/hexical/status_effects/MediaVisionStatusEffect.kt
+++
@@ -1,8 +0,0 @@
-package miyucomics.hexical.status_effects
-
-import net.minecraft.entity.effect.StatusEffect
-import net.minecraft.entity.effect.StatusEffectCategory
-
-class MediaVisionStatusEffect : StatusEffect(StatusEffectCategory.NEUTRAL, 0x98D982) {
-	override fun canApplyUpdateEffect(duration: Int, amplifier: Int) = true
-}
