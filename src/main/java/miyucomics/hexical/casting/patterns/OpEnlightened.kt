037dc08d25aed130679816d2083a149a04692932
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-08-20 13:28:21 -0400

    Pricing changes

--- src/main/java/miyucomics/hexical/casting/patterns/OpEnlightened.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/OpEnlightened.kt
@@ -3,9 +3,14 @@ package miyucomics.hexical.casting.patterns
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.getPlayer
 import at.petrak.hexcasting.api.spell.iota.Iota
+import miyucomics.hexical.utils.CastingUtils
 
 class OpEnlightened : ConstMediaAction {
-	override val argc = 0
-	override fun execute(args: List<Iota>, ctx: CastingContext) = ctx.isCasterEnlightened.asActionResult
+	override val argc = 1
+	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
+		val player = args.getPlayer(0, argc)
+		return CastingUtils.isEnlightened(player).asActionResult
+	}
 }
