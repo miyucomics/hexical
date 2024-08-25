7d914445dd1dfe2fc304c2164603d7bc19a351ef
miyucomics <72044529+miyucomics@users.noreply.github.com>
2024-08-25 13:03:45 -0400

    Conjure Hexxy advancement

--- src/main/java/miyucomics/hexical/casting/patterns/specks/OpConjureSpeck.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/specks/OpConjureSpeck.kt
@@ -6,6 +6,9 @@ import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getVec3
 import at.petrak.hexcasting.api.spell.iota.EntityIota
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.iota.PatternIota
+import at.petrak.hexcasting.api.spell.math.HexDir
+import at.petrak.hexcasting.api.spell.math.HexPattern
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexical.entities.SpeckEntity
 import miyucomics.hexical.registry.HexicalAdvancements
@@ -16,17 +19,20 @@ class OpConjureSpeck : ConstMediaAction {
 	override val mediaCost: Int = MediaConstants.DUST_UNIT / 100
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val iota = args[0]
+		if (iota is PatternIota && iota.pattern == HexPattern.fromAngles("deaqq", HexDir.SOUTH_EAST))
+			HexicalAdvancements.HEXXY.trigger(ctx.caster)
+
 		val position = args.getVec3(1, argc)
 		val rotation = args.getVec3(2, argc)
 		ctx.assertVecInRange(position)
+
 		val speck = SpeckEntity(ctx.world)
 		speck.setPosition(position.subtract(0.0, speck.standingEyeHeight.toDouble(), 0.0))
 		speck.lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, speck.pos.add(rotation))
-		speck.setIota(iota)
 		speck.setPigment(IXplatAbstractions.INSTANCE.getColorizer(ctx.caster))
-		speck.setSize(1f)
-		speck.setThickness(1f)
+		speck.setIota(iota)
 		ctx.world.spawnEntity(speck)
+
 		HexicalAdvancements.AR.trigger(ctx.caster)
 		return listOf(EntityIota(speck))
 	}
