9100d11f5fb33b3e7558eaf9447d02356b3c583b
miyucomics <nbriannyc@gmail.com>
2024-08-25 13:03:45 -0400

    Conjure Hexxy advancement

--- src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalAdvancements.kt
@@ -5,6 +5,7 @@ import net.minecraft.advancement.criterion.Criteria
 
 object HexicalAdvancements {
 	lateinit var AR: ARCriterion
+	lateinit var HEXXY: HexxyCriterion
 	lateinit var DIY: DIYCriterion
 	lateinit var HALLUCINATE: HallucinateCriterion
 	lateinit var EDUCATE_GENIE: EducateGenieCriterion
@@ -14,6 +15,7 @@ object HexicalAdvancements {
 	@JvmStatic
 	fun init() {
 		AR = Criteria.register(ARCriterion())
+		HEXXY = Criteria.register(HexxyCriterion())
 		DIY = Criteria.register(DIYCriterion())
 		HALLUCINATE = Criteria.register(HallucinateCriterion())
 		EDUCATE_GENIE = Criteria.register(EducateGenieCriterion())
