f3b5cefc96789afb847c887ae2e497a67fd1cbc9
miyucomics <nbriannyc@gmail.com>
2024-08-28 15:25:29 -0400

    Docs

--- src/main/java/miyucomics/hexical/state/EvokeState.kt
+++ src/main/java/miyucomics/hexical/state/EvokeState.kt
@@ -5,4 +5,7 @@ import java.util.*
 object EvokeState {
 	val active: HashMap<UUID, Boolean> = HashMap()
 	val duration: HashMap<UUID, Int> = HashMap()
+
+	@JvmStatic
+	fun isEvoking(uuid: UUID): Boolean = active.getOrDefault(uuid, false)
 }
