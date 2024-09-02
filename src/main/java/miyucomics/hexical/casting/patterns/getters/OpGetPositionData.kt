8371dfab22e80e478747c38e72b4b5afad98a396
miyucomics <nbriannyc@gmail.com>
2024-09-01 20:04:05 -0400

    Scrying refactor

--- src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetPositionData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetPositionData.kt
@@ -1,21 +1,17 @@
 package miyucomics.hexical.casting.patterns.getters
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
-import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getBlockPos
 import at.petrak.hexcasting.api.spell.iota.Iota
-import miyucomics.hexical.casting.iota.asActionResult
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
 
-class OpGetPositionData(private val mode: Int) : ConstMediaAction {
+class OpGetPositionData(private val process: (ServerWorld, BlockPos) -> List<Iota>) : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val position = args.getBlockPos(0, argc)
 		ctx.assertVecInRange(position)
-		return when (mode) {
-			0 -> ctx.world.getLightLevel(position).asActionResult
-			1 -> ctx.world.getBiome(position).key.get().value.asActionResult()
-			else -> throw IllegalStateException()
-		}
+		return process(ctx.world, position)
 	}
 }
