ffedc5a8940d1c045b6c208a8761ef8d8ea293c4
miyucomics <nbriannyc@gmail.com>
2024-09-01 20:04:05 -0400

    Scrying refactor

--- src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetLivingEntityData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetLivingEntityData.kt
@@ -1,16 +1,13 @@
 package miyucomics.hexical.casting.patterns.getters
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
-import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.EntityIota
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
-import miyucomics.hexical.casting.iota.IdentifierIota
 import net.minecraft.entity.LivingEntity
-import net.minecraft.util.registry.Registry
 
-class OpGetLivingEntityData(private val mode: Int) : ConstMediaAction {
+class OpGetLivingEntityData(private val process: (LivingEntity) -> List<Iota>) : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val iota = args[0]
@@ -19,21 +16,6 @@ class OpGetLivingEntityData(private val mode: Int) : ConstMediaAction {
 		val entity = iota.entity
 		if (entity !is LivingEntity)
 			throw MishapInvalidIota.ofType(iota, 0, "lenient_living")
-		return when (mode) {
-			0 -> entity.health.asActionResult
-			1 -> entity.maxHealth.asActionResult
-			2 -> (entity.air.toDouble() / 20 + 1).asActionResult
-			3 -> (entity.maxAir.toDouble() / 20 + 1).asActionResult
-			4 -> entity.isSleeping.asActionResult
-			5 -> entity.isSprinting.asActionResult
-			6 -> entity.isBaby.asActionResult
-			7 -> {
-				val list = mutableListOf<Iota>()
-				for (effect in entity.statusEffects)
-					list.add(IdentifierIota(Registry.STATUS_EFFECT.getId(effect.effectType)!!))
-				list.asActionResult
-			}
-			else -> throw IllegalStateException()
-		}
+		return process(entity)
 	}
 }
--- src/main/java/miyucomics/hexical/casting/patterns/getters/types/OpGetItemTypeData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/types/OpGetItemTypeData.kt
@@ -1,25 +1,19 @@
 package miyucomics.hexical.casting.patterns.getters.types
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
-import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.mishaps.MishapInvalidIota
 import miyucomics.hexical.casting.iota.getIdentifier
+import net.minecraft.item.Item
 import net.minecraft.util.registry.Registry
 
-class OpGetItemTypeData(private val mode: Int) : ConstMediaAction {
+class OpGetItemTypeData(private val process: (Item) -> List<Iota>) : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
 		val id = args.getIdentifier(0, argc)
 		if (!Registry.ITEM.containsId(id))
 			throw MishapInvalidIota.of(args[0], 0, "item_id")
-		val item = Registry.ITEM.get(id)
-		return when (mode) {
-			0 -> item.maxCount.asActionResult
-			1 -> item.maxDamage.asActionResult
-			2 -> item.isFood.asActionResult
-			else -> throw IllegalStateException()
-		}
+		return process(Registry.ITEM.get(id))
 	}
 }
