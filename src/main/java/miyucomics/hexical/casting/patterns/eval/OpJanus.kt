64938103a7b8d9bbfb4a6df775527e2a06c94c01
miyucomics <nbriannyc@gmail.com>
2024-09-02 11:39:41 -0400

    Documented Castor and Pollux (revisit later)

--- src/main/java/miyucomics/hexical/casting/patterns/eval/OpJanus.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/eval/OpJanus.kt
@@ -7,5 +7,5 @@ import at.petrak.hexcasting.api.spell.casting.eval.SpellContinuation
 import at.petrak.hexcasting.api.spell.iota.Iota
 
 object OpJanus : Action {
-	override fun operate(continuation: SpellContinuation, stack: MutableList<Iota>, ravenmind: Iota?, ctx: CastingContext) = OperationResult(SpellContinuation.Done, listOf(), null, listOf())
+	override fun operate(continuation: SpellContinuation, stack: MutableList<Iota>, ravenmind: Iota?, ctx: CastingContext) = OperationResult(SpellContinuation.Done, listOf(), ravenmind, listOf())
 }
