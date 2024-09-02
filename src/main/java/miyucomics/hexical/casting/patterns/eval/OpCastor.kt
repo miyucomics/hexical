87193e00dc9c35be6f221f48e26c6e44b1f57fe5
object-Object <object@objectobject.ca>
2024-09-02 00:11:56 -0400

    Reimplement OpCastor and OpPollux using a new abstract OpModifyThoth class

--- src/main/java/miyucomics/hexical/casting/patterns/eval/OpCastor.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/eval/OpCastor.kt
@@ -1,32 +1,16 @@
 package miyucomics.hexical.casting.patterns.eval
 
-import at.petrak.hexcasting.api.spell.Action
-import at.petrak.hexcasting.api.spell.OperationResult
 import at.petrak.hexcasting.api.spell.SpellList
-import at.petrak.hexcasting.api.spell.casting.CastingContext
-import at.petrak.hexcasting.api.spell.casting.eval.FrameFinishEval
 import at.petrak.hexcasting.api.spell.casting.eval.FrameForEach
-import at.petrak.hexcasting.api.spell.casting.eval.SpellContinuation
 import at.petrak.hexcasting.api.spell.iota.Iota
 import at.petrak.hexcasting.api.spell.mishaps.MishapNotEnoughArgs
 
-object OpCastor : Action {
-	override fun operate(continuation: SpellContinuation, stack: MutableList<Iota>, ravenmind: Iota?, ctx: CastingContext): OperationResult {
-		if (stack.size < 1)
-			throw MishapNotEnoughArgs(1, 0)
-		val iota = stack[stack.lastIndex]
-		stack.removeLastOrNull()
-
-		if (continuation is SpellContinuation.NotDone && continuation.frame is FrameFinishEval) {
-			val next = continuation.next
-			if (next is SpellContinuation.NotDone && next.frame is FrameForEach) {
-				val thoth = (next.frame as FrameForEach)
-				val newIterator = thoth.data.cdr.toMutableList()
-				newIterator.add(0, iota)
-				return OperationResult(next.pushFrame(FrameForEach(SpellList.LList(newIterator), thoth.code, thoth.baseStack, thoth.acc)), stack, ravenmind, listOf())
-			}
-		}
-
-		return OperationResult(continuation, stack, ravenmind, listOf())
+// prepend
+object OpCastor : OpModifyThoth() {
+	override fun updateFrame(frame: FrameForEach, stack: MutableList<Iota>): FrameForEach {
+		val iota = stack.removeLastOrNull() ?: throw MishapNotEnoughArgs(1, 0)
+		return frame.copy(
+			data = SpellList.LPair(iota, frame.data)
+		)
 	}
 }
