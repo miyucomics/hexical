dbc2eec5a3f3921541a70975302ccbb915a44d4a
miyucomics <nbriannyc@gmail.com>
2024-09-03 16:10:10 -0400

    Reformatting!

--- src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetBlockStateData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/getters/OpGetBlockStateData.kt
@@ -1,18 +1,10 @@
 package miyucomics.hexical.casting.patterns.getters
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
-import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getBlockPos
 import at.petrak.hexcasting.api.spell.iota.Iota
-import at.petrak.hexcasting.api.spell.iota.NullIota
-import at.petrak.hexcasting.common.blocks.akashic.BlockAkashicBookshelf
 import net.minecraft.block.BlockState
-import net.minecraft.block.CandleBlock
-import net.minecraft.block.SeaPickleBlock
-import net.minecraft.block.TurtleEggBlock
-import net.minecraft.state.property.Properties
-import net.minecraft.util.math.Direction
 
 class OpGetBlockStateData(private val process: (BlockState) -> List<Iota>) : ConstMediaAction {
 	override val argc = 1
