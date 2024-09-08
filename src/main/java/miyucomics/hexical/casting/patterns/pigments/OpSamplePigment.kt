92d43719efe812d58952ff5a57531a4d0a27d61a
miyucomics <nbriannyc@gmail.com>
2024-09-08 11:18:42 -0400

    Pigment iota

--- src/main/java/miyucomics/hexical/casting/patterns/pigments/OpSamplePigment.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/pigments/OpSamplePigment.kt
@@ -1,4 +1,4 @@
-package miyucomics.hexical.casting.patterns.colors
+package miyucomics.hexical.casting.patterns.pigments
 
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
@@ -6,16 +6,17 @@ import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getDouble
 import at.petrak.hexcasting.api.spell.getVec3
 import at.petrak.hexcasting.api.spell.iota.Iota
-import at.petrak.hexcasting.xplat.IXplatAbstractions
+import miyucomics.hexical.casting.iota.getPigment
 import net.minecraft.util.math.ColorHelper
 import net.minecraft.util.math.Vec3d
 
 class OpSamplePigment : ConstMediaAction {
-	override val argc = 2
+	override val argc = 3
 	override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
-		val location = args.getVec3(0, argc)
-		val time = args.getDouble(1, argc)
-		val color = IXplatAbstractions.INSTANCE.getColorizer(ctx.caster).getColor(time.toFloat(), location)
+		val pigment = args.getPigment(0, argc)
+		val location = args.getVec3(1, argc)
+		val time = args.getDouble(2, argc)
+		val color = pigment.getColor(time.toFloat(), location)
 		return Vec3d(ColorHelper.Argb.getRed(color).toDouble() / 255.0, ColorHelper.Argb.getGreen(color).toDouble() / 255.0, ColorHelper.Argb.getBlue(color).toDouble() / 255.0).asActionResult
 	}
 }
