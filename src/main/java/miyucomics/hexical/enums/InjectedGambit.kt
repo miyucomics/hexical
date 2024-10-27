cf055ae379b4b9eccd72d8592f087e1bb33faabc
miyucomics <nbriannyc@gmail.com>
2024-10-27 10:24:36 -0400

    Code refactors

--- src/main/java/miyucomics/hexical/enums/InjectedGambit.kt
+++ src/main/java/miyucomics/hexical/enums/InjectedGambit.kt
@@ -1,7 +1,17 @@
 package miyucomics.hexical.enums
 
+import at.petrak.hexcasting.api.spell.SpellList
+import at.petrak.hexcasting.api.spell.casting.CastingHarness
+import at.petrak.hexcasting.api.spell.casting.eval.SpellContinuation
+import at.petrak.hexcasting.api.spell.iota.Iota
+
 enum class InjectedGambit {
 	THEMIS,
 	SISYPHUS,
 	NONE
+}
+
+interface FakeGambit {
+	fun breakDownwards(baseStack: List<Iota>, accumulator: MutableList<Iota>): Pair<Boolean, List<Iota>>
+	fun evaluate(continuation: SpellContinuation, harness: CastingHarness, data: SpellList, code: SpellList, baseStack: List<Iota>?, accumulator: MutableList<Iota>): CastingHarness.CastResult
 }
