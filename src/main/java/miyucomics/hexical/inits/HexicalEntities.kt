3269ff9ca74c784bb6ab09ebf451f03bfd934733
miyucomics <nbriannyc@gmail.com>
2025-01-04 16:47:54 -0500

    Documentation overhaul

--- src/main/java/miyucomics/hexical/inits/HexicalEntities.kt
+++ src/main/java/miyucomics/hexical/inits/HexicalEntities.kt
@@ -13,7 +13,7 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 
 object HexicalEntities {
-	val LIVING_SCROLL_ENTITY: EntityType<LivingScrollEntity> = EntityType.Builder.create(::LivingScrollEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":living_scroll")
+	val ANIMATED_SCROLL_ENTITY: EntityType<AnimatedScrollEntity> = EntityType.Builder.create(::AnimatedScrollEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":animated_scroll")
 	val MAGIC_MISSILE_ENTITY: EntityType<MagicMissileEntity> = EntityType.Builder.create(::MagicMissileEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(4).trackingTickInterval(20).build(HexicalMain.MOD_ID + ":magic_missile")
 	val SPIKE_ENTITY: EntityType<SpikeEntity> = EntityType.Builder.create(::SpikeEntity, SpawnGroup.MISC).setDimensions(1f, 1f).maxTrackingRange(10).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":spike")
 	val MESH_ENTITY: EntityType<MeshEntity> = EntityType.Builder.create(::MeshEntity, SpawnGroup.MISC).setDimensions(0.5f, 0.5f).maxTrackingRange(32).trackingTickInterval(1).build(HexicalMain.MOD_ID + ":mesh")
@@ -21,7 +21,7 @@ object HexicalEntities {
 
 	@JvmStatic
 	fun init() {
-		Registry.register(Registries.ENTITY_TYPE, HexicalMain.id("living_scroll"), LIVING_SCROLL_ENTITY)
+		Registry.register(Registries.ENTITY_TYPE, HexicalMain.id("animated_scroll"), ANIMATED_SCROLL_ENTITY)
 		Registry.register(Registries.ENTITY_TYPE, HexicalMain.id("magic_missile"), MAGIC_MISSILE_ENTITY)
 		Registry.register(Registries.ENTITY_TYPE, HexicalMain.id("spike"), SPIKE_ENTITY)
 		Registry.register(Registries.ENTITY_TYPE, HexicalMain.id("mesh"), MESH_ENTITY)
@@ -30,7 +30,7 @@ object HexicalEntities {
 
 	@JvmStatic
 	fun clientInit() {
-		EntityRendererRegistry.register(LIVING_SCROLL_ENTITY) { ctx -> LivingScrollRenderer(ctx) }
+		EntityRendererRegistry.register(ANIMATED_SCROLL_ENTITY) { ctx -> AnimatedScrollRenderer(ctx) }
 		EntityRendererRegistry.register(MAGIC_MISSILE_ENTITY) { ctx -> MagicMissileRenderer(ctx) }
 		EntityRendererRegistry.register(SPIKE_ENTITY) { ctx -> SpikeRenderer(ctx) }
 		EntityRendererRegistry.register(MESH_ENTITY) { ctx -> MeshRenderer(ctx) }
