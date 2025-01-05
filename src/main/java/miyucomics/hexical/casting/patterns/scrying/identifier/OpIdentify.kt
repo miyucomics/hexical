7fae7d46008f82fef8ec188e23050d5fa33aa5e7
miyucomics <nbriannyc@gmail.com>
2025-01-04 21:07:04 -0500

    Slowly rewriting the book

--- src/main/java/miyucomics/hexical/casting/patterns/scrying/identifier/OpIdentify.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/scrying/identifier/OpIdentify.kt
@@ -2,13 +2,13 @@ package miyucomics.hexical.casting.patterns.scrying.identifier
 
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getBlockPos
 import at.petrak.hexcasting.api.casting.iota.EntityIota
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import miyucomics.hexical.casting.iota.asActionResult
 import net.minecraft.registry.Registries
-import net.minecraft.util.math.BlockPos
 
 class OpIdentify : ConstMediaAction {
 	override val argc = 1
@@ -16,8 +16,9 @@ class OpIdentify : ConstMediaAction {
 		return when (val arg = args[0]) {
 			is EntityIota -> Registries.ENTITY_TYPE.getId(arg.entity.type).asActionResult()
 			is Vec3Iota -> {
-				env.assertVecInRange(arg.vec3)
-				Registries.BLOCK.getId(env.world.getBlockState(BlockPos(arg.vec3.x.toInt(), arg.vec3.y.toInt(), arg.vec3.z.toInt())).block).asActionResult()
+				val pos = args.getBlockPos(0, argc)
+				env.assertPosInRange(pos)
+				Registries.BLOCK.getId(env.world.getBlockState(pos).block).asActionResult()
 			}
 			else -> throw MishapInvalidIota.of(arg, 0, "identifiable")
 		}
