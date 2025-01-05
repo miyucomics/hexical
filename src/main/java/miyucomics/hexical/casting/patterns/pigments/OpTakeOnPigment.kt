fa7a434f49a844e994ba3b84e1f2138d26feb8c2
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-01-04 21:54:56 -0500

    Further work

--- src/main/java/miyucomics/hexical/casting/patterns/pigments/OpTakeOnPigment.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/pigments/OpTakeOnPigment.kt
@@ -3,12 +3,10 @@ package miyucomics.hexical.casting.patterns.pigments
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.misc.MediaConstants
 import miyucomics.hexical.casting.iota.getPigment
 
 class OpTakeOnPigment : ConstMediaAction {
 	override val argc = 1
-	override val mediaCost = MediaConstants.DUST_UNIT / 8
 	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
 		env.pigment = args.getPigment(0, 1)
 		return listOf()
