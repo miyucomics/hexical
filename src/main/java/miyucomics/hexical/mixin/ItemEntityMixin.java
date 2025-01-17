24b8bde4e6ade22b041f0fe5fc23cd53d7ce7c8c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-01-17 12:03:22 -0500

    New idea

--- src/main/java/miyucomics/hexical/mixin/ItemEntityMixin.java
+++ src/main/java/miyucomics/hexical/mixin/ItemEntityMixin.java
@@ -12,11 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
 	@Shadow public abstract ItemStack getStack();
-	@Shadow private int itemAge;
 
-	@Inject(method = "tick", at = @At("HEAD"))
+	@Inject(method = "onPlayerCollision", at = @At("HEAD"))
 	void deactivateDroppedLamp(CallbackInfo ci) {
-		if (this.getStack().getItem() == HexicalItems.ARCH_LAMP_ITEM && this.getStack().getNbt() != null && this.itemAge > 100)
+		if (this.getStack().getItem() == HexicalItems.ARCH_LAMP_ITEM && this.getStack().getNbt() != null)
 			this.getStack().getNbt().putBoolean("active", false);
 	}
 }
