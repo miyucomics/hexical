5016fd36240c4702f7ac6520a85965f604536db3
miyucomics <nbriannyc@gmail.com>
2025-01-20 13:38:56 -0500

    Archlamp data

--- src/main/java/miyucomics/hexical/casting/mishaps/NeedsArchGenieLamp.kt
+++ src/main/java/miyucomics/hexical/casting/mishaps/NeedsArchGenieLamp.kt
@@ -8,7 +8,7 @@ import miyucomics.hexical.HexicalMain
 import net.minecraft.text.Text
 import net.minecraft.util.DyeColor
 
-class NeedsActiveArchLampMishap : Mishap() {
+class NeedsArchGenieLamp : Mishap() {
 	override fun accentColor(env: CastingEnvironment, errorCtx: Context): FrozenPigment = dyeColor(DyeColor.RED)
 	override fun errorMessage(env: CastingEnvironment, errorCtx: Context): Text = error(HexicalMain.MOD_ID + ":active_arch_lamp")
 	override fun execute(env: CastingEnvironment, errorCtx: Context, stack: MutableList<Iota>) {
