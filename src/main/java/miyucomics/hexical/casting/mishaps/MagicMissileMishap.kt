fb3be73ebcfcbbecf50314453ed33a9e6aeb55cd
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-01-20 14:04:43 -0500

    Pedestal magic missile

---
+++ src/main/java/miyucomics/hexical/casting/mishaps/MagicMissileMishap.kt
@@ -0,0 +1,18 @@
+package miyucomics.hexical.casting.mishaps
+
+import at.petrak.hexcasting.api.casting.ParticleSpray
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.Mishap
+import at.petrak.hexcasting.api.pigment.FrozenPigment
+import miyucomics.hexical.HexicalMain
+import net.minecraft.text.Text
+import net.minecraft.util.DyeColor
+import net.minecraft.util.math.Vec3d
+
+class MagicMissileMishap : Mishap() {
+	override fun accentColor(env: CastingEnvironment, errorCtx: Context): FrozenPigment = dyeColor(DyeColor.RED)
+	override fun particleSpray(env: CastingEnvironment) = ParticleSpray.burst(env.mishapSprayPos(), 1.0)
+	override fun errorMessage(env: CastingEnvironment, errorCtx: Context): Text = error(HexicalMain.MOD_ID + ":magic_missile_coordinates")
+	override fun execute(env: CastingEnvironment, errorCtx: Context, stack: MutableList<Iota>) {}
+}
