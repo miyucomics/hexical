2272ae080666371dc16cf33377c3f1ce8be22b7c
miyucomics <nbriannyc@gmail.com>
2025-01-24 18:22:43 -0500

    Baby's first datagen

--- src/main/java/miyucomics/hexical/HexicalDatagen.kt
+++ src/main/java/miyucomics/hexical/HexicalDatagen.kt
@@ -1,5 +1,6 @@
 package miyucomics.hexical
 
+import miyucomics.hexical.inits.HexicalItems
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
@@ -8,16 +9,17 @@ import net.minecraft.data.client.*
 
 class HexicalDatagen : DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(generator: FabricDataGenerator) {
-
+		val pack = generator.createPack()
+		pack.addProvider(::HexicalModelGenerator)
 	}
 }
 
-private class MyModelGenerator(generator: FabricDataOutput) : FabricModelProvider(generator) {
+private class HexicalModelGenerator(generator: FabricDataOutput) : FabricModelProvider(generator) {
 	override fun generateBlockStateModels(generator: BlockStateModelGenerator) {
 
 	}
 
-	override fun generateItemModels(itemModelGenerator: ItemModelGenerator) {
-
+	override fun generateItemModels(generator: ItemModelGenerator) {
+		generator.registerCompass(HexicalItems.CONJURED_COMPASS_ITEM)
 	}
 }
