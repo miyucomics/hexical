9b21e183b173dd1d708cf8bec712d5824487bcc5
miyucomics <nbriannyc@gmail.com>
2025-02-01 12:19:59 -0500

    Cleanup

--- src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
+++ src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
@@ -1,10 +1,7 @@
 package miyucomics.hexical.blocks
 
 import miyucomics.hexical.inits.HexicalSounds
-import net.minecraft.block.BlockRenderType
-import net.minecraft.block.BlockState
-import net.minecraft.block.ShapeContext
-import net.minecraft.block.TransparentBlock
+import net.minecraft.block.*
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
@@ -25,11 +22,13 @@ class MediaJarBlock : TransparentBlock(
 		.sounds(BlockSoundGroup.GLASS)
 		.nonOpaque()
 		.solidBlock { _, _, _ -> false }
-) {
+), BlockEntityProvider {
 	override fun getRenderType(state: BlockState) = BlockRenderType.MODEL
 	override fun getAmbientOcclusionLightLevel(state: BlockState, world: BlockView, pos: BlockPos) = 1f
 	override fun getCameraCollisionShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.empty()
 	override fun getOutlineShape(state: BlockState, view: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.cuboid(4.0 / 16, 0.0, 4.0 / 16, 12.0 / 16, 10.0 / 16, 12.0 / 16)
+
+	override fun createBlockEntity(pos: BlockPos, state: BlockState) = MediaJarBlockEntity(pos, state)
 
 	override fun onUse(state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult {
 		if (world.isClient)
--- src/main/java/miyucomics/hexical/casting/patterns/wristpocket/OpGetWristpocketData.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/wristpocket/OpGetWristpocketData.kt
@@ -3,12 +3,9 @@ package miyucomics.hexical.casting.patterns.wristpocket
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.NullIota
 import miyucomics.hexical.casting.mishaps.NeedsWristpocketMishap
-import miyucomics.hexical.state.PersistentStateHandler
 import miyucomics.hexical.utils.WristpocketUtils
 import net.minecraft.item.ItemStack
-import net.minecraft.server.network.ServerPlayerEntity
 
 class OpGetWristpocketData(private val process: (ItemStack) -> List<Iota>) : ConstMediaAction {
 	override val argc = 0
