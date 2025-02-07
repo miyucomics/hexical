34568901fcf74faa00c9342a809563948f399940
miyucomics <nbriannyc@gmail.com>
2025-02-06 20:36:29 -0500

    Status effect

--- src/main/java/miyucomics/hexical/mixin/InGameHudMixin.java
+++ src/main/java/miyucomics/hexical/mixin/InGameHudMixin.java
@@ -1,8 +1,11 @@
 package miyucomics.hexical.mixin;
 
 import miyucomics.hexical.HexicalMain;
+import miyucomics.hexical.inits.HexicalEffects;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -17,6 +20,11 @@ public abstract class InGameHudMixin {
 
 	@Inject(method = "drawHeart", at = @At("HEAD"), cancellable = true)
 	private void amethystHearts(DrawContext context, InGameHud.HeartType type, int x, int y, int v, boolean blinking, boolean halfHeart, CallbackInfo ci) {
+		PlayerEntity player = MinecraftClient.getInstance().player;
+		if (player == null)
+			return;
+		if (!player.hasStatusEffect(HexicalEffects.WOOLEYED_EFFECT))
+			return;
 		if (type == InGameHud.HeartType.NORMAL) {
 			context.drawTexture(HEARTS, x, y, halfHeart ? 9 : 0, v, 9, 9);
 			ci.cancel();
