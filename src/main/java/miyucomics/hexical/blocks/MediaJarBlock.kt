6498b8c2a2542c486cfb66040306e6e2c6b3029f
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-02-09 10:34:03 -0500

    Limited size of media jar

--- src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
+++ src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
@@ -2,7 +2,6 @@ package miyucomics.hexical.blocks
 
 import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.api.utils.isMediaItem
-import at.petrak.hexcasting.interop.patchouli.PatchouliUtils.getRecipe
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexical.inits.HexicalEffects
 import miyucomics.hexical.inits.HexicalRecipe
@@ -18,7 +17,6 @@ import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
-import net.minecraft.sound.SoundEvents
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
@@ -27,6 +25,7 @@ import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import kotlin.math.min
 
 class MediaJarBlock : TransparentBlock(
 	Settings
@@ -41,7 +40,6 @@ class MediaJarBlock : TransparentBlock(
 	override fun getAmbientOcclusionLightLevel(state: BlockState, world: BlockView, pos: BlockPos) = 1f
 	override fun getCameraCollisionShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.empty()
 	override fun getOutlineShape(state: BlockState, view: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.cuboid(4.0 / 16, 0.0, 4.0 / 16, 12.0 / 16, 10.0 / 16, 12.0 / 16)
-
 	override fun createBlockEntity(pos: BlockPos, state: BlockState) = MediaJarBlockEntity(pos, state)
 
 	override fun onUse(state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult {
@@ -49,9 +47,9 @@ class MediaJarBlock : TransparentBlock(
 		val stack = player.getStackInHand(hand)
 		player.swingHand(hand)
 
-		if (isMediaItem(stack)) {
-			val mediaHolder = IXplatAbstractions.INSTANCE.findMediaHolder(stack)
-			jarData.addMedia(mediaHolder!!.media)
+		if (isMediaItem(stack) && jarData.getMedia() < MAX_CAPACITY) {
+			val mediaHolder = IXplatAbstractions.INSTANCE.findMediaHolder(stack.split(1))!!
+			jarData.setMedia(min(jarData.getMedia() + mediaHolder.media, MAX_CAPACITY))
 			mediaHolder.withdrawMedia(mediaHolder.media, false)
 			world.playSoundFromEntity(null, player, HexicalSounds.AMETHYST_MELT, SoundCategory.BLOCKS, 1f, 1f)
 			return ActionResult.SUCCESS
@@ -60,8 +58,8 @@ class MediaJarBlock : TransparentBlock(
 		val recipe = getRecipe(stack, world)
 		if (recipe != null && stack.count >= recipe.inputCount && jarData.withdrawMedia(recipe.cost)) {
 			stack.decrement(recipe.inputCount)
-			world.playSound(null, pos, HexicalSounds.ITEM_DUNKS, SoundCategory.BLOCKS, 1f, 1f)
 			recipe.output.forEach { reward -> player.giveItemStack(reward.copy()) }
+			world.playSound(null, pos, HexicalSounds.ITEM_DUNKS, SoundCategory.BLOCKS, 1f, 1f)
 			return ActionResult.SUCCESS
 		}
 
@@ -77,6 +75,8 @@ class MediaJarBlock : TransparentBlock(
 	}
 
 	companion object {
+		const val MAX_CAPACITY = MediaConstants.CRYSTAL_UNIT * 128
+
 		private fun getRecipe(input: ItemStack, world: World): TransmutingRecipe? {
 			world.recipeManager.listAllOfType(HexicalRecipe.TRANSMUTING_RECIPE).forEach { recipe ->
 				if (recipe.matches(SimpleInventory(input), world) && input.count >= recipe.inputCount)
--- src/main/java/miyucomics/hexical/casting/patterns/OpImitateParrot.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/OpImitateParrot.kt
@@ -3,23 +3,15 @@ package miyucomics.hexical.casting.patterns
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
-import at.petrak.hexcasting.api.casting.getBlockPos
-import at.petrak.hexcasting.api.casting.getList
 import at.petrak.hexcasting.api.casting.getVec3
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.PatternIota
-import at.petrak.hexcasting.api.casting.math.HexPattern
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import at.petrak.hexcasting.api.misc.MediaConstants
-import at.petrak.hexcasting.common.msgs.MsgNewSpiralPatternsS2C
-import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexical.casting.iotas.getIdentifier
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.passive.ParrotEntity
 import net.minecraft.registry.Registries
-import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.sound.SoundCategory
-import net.minecraft.util.Identifier
 import net.minecraft.util.math.Vec3d
 
 class OpImitateParrot : SpellAction {
