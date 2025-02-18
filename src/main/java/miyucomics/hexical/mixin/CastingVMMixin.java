badc82136f2878db64be0fe6b4dd91c6533dca69
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-02-18 11:17:37 -0500

    Fixed ledgers

--- src/main/java/miyucomics/hexical/mixin/CastingVMMixin.java
+++ src/main/java/miyucomics/hexical/mixin/CastingVMMixin.java
@@ -1,14 +1,25 @@
 package miyucomics.hexical.mixin;
 
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
 import at.petrak.hexcasting.api.casting.eval.ExecutionClientView;
+import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv;
+import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
+import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
 import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
 import at.petrak.hexcasting.api.casting.iota.Iota;
+import com.llamalad7.mixinextras.sugar.Local;
+import miyucomics.hexical.state.LedgerData;
+import miyucomics.hexical.state.PersistentStateHandler;
 import miyucomics.hexical.utils.InjectionHelper;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(value = CastingVM.class, priority = 100, remap = false)
 public class CastingVMMixin {
@@ -17,5 +28,16 @@ public class CastingVMMixin {
 		ExecutionClientView view = InjectionHelper.handleGrimoire((CastingVM) (Object) this, iota, world);
 		if (view != null)
 			cir.setReturnValue(view);
+	}
+
+	@Inject(method = "performSideEffects", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/casting/eval/sideeffects/OperatorSideEffect;performEffect(Lat/petrak/hexcasting/api/casting/eval/vm/CastingVM;)V"))
+	void captureStack(List<? extends OperatorSideEffect> sideEffects, CallbackInfo ci, @Local OperatorSideEffect sideEffect) {
+		if (sideEffect instanceof OperatorSideEffect.DoMishap) {
+			CastingVM vm = (CastingVM) (Object) this;
+			CastingEnvironment env = vm.getEnv();
+			if (!(env instanceof PlayerBasedCastEnv))
+				return;
+			PersistentStateHandler.getLedger((ServerPlayerEntity) env.getCastingEntity()).saveStack(vm.getImage().getStack());
+		}
 	}
 }
