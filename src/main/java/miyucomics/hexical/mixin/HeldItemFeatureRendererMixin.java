497b0d691f69873b7d85e659f93ecec14279d492
miyucomics <nbriannyc@gmail.com>
2025-02-18 16:08:10 -0500

    Fixed confetti

---
+++ src/main/java/miyucomics/hexical/mixin/HeldItemFeatureRendererMixin.java
@@ -0,0 +1,66 @@
+package miyucomics.hexical.mixin;
+
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
+import at.petrak.hexcasting.api.casting.eval.ExecutionClientView;
+import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv;
+import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
+import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
+import at.petrak.hexcasting.api.casting.iota.Iota;
+import at.petrak.hexcasting.xplat.IXplatAbstractions;
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.kosmx.playerAnim.core.util.Vec3f;
+import miyucomics.hexical.state.EvokeState;
+import miyucomics.hexical.state.PersistentStateHandler;
+import miyucomics.hexical.utils.InjectionHelper;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.HeldItemFeatureRenderer;
+import net.minecraft.client.render.entity.model.ModelWithArms;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(value = HeldItemFeatureRenderer.class)
+public class HeldItemFeatureRendererMixin {
+	@Inject(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"))
+	void spawnEvokingParticles(LivingEntity entity, ItemStack stack, ModelTransformationMode modelTransformationMode, Arm arm, MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+//		if (entity instanceof ClientPlayerEntity player && EvokeState.isEvoking(entity.getUuid())) {
+//			ClientWorld world = (ClientWorld) entity.getWorld();
+//
+//			matrices.push();
+//			((HeldItemFeatureRenderer<?, ?>) (Object) this).getContextModel().setArmAngle(arm, matrices);
+//			matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90.0f));
+//			matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(180.0f));
+//			boolean bl = arm == Arm.LEFT;
+//			matrices.translate((float)(bl ? -1 : 1) / 16.0f, 0.125f, -0.625f);
+//
+//			Vector3f position = matrices.peek().getPositionMatrix().transformPosition(new Vector3f(0, 0, 0)).add((float) entity.getX(), (float) entity.getY(), (float) entity.getZ());
+//
+//			int color = IXplatAbstractions.INSTANCE.getPigment(player).getColorProvider().getColor(world.getTime() * 10, player.getPos());
+//			float r = ColorHelper.Argb.getRed(color) / 255f;
+//			float g = ColorHelper.Argb.getGreen(color) / 255f;
+//			float b = ColorHelper.Argb.getBlue(color) / 255f;
+//			world.addParticle(ParticleTypes.ENTITY_EFFECT, position.x, position.y, position.z, r, g, b);
+//
+//			matrices.pop();
+//		}
+	}
+}
