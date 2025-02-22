6d54fcad2092e573f29fead2b31942998e5095ae
miyucomics <nbriannyc@gmail.com>
2025-02-22 15:58:58 -0500

    Cleaned up some shader and rendering code

--- src/main/java/miyucomics/hexical/casting/patterns/OpCrackDevice.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/OpCrackDevice.kt
@@ -6,14 +6,9 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadOffhandItem
 import at.petrak.hexcasting.api.misc.MediaConstants
-import at.petrak.hexcasting.api.utils.getOrCreateList
-import at.petrak.hexcasting.api.utils.putCompound
 import at.petrak.hexcasting.common.items.magic.ItemPackagedHex
-import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexical.casting.mishaps.IllegalJailbreakMishap
 import net.minecraft.item.ItemStack
-import net.minecraft.nbt.NbtCompound
-import net.minecraft.server.network.ServerPlayerEntity
 
 class OpCrackDevice : SpellAction {
 	override val argc = 0
