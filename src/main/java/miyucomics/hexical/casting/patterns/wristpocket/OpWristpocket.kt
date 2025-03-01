f2d338167f2bdbb87ad60cc47d303af519f158c8
miyucomics <nbriannyc@gmail.com>
2025-02-28 22:44:16 -0500

    Player stores data now

--- src/main/java/miyucomics/hexical/casting/patterns/wristpocket/OpWristpocket.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/wristpocket/OpWristpocket.kt
@@ -6,7 +6,7 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster
 import at.petrak.hexcasting.api.misc.MediaConstants
-import miyucomics.hexical.state.PersistentStateHandler
+import miyucomics.hexical.interfaces.PlayerEntityMinterface
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Hand
 
@@ -22,8 +22,8 @@ class OpWristpocket : SpellAction {
 	private data class Spell(val hand: Hand) : RenderedSpell {
 		override fun cast(env: CastingEnvironment) {
 			val caster = env.castingEntity as ServerPlayerEntity
-			val item = PersistentStateHandler.getWristpocketStack(caster)
-			PersistentStateHandler.setWristpocketStack(caster, caster.getStackInHand(hand))
+			val item = (caster as PlayerEntityMinterface).getWristpocket()
+			(caster as PlayerEntityMinterface).setWristpocket(caster.getStackInHand(hand))
 			caster.setStackInHand(hand, item)
 		}
 	}
--- src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
+++ src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
@@ -5,7 +5,10 @@ import miyucomics.hexical.interfaces.PlayerEntityMinterface;
 import miyucomics.hexical.state.EvokeState;
 import miyucomics.hexical.utils.CastingUtils;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,9 +16,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin implements PlayerEntityMinterface {
+public abstract class PlayerEntityMixin implements PlayerEntityMinterface {
 	@Unique
 	private boolean hexical$archLampCastedThisTick = false;
+	@Unique
+	private NbtCompound hexical$evocation = new NbtCompound();
+	@Unique
+	private ItemStack hexical$wristpocket = ItemStack.EMPTY;
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	void tick(CallbackInfo ci) {
@@ -31,6 +38,24 @@ public class PlayerEntityMixin implements PlayerEntityMinterface {
 		hexical$archLampCastedThisTick = false;
 	}
 
+	@Inject(method = "readCustomDataFromNbt", at = @At("HEAD"))
+	void reaadPlayerData(NbtCompound nbtCompound, CallbackInfo ci) {
+		if (nbtCompound.contains("evocation"))
+			hexical$evocation = nbtCompound.getCompound("evocation");
+
+		hexical$wristpocket = ItemStack.fromNbt(nbtCompound.getCompound("wristpocket"));
+	}
+
+	@Inject(method = "writeCustomDataToNbt", at = @At("HEAD"))
+	void writePlayerData(NbtCompound nbtCompound, CallbackInfo ci) {
+		if (hexical$evocation != null)
+			nbtCompound.put("evocation", hexical$evocation);
+
+		NbtCompound wristpocket = new NbtCompound();
+		hexical$wristpocket.writeNbt(wristpocket);
+		nbtCompound.put("wristpocket", wristpocket);
+	}
+
 	public boolean getArchLampCastedThisTick() {
 		return hexical$archLampCastedThisTick;
 	}
@@ -38,5 +63,25 @@ public class PlayerEntityMixin implements PlayerEntityMinterface {
 	@Override
 	public void archLampCasted() {
 		hexical$archLampCastedThisTick = true;
+	}
+
+	@Override
+	public @NotNull ItemStack getWristpocket() {
+		return hexical$wristpocket;
+	}
+
+	@Override
+	public void setWristpocket(@NotNull ItemStack stack) {
+		hexical$wristpocket = stack;
+	}
+
+	@Override
+	public NbtCompound getEvocation() {
+		return hexical$evocation;
+	}
+
+	@Override
+	public void setEvocation(@NotNull NbtCompound hex) {
+		hexical$evocation = hex;
 	}
 }
