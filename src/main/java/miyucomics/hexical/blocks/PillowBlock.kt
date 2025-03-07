8d44f26f1f705d7086e53b5b683df3914483a9f7
miyucomics <nbriannyc@gmail.com>
2025-03-07 18:22:05 -0500

    Pillow

---
+++ src/main/java/miyucomics/hexical/blocks/PillowBlock.kt
@@ -0,0 +1,13 @@
+package miyucomics.hexical.blocks
+
+import net.minecraft.block.*
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+class PillowBlock : Block(Settings.copy(Blocks.DEEPSLATE_TILES).strength(4f, 4f)), BlockEntityProvider {
+	override fun createBlockEntity(pos: BlockPos, state: BlockState): BlockEntity = PillowBlockEntity(pos, state)
+	override fun <T : BlockEntity> getTicker(world: World, state: BlockState, type: BlockEntityType<T>): BlockEntityTicker<T> = BlockEntityTicker { world1, pos, _, blockEntity -> (blockEntity as PillowBlockEntity).tick(world1, pos) }
+}
---
+++ src/main/java/miyucomics/hexical/blocks/PillowBlockEntityRenderer.kt
@@ -0,0 +1,28 @@
+package miyucomics.hexical.blocks
+
+import miyucomics.hexical.inits.HexicalItems
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.WorldRenderer
+import net.minecraft.client.render.block.entity.BlockEntityRenderer
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory
+import net.minecraft.client.render.model.json.ModelTransformationMode
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.item.ItemStack
+import net.minecraft.util.math.RotationAxis
+
+class PillowBlockEntityRenderer(ctx: BlockEntityRendererFactory.Context) : BlockEntityRenderer<PillowBlockEntity> {
+	private val swordStack: ItemStack = ItemStack(HexicalItems.HAND_LAMP_ITEM)
+
+	override fun render(entity: PillowBlockEntity, tickDelta: Float, matrices: MatrixStack, vertexConsumers: VertexConsumerProvider, light: Int, overlay: Int) {
+		matrices.push()
+		matrices.translate(0.5, 1.0, 0.5)
+		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(entity.currentYaw))
+		matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(entity.currentPitch))
+		matrices.scale(1.5f, 1.5f, 1.5f)
+
+		val lightAbove = WorldRenderer.getLightmapCoordinates(entity.world, entity.pos.up())
+		MinecraftClient.getInstance().itemRenderer.renderItem(swordStack, ModelTransformationMode.GROUND, lightAbove, overlay, matrices, vertexConsumers, entity.world, 0)
+		matrices.pop()
+	}
+}
