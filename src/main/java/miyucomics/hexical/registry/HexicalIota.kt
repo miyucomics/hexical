b5f478ebe434538b2dfb4e3e2c0691c0c083b715
miyucomics <nbriannyc@gmail.com>
2025-03-29 18:29:45 -0400

    Ripped out scrying

--- src/main/java/miyucomics/hexical/registry/HexicalIota.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalIota.kt
@@ -3,7 +3,6 @@ package miyucomics.hexical.registry
 import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
 import miyucomics.hexical.HexicalMain
 import miyucomics.hexical.casting.iotas.DyeIota
-import miyucomics.hexical.casting.iotas.IdentifierIota
 import miyucomics.hexical.casting.iotas.PigmentIota
 import net.minecraft.registry.Registry
 
@@ -11,7 +10,6 @@ object HexicalIota {
 	@JvmStatic
 	fun init() {
 		Registry.register(HexIotaTypes.REGISTRY, HexicalMain.id("dye"), DyeIota.TYPE)
-		Registry.register(HexIotaTypes.REGISTRY, HexicalMain.id("identifier"), IdentifierIota.TYPE)
 		Registry.register(HexIotaTypes.REGISTRY, HexicalMain.id("pigment"), PigmentIota.TYPE)
 	}
 }
