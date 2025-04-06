7c93bfc2f2063032b22f74d0a7c8e64da5fad710
miyucomics <nbriannyc@gmail.com>
2025-04-06 12:28:27 -0400

    Periwinkle :D

--- src/main/java/miyucomics/hexical/mixin/CastingEnvironmentMixin.java
+++ src/main/java/miyucomics/hexical/mixin/CastingEnvironmentMixin.java
@@ -1,31 +1,14 @@
 package miyucomics.hexical.mixin;
 
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
-import at.petrak.hexcasting.api.casting.eval.ExecutionClientView;
-import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv;
-import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect;
-import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
-import at.petrak.hexcasting.api.casting.iota.Iota;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.sugar.Local;
-import miyucomics.hexical.data.LedgerData;
-import miyucomics.hexical.registry.HexicalEffects;
-import miyucomics.hexical.utils.InjectionHelper;
+import miyucomics.hexical.registry.HexicalPotions;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.List;
 
 @Mixin(value = CastingEnvironment.class, remap = false)
 public abstract class CastingEnvironmentMixin {
@@ -35,7 +18,7 @@ public abstract class CastingEnvironmentMixin {
 	private boolean canOvercast(Operation<Boolean> original) {
 		if (this.getCastingEntity() == null)
 			return original.call();
-		StatusEffectInstance wooleye = this.getCastingEntity().getStatusEffect(HexicalEffects.WOOLEYED_EFFECT);
+		StatusEffectInstance wooleye = this.getCastingEntity().getStatusEffect(HexicalPotions.WOOLEYED_EFFECT);
 		if (wooleye == null)
 			return original.call();
 		System.out.println(wooleye.getAmplifier());
