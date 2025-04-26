abae6c3783773c3ee105f7f2d11c6cc456a12195
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-04-26 15:55:29 -0400

    Prestidigitation rework

--- src/main/java/miyucomics/hexical/registry/HexicalData.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalData.kt
@@ -1,7 +1,7 @@
 package miyucomics.hexical.registry
 
 import miyucomics.hexical.data.DyeData
-import miyucomics.hexical.data.PrestidigitationData
+import miyucomics.hexical.data.prestidigitation.PrestidigitationData
 
 object HexicalData {
 	fun init() {
