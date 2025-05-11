521372ab4f2561ddaf7000d772d9a08fa8d6d6c6
miyucomics <nbriannyc@gmail.com>
2025-05-11 16:09:39 -0400

    Sodium is so mean

---
+++ src/main/java/miyucomics/hexical/casting/patterns/OpHorrible.kt
@@ -0,0 +1,30 @@
+package miyucomics.hexical.casting.patterns
+
+import at.petrak.hexcasting.api.casting.RenderedSpell
+import at.petrak.hexcasting.api.casting.castables.SpellAction
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getVec3
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
+import at.petrak.hexcasting.api.misc.MediaConstants
+import miyucomics.hexposition.iotas.getIdentifier
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.passive.ParrotEntity
+import net.minecraft.registry.Registries
+import net.minecraft.sound.SoundCategory
+import net.minecraft.util.math.Vec3d
+
+class OpHorrible : SpellAction {
+	override val argc = 2
+	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
+		val pos = args.getVec3(0, argc)
+		env.assertVecInRange(pos)
+		return SpellAction.Result(Spell(pos), 0, listOf())
+	}
+
+	private data class Spell(val pos: Vec3d) : RenderedSpell {
+		override fun cast(env: CastingEnvironment) {
+
+		}
+	}
+}
