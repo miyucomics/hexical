7acb1f18ca413c19b52d7197e4ac30bee312611d
miyucomics <nbriannyc@gmail.com>
2025-06-03 18:13:21 -0400

    Strived for elegance?

--- src/main/java/miyucomics/hexical/HexicalCardinalComponents.kt
+++ src/main/java/miyucomics/hexical/HexicalCardinalComponents.kt
@@ -3,12 +3,20 @@ package miyucomics.hexical
 import at.petrak.hexcasting.api.addldata.ADMediaHolder
 import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.fabric.cc.HexCardinalComponents
+import at.petrak.hexcasting.fabric.cc.adimpl.CCIotaHolder
 import at.petrak.hexcasting.fabric.cc.adimpl.CCMediaHolder
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer
 import dev.onyxstudios.cca.api.v3.item.ItemComponentFactoryRegistry
 import dev.onyxstudios.cca.api.v3.item.ItemComponentInitializer
+import miyucomics.hexical.entities.AnimatedScrollEntity
 import miyucomics.hexical.registry.HexicalItems
 
-class HexicalCardinalComponents : ItemComponentInitializer {
+class HexicalCardinalComponents : EntityComponentInitializer, ItemComponentInitializer {
+	override fun registerEntityComponentFactories(registry: EntityComponentFactoryRegistry) {
+		registry.registerFor(AnimatedScrollEntity::class.java, HexCardinalComponents.IOTA_HOLDER) { entity -> CCIotaHolder }
+	}
+
 	override fun registerItemComponentFactories(registry: ItemComponentFactoryRegistry) {
 		registry.register(HexicalItems.HEX_GUMMY, HexCardinalComponents.MEDIA_HOLDER) { stack -> CCMediaHolder.Static({ MediaConstants.DUST_UNIT / 10 }, ADMediaHolder.AMETHYST_DUST_PRIORITY, stack) }
 	}
