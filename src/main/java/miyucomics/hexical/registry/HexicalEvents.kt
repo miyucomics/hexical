f7dcdff7e273a8d874c62ef1f40a951ac2a6958a
miyucomics <nbriannyc@gmail.com>
2025-06-06 16:45:18 -0400

    Cleaned up tooltip code

--- src/main/java/miyucomics/hexical/registry/HexicalEvents.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalEvents.kt
@@ -2,6 +2,12 @@ package miyucomics.hexical.registry
 
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv
+import at.petrak.hexcasting.api.casting.iota.IotaType
+import at.petrak.hexcasting.api.misc.MediaConstants
+import at.petrak.hexcasting.api.pigment.FrozenPigment
+import at.petrak.hexcasting.api.utils.mediaBarColor
+import at.petrak.hexcasting.common.items.magic.ItemMediaHolder
+import at.petrak.hexcasting.common.items.magic.ItemPackagedHex
 import miyucomics.hexical.casting.components.LedgerRecordComponent
 import miyucomics.hexical.casting.components.SentinelBedComponent
 import miyucomics.hexical.client.ClientStorage
@@ -10,19 +16,84 @@ import miyucomics.hexical.data.EvokeState
 import miyucomics.hexical.data.KeybindData
 import miyucomics.hexical.data.LesserSentinelState
 import miyucomics.hexical.interfaces.PlayerEntityMinterface
+import miyucomics.hexical.utils.CharmedItemUtilities
+import miyucomics.hexical.utils.CharmedItemUtilities.getMaxMedia
+import miyucomics.hexical.utils.CharmedItemUtilities.getMedia
+import miyucomics.hexical.utils.CharmedItemUtilities.isStackCharmed
 import miyucomics.hexical.utils.HexagonRendering
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.text.Text
+import net.minecraft.text.TextColor
+import net.minecraft.util.Formatting
+import net.minecraft.util.math.Vec3d
+import java.text.DecimalFormat
+import java.util.function.Consumer
 
 object HexicalEvents {
+	private val format = DecimalFormat("###,###.##")
+
 	@JvmStatic
 	fun init() {
 		LesserSentinelState.registerServerReciever()
+
+		ItemTooltipCallback.EVENT.register { stack, context, lines ->
+			val nbt = stack.nbt ?: return@register
+			if (stack.isOf(HexicalBlocks.MEDIA_JAR_ITEM))
+				lines.add(Text.translatable("hexcasting.tooltip.media", format.format(((nbt.getCompound("BlockEntityTag").getLong("media").toFloat()) / (MediaConstants.DUST_UNIT.toFloat())).toDouble())).styled { style -> style.withColor(ItemMediaHolder.HEX_COLOR) })
+		}
+
+		ItemTooltipCallback.EVENT.register { stack, context, lines ->
+			if (!isStackCharmed(stack))
+				return@register
+			val media = getMedia(stack)
+			val maxMedia = getMaxMedia(stack)
+			lines.add(Text.translatable("hexical.charmed").styled { style -> style.withColor(ItemMediaHolder.HEX_COLOR) })
+			lines.add(Text.translatable("hexcasting.tooltip.media_amount.advanced",
+				Text.literal(CharmedItemUtilities.DUST_AMOUNT.format((media / MediaConstants.DUST_UNIT.toFloat()).toDouble())).styled { style -> style.withColor(ItemMediaHolder.HEX_COLOR) },
+				Text.translatable("hexcasting.tooltip.media", CharmedItemUtilities.DUST_AMOUNT.format((maxMedia / MediaConstants.DUST_UNIT.toFloat()).toDouble())).styled { style -> style.withColor(ItemMediaHolder.HEX_COLOR) },
+				Text.literal(CharmedItemUtilities.PERCENTAGE.format((100f * media / maxMedia).toDouble()) + "%").styled { style -> style.withColor(TextColor.fromRgb(mediaBarColor(media, maxMedia))) }
+			))
+		}
+
+		ItemTooltipCallback.EVENT.register { stack, context, lines ->
+			val nbt = stack.nbt ?: return@register
+			if (stack.item !is ItemPackagedHex || !nbt.getBoolean("cracked"))
+				return@register
+
+			lines.add(Text.translatable("hexical.cracked.cracked").formatted(Formatting.GOLD))
+			if (nbt.contains(ItemPackagedHex.TAG_PROGRAM)) {
+				val text = Text.empty()
+				val entries = nbt.getList(ItemPackagedHex.TAG_PROGRAM, NbtElement.COMPOUND_TYPE.toInt())
+				entries.forEach { text.append(IotaType.getDisplay(it as NbtCompound)) }
+				lines.add(Text.translatable("hexical.cracked.program").append(text))
+			}
+		}
+
+		ItemTooltipCallback.EVENT.register { stack, context, lines ->
+			val nbt = stack.nbt ?: return@register
+			if (!nbt.contains("autographs"))
+				return@register
+
+			lines.add(Text.translatable("hexical.autograph.header").styled { style -> style.withColor(Formatting.GRAY) })
+
+			nbt.getList("autographs", NbtCompound.COMPOUND_TYPE.toInt()).forEach(Consumer { element: NbtElement? ->
+				val compound = element as NbtCompound
+				val name = compound.getString("name")
+				val pigment = FrozenPigment.fromNBT(compound.getCompound("pigment")).colorProvider
+				val output = Text.literal("")
+				for (i in 0 until name.length)
+					output.append(Text.literal(name[i].toString()).styled { style -> style.withColor(pigment.getColor((ClientStorage.ticks * 3).toFloat(), Vec3d(0.0, i.toDouble(), 0.0))) })
+				lines.add(output)
+			})
+		}
 
 		CastingEnvironment.addCreateEventListener { env: CastingEnvironment, _: NbtCompound ->
 			env.addExtension(SentinelBedComponent(env))
