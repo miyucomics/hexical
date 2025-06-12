a606c84615853cd376cf3b1ace7204da80238dba
miyucomics <nbriannyc@gmail.com>
2025-06-12 19:23:41 -0400

    Changed rotate to radians

--- src/main/java/miyucomics/hexical/casting/patterns/rotate/OpRotateEntity.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/rotate/OpRotateEntity.kt
@@ -1,4 +1,4 @@
-package miyucomics.hexical.casting.patterns
+package miyucomics.hexical.casting.patterns.rotate
 
 import at.petrak.hexcasting.api.casting.ParticleSpray
 import at.petrak.hexcasting.api.casting.RenderedSpell
@@ -10,29 +10,30 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.misc.MediaConstants
 import net.minecraft.entity.Entity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.math.MathHelper
 
 class OpRotateEntity : SpellAction {
 	override val argc = 3
 	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
 		val target = args.getEntity(0, argc)
-		val yaw = args.getDouble(1, argc)
-		val pitch = args.getDouble(2, argc)
+		val yaw = args.getDouble(1, argc) * MathHelper.DEGREES_PER_RADIAN
+		val pitch = args.getDouble(2, argc) * MathHelper.DEGREES_PER_RADIAN
 		var cost = MediaConstants.DUST_UNIT / 2
 		if (target is PlayerEntity)
 			cost = MediaConstants.CRYSTAL_UNIT
 		if (target == env.castingEntity)
 			cost = 0
-		return SpellAction.Result(Spell(target, yaw, pitch), cost, listOf(ParticleSpray.burst(target.eyePos, 1.0)))
+		return SpellAction.Result(Spell(target, yaw.toFloat(), pitch.toFloat()), cost, listOf(ParticleSpray.burst(target.eyePos, 1.0)))
 	}
 
-	private data class Spell(val target: Entity, val yaw: Double, val pitch: Double) : RenderedSpell {
+	private data class Spell(val target: Entity, val yaw: Float, val pitch: Float) : RenderedSpell {
 		override fun cast(env: CastingEnvironment) {
 			target.prevPitch = target.pitch
 			target.prevYaw = target.yaw
-			target.bodyYaw += (yaw * 360).toFloat()
-			target.yaw += (yaw * 360).toFloat()
-			target.headYaw += (yaw * 360).toFloat()
-			target.pitch += (pitch * 360).toFloat()
+			target.bodyYaw += yaw
+			target.yaw += yaw
+			target.headYaw += yaw
+			target.pitch += pitch
 		}
 	}
 }
