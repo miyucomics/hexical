d9c88913f4f3c90d9db4978557030425952f94d1
miyucomics <nbriannyc@gmail.com>
2025-06-26 09:52:08 -0400

    Cookie

--- src/main/java/miyucomics/hexical/casting/patterns/OpHorrible.kt
+++ src/main/java/miyucomics/hexical/casting/patterns/OpHorrible.kt
@@ -1,10 +1,14 @@
 package miyucomics.hexical.casting.patterns
 
+import at.petrak.hexcasting.api.casting.ParticleSpray
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getVec3
 import at.petrak.hexcasting.api.casting.iota.Iota
+import net.minecraft.entity.ItemEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.util.math.Vec3d
 
 class OpHorrible : SpellAction {
@@ -12,12 +16,14 @@ class OpHorrible : SpellAction {
 	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
 		val pos = args.getVec3(0, argc)
 		env.assertVecInRange(pos)
-		return SpellAction.Result(Spell(pos), 0, listOf())
+		return SpellAction.Result(Spell(pos), 0, listOf(ParticleSpray.burst(pos, 2.0)))
 	}
 
 	private data class Spell(val pos: Vec3d) : RenderedSpell {
 		override fun cast(env: CastingEnvironment) {
-
+			val cookie = ItemEntity(env.world, pos.x, pos.y, pos.z, ItemStack(Items.COOKIE), 0.0, 0.0, 0.0)
+			cookie.setNoGravity(true)
+			env.world.spawnEntity(cookie)
 		}
 	}
 }
