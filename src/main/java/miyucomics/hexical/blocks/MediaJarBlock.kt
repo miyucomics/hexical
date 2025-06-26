06f7ba2f9d0a31e04502c924553ecd28c65f67c4
miyucomics <nbriannyc@gmail.com>
2025-06-26 17:49:05 -0400

    No replacing my jars

--- src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
+++ src/main/java/miyucomics/hexical/blocks/MediaJarBlock.kt
@@ -10,33 +10,46 @@ import miyucomics.hexical.registry.HexicalSounds
 import net.minecraft.block.*
 import net.minecraft.entity.ItemEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.fluid.FluidState
+import net.minecraft.fluid.Fluids
 import net.minecraft.inventory.SimpleInventory
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.sound.SoundCategory
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import net.minecraft.world.WorldAccess
 
-class MediaJarBlock : TransparentBlock(
+class MediaJarBlock : Block(
 	Settings
 		.create()
 		.emissiveLighting { _, _, _ -> true }
 		.luminance { _ -> 15 }
 		.sounds(BlockSoundGroup.GLASS)
 		.nonOpaque()
-		.solidBlock { _, _, _ -> false }
-), BlockEntityProvider {
-	override fun getRenderType(state: BlockState) = BlockRenderType.MODEL
-	override fun getAmbientOcclusionLightLevel(state: BlockState, world: BlockView, pos: BlockPos) = 1f
-	override fun getCameraCollisionShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.empty()
+), BlockEntityProvider, Waterloggable {
+	init {
+		this.defaultState = this.stateManager.getDefaultState().with(WATERLOGGED, false)
+	}
+
 	override fun getOutlineShape(state: BlockState, view: BlockView, pos: BlockPos, context: ShapeContext): VoxelShape = VoxelShapes.cuboid(3.0 / 16, 0.0, 3.0 / 16, 13.0 / 16, 14.0 / 16, 13.0 / 16)
 	override fun createBlockEntity(pos: BlockPos, state: BlockState) = MediaJarBlockEntity(pos, state)
+
+	override fun getPlacementState(itemPlacementContext: ItemPlacementContext): BlockState? {
+		val fluidState = itemPlacementContext.world.getFluidState(itemPlacementContext.blockPos)
+		return this.defaultState.with(LanternBlock.WATERLOGGED, fluidState.fluid === Fluids.WATER)
+	}
 
 	override fun onBreak(world: World, blockPos: BlockPos, blockState: BlockState?, playerEntity: PlayerEntity) {
 		val blockEntity = world.getBlockEntity(blockPos)
@@ -76,7 +89,24 @@ class MediaJarBlock : TransparentBlock(
 		return ActionResult.FAIL
 	}
 
+	override fun getStateForNeighborUpdate(blockState: BlockState, direction: Direction, blockState2: BlockState, worldAccess: WorldAccess, blockPos: BlockPos, blockPos2: BlockPos): BlockState {
+		if (blockState.get(WATERLOGGED))
+			worldAccess.scheduleFluidTick(blockPos, Fluids.WATER, Fluids.WATER.getTickRate(worldAccess))
+		return super.getStateForNeighborUpdate(blockState, direction, blockState2, worldAccess, blockPos, blockPos2)
+	}
+
+	override fun getFluidState(blockState: BlockState): FluidState? {
+		if (blockState.get(WATERLOGGED))
+			return Fluids.WATER.getStill(false)
+		return super.getFluidState(blockState)
+	}
+
+	override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+		builder.add(WATERLOGGED)
+	}
+
 	companion object {
+		val WATERLOGGED: BooleanProperty = Properties.WATERLOGGED
 		const val MAX_CAPACITY = MediaConstants.CRYSTAL_UNIT * 64
 
 		private fun getRecipe(input: ItemStack, world: World): TransmutingRecipe? {
