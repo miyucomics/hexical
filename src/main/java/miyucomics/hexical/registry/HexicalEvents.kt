9872724701b23e3fa62f9bf6f5f04a4cfc96b2e2
miyucomics <nbriannyc@gmail.com>
2025-06-29 08:08:35 -0400

    Moved client events to client

--- src/main/java/miyucomics/hexical/registry/HexicalEvents.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalEvents.kt
@@ -59,6 +59,46 @@ object HexicalEvents {
 	fun init() {
 		LesserSentinelState.registerServerReciever()
 
+		CastingEnvironment.addCreateEventListener { env: CastingEnvironment, _: NbtCompound ->
+			env.addExtension(SentinelBedComponent(env))
+			if (env is PlayerBasedCastEnv)
+				env.addExtension(LedgerRecordComponent(env))
+		}
+
+		ServerPlayerEvents.AFTER_RESPAWN.register { oldPlayer, newPlayer, alive ->
+			if (!alive)
+				ShaderRenderer.setEffect(null)
+			(newPlayer as PlayerEntityMinterface).setLesserSentinels((oldPlayer as PlayerEntityMinterface).getLesserSentinels())
+			(newPlayer as PlayerEntityMinterface).setEvocation((oldPlayer as PlayerEntityMinterface).getEvocation())
+			(newPlayer as PlayerEntityMinterface).setWristpocket((oldPlayer as PlayerEntityMinterface).getWristpocket())
+		}
+
+		ServerPlayConnectionEvents.DISCONNECT.register { handler, _ ->
+			val player = handler.player.uuid
+			EvokeState.active[player] = false
+			if (KeybindData.active.containsKey(player)) {
+				for (key in KeybindData.active[player]!!.keys) {
+					KeybindData.active[player]!![key] = false
+					KeybindData.duration[player]!![key] = 0
+				}
+			}
+		}
+
+		ServerTickEvents.END_SERVER_TICK.register {
+			for (player in EvokeState.active.keys)
+				if (EvokeState.active[player]!!)
+					EvokeState.duration[player] = EvokeState.duration[player]!! - 1
+			for (player in KeybindData.duration.keys) {
+				val binds = KeybindData.active[player]!!
+				for (key in binds.keys)
+					if (KeybindData.active[player]!!.getOrDefault(key, false))
+						KeybindData.duration[player]!![key] = KeybindData.duration[player]!![key]!! + 1
+			}
+		}
+	}
+
+	@JvmStatic
+	fun clientInit() {
 		MouseScrollCallback.EVENT.register { delta ->
 			if (HexicalKeybinds.TELEPATHY_KEYBIND.isPressed) {
 				val buf = PacketByteBufs.create()
@@ -114,46 +154,6 @@ object HexicalEvents {
 			})
 		}
 
-		CastingEnvironment.addCreateEventListener { env: CastingEnvironment, _: NbtCompound ->
-			env.addExtension(SentinelBedComponent(env))
-			if (env is PlayerBasedCastEnv)
-				env.addExtension(LedgerRecordComponent(env))
-		}
-
-		ServerPlayerEvents.AFTER_RESPAWN.register { oldPlayer, newPlayer, alive ->
-			if (!alive)
-				ShaderRenderer.setEffect(null)
-			(newPlayer as PlayerEntityMinterface).setLesserSentinels((oldPlayer as PlayerEntityMinterface).getLesserSentinels())
-			(newPlayer as PlayerEntityMinterface).setEvocation((oldPlayer as PlayerEntityMinterface).getEvocation())
-			(newPlayer as PlayerEntityMinterface).setWristpocket((oldPlayer as PlayerEntityMinterface).getWristpocket())
-		}
-
-		ServerPlayConnectionEvents.DISCONNECT.register { handler, _ ->
-			val player = handler.player.uuid
-			EvokeState.active[player] = false
-			if (KeybindData.active.containsKey(player)) {
-				for (key in KeybindData.active[player]!!.keys) {
-					KeybindData.active[player]!![key] = false
-					KeybindData.duration[player]!![key] = 0
-				}
-			}
-		}
-
-		ServerTickEvents.END_SERVER_TICK.register {
-			for (player in EvokeState.active.keys)
-				if (EvokeState.active[player]!!)
-					EvokeState.duration[player] = EvokeState.duration[player]!! - 1
-			for (player in KeybindData.duration.keys) {
-				val binds = KeybindData.active[player]!!
-				for (key in binds.keys)
-					if (KeybindData.active[player]!!.getOrDefault(key, false))
-						KeybindData.duration[player]!![key] = KeybindData.duration[player]!![key]!! + 1
-			}
-		}
-	}
-
-	@JvmStatic
-	fun clientInit() {
 		ClientPlayConnectionEvents.DISCONNECT.register { _, _ -> ShaderRenderer.setEffect(null) }
 		ClientTickEvents.END_CLIENT_TICK.register { ClientStorage.ticks += 1 }
 		WorldRenderEvents.LAST.register { ctx ->
