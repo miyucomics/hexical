7541742ada8b7a2e7ee3e912ba771e56d0ed4fa5
miyucomics <nbriannyc@gmail.com>
2025-06-30 08:40:41 -0400

    Cleanup and fixes

--- src/main/java/miyucomics/hexical/registry/HexicalRecipe.kt
+++ src/main/java/miyucomics/hexical/registry/HexicalRecipe.kt
@@ -10,7 +10,6 @@ import net.minecraft.registry.Registry
 object HexicalRecipe {
 	lateinit var TRANSMUTING_RECIPE: RecipeType<TransmutingRecipe>
 
-	@JvmStatic
 	fun init() {
 		Registry.register(Registries.RECIPE_SERIALIZER, HexicalMain.id("transmuting"), TransmutingSerializer.INSTANCE)
 		TRANSMUTING_RECIPE = Registry.register(Registries.RECIPE_TYPE, HexicalMain.id("transmuting"), TransmutingRecipe.Type.INSTANCE)
