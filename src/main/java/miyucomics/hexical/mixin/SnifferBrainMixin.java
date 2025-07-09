3563969d523bb9f1da5afd24e9d5d1b16b25112e
miyucomics <nbriannyc@gmail.com>
2025-07-09 11:07:20 -0400

    Periwinkle rework and more bobbles

---
+++ src/main/java/miyucomics/hexical/mixin/SnifferBrainMixin.java
@@ -0,0 +1,29 @@
+package miyucomics.hexical.mixin;
+
+import miyucomics.hexical.interfaces.SnifferEntityMinterface;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.passive.SnifferEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Unit;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = "net.minecraft.entity.passive.SnifferBrain$DiggingTask")
+public class SnifferBrainMixin {
+	@Inject(method = "shouldKeepRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/SnifferEntity;J)Z", at = @At("HEAD"), cancellable = true)
+	private void allowCustomDigging(ServerWorld world, SnifferEntity sniffer, long l, CallbackInfoReturnable<Boolean> cir) {
+		if (((SnifferEntityMinterface) sniffer).hasCustomItem())
+			cir.setReturnValue(true);
+	}
+
+	@Inject(method = "finishRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/passive/SnifferEntity;J)V", at = @At("HEAD"), cancellable = true)
+	private void forceDiggingSuccess(ServerWorld world, SnifferEntity sniffer, long l, CallbackInfo ci) {
+		if (((SnifferEntityMinterface) sniffer).hasCustomItem()) {
+			sniffer.getBrain().remember(MemoryModuleType.SNIFF_COOLDOWN, Unit.INSTANCE, 9600L);
+			ci.cancel();
+		}
+	}
+}
