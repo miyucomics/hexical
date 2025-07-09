43676916c3702ca6194139a01538c6fa63ff4a66
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-07-09 11:07:20 -0400

    Periwinkle rework and more bobbles

---
+++ src/main/java/miyucomics/hexical/mixin/SnifferEntityMixin.java
@@ -0,0 +1,76 @@
+package miyucomics.hexical.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import miyucomics.hexical.interfaces.SnifferEntityMinterface;
+import net.minecraft.entity.ai.brain.Activity;
+import net.minecraft.entity.ai.brain.Brain;
+import net.minecraft.entity.ai.brain.MemoryModuleType;
+import net.minecraft.entity.passive.SnifferEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.server.world.ServerWorld;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SnifferEntity.class)
+public abstract class SnifferEntityMixin implements SnifferEntityMinterface {
+	@Shadow public abstract SnifferEntity startState(SnifferEntity.State state);
+	@Shadow public abstract Brain<SnifferEntity> getBrain();
+	@Unique private ItemStack customItem = null;
+	@Unique private boolean isForceDigging = false;
+
+	@Override
+	public boolean hasCustomItem() {
+		return customItem != null;
+	}
+
+	@Override
+	public void produceItem(@NotNull ItemStack stack) {
+		this.customItem = stack;
+		this.isForceDigging = true;
+		getBrain().forget(MemoryModuleType.SNIFF_COOLDOWN);
+		getBrain().forget(MemoryModuleType.DIG_COOLDOWN);
+		getBrain().forget(MemoryModuleType.WALK_TARGET);
+		getBrain().forget(MemoryModuleType.SNIFFER_SNIFFING_TARGET);
+		getBrain().forget(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE);
+
+		getBrain().remember(MemoryModuleType.SNIFFER_DIGGING, true);
+
+		getBrain().stopAllTasks((ServerWorld) ((SnifferEntity) (Object) this).getWorld(), ((SnifferEntity) (Object) this));
+		getBrain().resetPossibleActivities();
+		startState(SnifferEntity.State.DIGGING);
+	}
+
+	@WrapMethod(method = "canTryToDig")
+	public boolean youWantToDig(Operation<Boolean> original) {
+		if (isForceDigging)
+			return true;
+		return original.call();
+	}
+
+	@WrapMethod(method = "canDig")
+	public boolean youCanDig(Operation<Boolean> original) {
+		if (isForceDigging)
+			return true;
+		return original.call();
+	}
+
+	@WrapOperation(method = "dropSeeds", at = @At(value = "INVOKE", target = "Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContextParameterSet;)Lit/unimi/dsi/fastutil/objects/ObjectArrayList;"))
+	public ObjectArrayList<ItemStack> alterDrops(LootTable instance, LootContextParameterSet lootContextParameterSet, Operation<ObjectArrayList<ItemStack>> original) {
+		if (customItem != null) {
+			ObjectArrayList<ItemStack> newDrops = new ObjectArrayList<>();
+			newDrops.add(customItem);
+			customItem = null;
+			isForceDigging = false;
+			return newDrops;
+		}
+		return original.call(instance, lootContextParameterSet);
+	}
+}
