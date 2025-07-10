40355a667d31e914eb25130d8a3e311d1491117b
miyucomics <nbriannyc@gmail.com>
2025-07-09 21:10:38 -0400

    Renamed to curio, datagenned stonecutting recipes

--- src/main/java/miyucomics/hexical/HexicalDatagen.kt
+++ src/main/java/miyucomics/hexical/HexicalDatagen.kt
@@ -1,19 +1,31 @@
 package miyucomics.hexical
 
+import at.petrak.hexcasting.common.lib.HexItems
+import com.ibm.icu.text.PluralRules
 import miyucomics.hexical.registry.HexicalBlocks
 import miyucomics.hexical.registry.HexicalItems
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
 import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.client.ItemModelGenerator
 import net.minecraft.data.client.Models
+import net.minecraft.data.server.recipe.RecipeJsonProvider
+import net.minecraft.data.server.recipe.SingleItemRecipeJsonBuilder
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.book.RecipeCategory
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import java.util.function.Consumer
 
 class HexicalDatagen : DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(generator: FabricDataGenerator) {
 		val pack = generator.createPack()
 		pack.addProvider(::HexicalModelGenerator)
+		pack.addProvider(::HexicalRecipeGenerator)
 	}
 }
 
@@ -24,9 +36,19 @@ private class HexicalModelGenerator(generator: FabricDataOutput) : FabricModelPr
 
 	override fun generateItemModels(generator: ItemModelGenerator) {
 		generator.registerCompass(HexicalItems.CONJURED_COMPASS_ITEM)
-		for (bobble in HexicalItems.BOBBLES)
-			generator.register(bobble, Models.GENERATED)
+		for (curio in HexicalItems.CURIOS)
+			generator.register(curio, Models.GENERATED)
 		for (plushie in HexicalItems.PLUSHIES)
 			generator.register(plushie, Models.GENERATED)
+	}
+}
+
+class HexicalRecipeGenerator(generator: FabricDataOutput) : FabricRecipeProvider(generator) {
+	override fun generate(exporter: Consumer<RecipeJsonProvider>) {
+		HexicalItems.CURIOS.forEach { curio ->
+			SingleItemRecipeJsonBuilder.createStonecutting(Ingredient.ofItems(HexItems.CHARGED_AMETHYST), RecipeCategory.MISC, curio, 1)
+				.criterion(hasItem(HexItems.CHARGED_AMETHYST), conditionsFromItem(HexItems.CHARGED_AMETHYST))
+				.offerTo(exporter, Identifier("items/" + Registries.ITEM.getId(curio).path + "_from_stonecutting"))
+		}
 	}
 }
