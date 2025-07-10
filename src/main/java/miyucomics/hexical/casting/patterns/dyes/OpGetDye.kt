8c920f861baff2a9ffb6c2953dad72ca9cb8bd40
miyucomics <nbriannyc@gmail.com>
2025-07-10 17:20:56 -0400

    Datagen media jar page

---
+++ src/main/java/miyucomics/hexical/casting/patterns/dyes/OpGetDye.kt
@@ -0,0 +1,94 @@
+package miyucomics.hexical.casting.patterns.dyes
+
+import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.getBlockPos
+import at.petrak.hexcasting.api.casting.getEntity
+import at.petrak.hexcasting.api.casting.iota.*
+import miyucomics.hexical.casting.iotas.DyeIota
+import miyucomics.hexical.data.DyeData
+import miyucomics.hexpose.iotas.IdentifierIota
+import miyucomics.hexpose.iotas.getIdentifier
+import net.minecraft.block.Block
+import net.minecraft.block.SignBlock
+import net.minecraft.block.entity.SignBlockEntity
+import net.minecraft.entity.Entity
+import net.minecraft.entity.ItemEntity
+import net.minecraft.entity.mob.ShulkerEntity
+import net.minecraft.entity.passive.CatEntity
+import net.minecraft.entity.passive.SheepEntity
+import net.minecraft.entity.passive.WolfEntity
+import net.minecraft.item.BlockItem
+import net.minecraft.registry.Registries
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
+
+class OpGetDye : ConstMediaAction {
+	override val argc = 1
+	override fun execute(args: List<Iota>, env: CastingEnvironment) = listOf(
+		when (args[0]) {
+			is EntityIota -> {
+				val entity = args.getEntity(0, argc)
+				env.assertEntityInRange(entity)
+				processEntity(entity)
+			}
+			is IdentifierIota -> {
+				when (val item = Registries.ITEM.get(args.getIdentifier(0, argc))) {
+					is BlockItem -> getDyeFromBlock(item.block)
+					else -> {
+						if (DyeData.getDye(item) != null)
+							DyeIota(DyeData.getDye(item)!!)
+						else
+							NullIota()
+					}
+				}
+			}
+			is Vec3Iota -> {
+				val position = args.getBlockPos(0, argc)
+				env.assertPosInRange(position)
+				processVec3d(position, env.world)
+			}
+			else -> NullIota()
+		}
+	)
+
+	private fun processEntity(entity: Entity): Iota {
+		return when (entity) {
+			is CatEntity -> DyeIota(entity.collarColor.name)
+			is ItemEntity -> {
+				when (val item = entity.stack.item) {
+					is BlockItem -> getDyeFromBlock(item.block)
+					else -> {
+						if (DyeData.getDye(item) != null)
+							DyeIota(DyeData.getDye(item)!!)
+						else
+							NullIota()
+					}
+				}
+			}
+			is SheepEntity -> DyeIota(entity.color.name)
+			is ShulkerEntity -> {
+				if (entity.color == null)
+					NullIota()
+				else
+					DyeIota(entity.color!!.name)
+			}
+			is WolfEntity -> DyeIota(entity.collarColor.name)
+			else -> NullIota()
+		}
+	}
+
+	private fun processVec3d(position: BlockPos, world: ServerWorld): Iota {
+		val state = world.getBlockState(position)
+		if (state.block is SignBlock) {
+			val sign = world.getBlockEntity(position) as SignBlockEntity
+			return ListIota(listOf(DyeIota(sign.frontText.color.name), DyeIota(sign.backText.color.name)))
+		}
+		return getDyeFromBlock(world.getBlockState(position).block)
+	}
+
+	private fun getDyeFromBlock(block: Block): Iota {
+		val dye = DyeData.getDye(block) ?: return NullIota()
+		return DyeIota(dye)
+	}
+}
---
+++ src/main/java/miyucomics/hexical/recipe/TransmutingJsonProvider.kt
@@ -0,0 +1,41 @@
+package miyucomics.hexical.recipe
+
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.mojang.serialization.JsonOps
+import miyucomics.hexical.HexicalMain
+import net.minecraft.data.server.recipe.RecipeJsonProvider
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtOps
+import net.minecraft.recipe.Ingredient
+import net.minecraft.recipe.RecipeSerializer
+import net.minecraft.registry.Registries
+import net.minecraft.util.Identifier
+
+class TransmutingJsonProvider(private val id: Identifier, private val input: Ingredient, private val output: List<ItemStack>, private val cost: Long) : RecipeJsonProvider {
+	override fun serialize(json: JsonObject) {
+		json.add("input", input.toJson())
+		json.addProperty("cost", cost)
+
+		if (output.size == 1) {
+			json.add("output", serializeItemStack(output[0]))
+		} else {
+			val array = JsonArray()
+			output.forEach { array.add(serializeItemStack(it)) }
+			json.add("output", array)
+		}
+	}
+
+	override fun getRecipeId(): Identifier = id
+	override fun getSerializer(): RecipeSerializer<*> = TransmutingSerializer.INSTANCE
+	override fun toAdvancementJson(): JsonObject? = null
+	override fun getAdvancementId(): Identifier? = null
+
+	private fun serializeItemStack(stack: ItemStack): JsonObject {
+		val obj = JsonObject()
+		obj.addProperty("item", Registries.ITEM.getId(stack.item).toString())
+		if (stack.count > 1) obj.addProperty("count", stack.count)
+		if (stack.hasNbt()) obj.add("nbt", NbtOps.INSTANCE.convertTo(JsonOps.INSTANCE, stack.nbt))
+		return obj
+	}
+}
