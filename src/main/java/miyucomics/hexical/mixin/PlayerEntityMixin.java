880a82e03ca1a0df43d213f2fbf59b30641b5887
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-07-13 08:29:44 -0400

    Reverted to bad evocation for now

--- src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
+++ src/main/java/miyucomics/hexical/mixin/PlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package miyucomics.hexical.mixin;
 
+import at.petrak.hexcasting.xplat.IXplatAbstractions;
 import miyucomics.hexical.casting.patterns.evocation.OpSetEvocation;
 import miyucomics.hexical.data.ArchLampState;
 import miyucomics.hexical.data.EvokeState;
@@ -10,7 +11,10 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -35,6 +39,18 @@ public class PlayerEntityMixin implements PlayerEntityMinterface {
 	@Inject(method = "tick", at = @At("TAIL"))
 	void tick(CallbackInfo ci) {
 		PlayerEntity player = ((PlayerEntity) (Object) this);
+
+		if (player.getWorld().isClient && EvokeState.isEvoking(player.getUuid())) {
+			float rot = player.bodyYaw * ((float) Math.PI / 180) + MathHelper.cos((float) player.age * 0.6662f) * 0.25f;
+			float cos = MathHelper.cos(rot);
+			float sin = MathHelper.sin(rot);
+			int color = IXplatAbstractions.INSTANCE.getPigment(player).getColorProvider().getColor(player.getWorld().getTime() * 10, player.getPos());
+			float r = ColorHelper.Argb.getRed(color) / 255f;
+			float g = ColorHelper.Argb.getGreen(color) / 255f;
+			float b = ColorHelper.Argb.getBlue(color) / 255f;
+			player.getWorld().addParticle(ParticleTypes.ENTITY_EFFECT, player.getX() + (double) cos * 0.6, player.getY() + 1.8, player.getZ() + (double) sin * 0.6, r, g, b);
+			player.getWorld().addParticle(ParticleTypes.ENTITY_EFFECT, player.getX() - (double) cos * 0.6, player.getY() + 1.8, player.getZ() - (double) sin * 0.6, r, g, b);
+		}
 
 		if (player.getWorld().isClient)
 			return;
