85f956e23141768948ea59790c50bdeec183349d
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-07-18 11:59:09 -0400

    Curios can now be cracked

--- src/main/java/miyucomics/hexical/features/cracked_items/OpCrackDevice.kt
+++ src/main/java/miyucomics/hexical/features/cracked_items/OpCrackDevice.kt
@@ -7,18 +7,22 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadOffhandItem
 import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.common.items.magic.ItemPackagedHex
+import miyucomics.hexical.features.charms.CharmUtilities
+import miyucomics.hexical.features.curios.CurioItem
 import net.minecraft.item.ItemStack
 
 class OpCrackDevice : SpellAction {
 	override val argc = 0
 	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
-		val heldData = env.getHeldItemToOperateOn { it.item is ItemPackagedHex }
+		val heldData = env.getHeldItemToOperateOn { it.item is ItemPackagedHex || it.item is CurioItem }
 		if (heldData == null)
-			throw MishapBadOffhandItem.Companion.of(null, "casting_device")
+			throw MishapBadOffhandItem.Companion.of(null, "crackable_item")
 		val stack = heldData.stack
-		if ((heldData.stack.item as ItemPackagedHex).hasHex(stack))
+		if (stack.item is ItemPackagedHex && (stack.item as ItemPackagedHex).hasHex(stack))
 			throw IllegalJailbreakMishap()
-		return SpellAction.Result(Spell(heldData.stack), MediaConstants.CRYSTAL_UNIT, listOf())
+		if (stack.item is CurioItem && CharmUtilities.isStackCharmed(stack))
+			throw IllegalJailbreakMishap()
+		return SpellAction.Result(Spell(stack), MediaConstants.CRYSTAL_UNIT, listOf())
 	}
 
 	private data class Spell(val stack: ItemStack) : RenderedSpell {
