74756348b6580f7f87328fcd817389313157ffc2
miyucomics <nbriannyc@gmail.com>
2025-07-18 11:59:09 -0400

    Curios can now be cracked

--- src/main/java/miyucomics/hexical/features/cracked_items/CrackedItemTooltip.kt
+++ src/main/java/miyucomics/hexical/features/cracked_items/CrackedItemTooltip.kt
@@ -2,6 +2,9 @@ package miyucomics.hexical.features.cracked_items
 
 import at.petrak.hexcasting.api.casting.iota.IotaType
 import at.petrak.hexcasting.common.items.magic.ItemPackagedHex
+import miyucomics.hexical.features.charms.CharmUtilities
+import miyucomics.hexical.features.charms.CharmUtilities.getCompound
+import miyucomics.hexical.features.curios.CurioItem
 import miyucomics.hexical.inits.InitHook
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback
 import net.minecraft.nbt.NbtCompound
@@ -23,6 +26,16 @@ object CrackedItemTooltip : InitHook() {
 				entries.forEach { text.append(IotaType.getDisplay(it as NbtCompound)) }
 				lines.add(Text.translatable("hexical.cracked.program").append(text))
 			}
+		}
+
+		ItemTooltipCallback.EVENT.register { stack, _, lines ->
+			val nbt = stack.nbt ?: return@register
+			if (stack.item !is CurioItem || !nbt.getBoolean("cracked"))
+				return@register
+
+			lines.add(Text.translatable("hexical.cracked.cracked").formatted(Formatting.GOLD))
+			if (CharmUtilities.isStackCharmed(stack))
+				lines.add(Text.translatable("hexical.cracked.program").append(IotaType.getDisplay(getCompound(stack).getCompound("instructions"))))
 		}
 	}
 }
