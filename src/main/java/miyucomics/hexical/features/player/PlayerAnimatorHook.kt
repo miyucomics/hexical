e5adae85bc092be5a2c37d0fceef9e88873f6e0f
miyucomics <nbriannyc@gmail.com>
2025-07-20 17:40:17 -0400

    Bell animation

--- src/main/java/miyucomics/hexical/features/player/PlayerAnimatorHook.kt
+++ src/main/java/miyucomics/hexical/features/player/PlayerAnimatorHook.kt
@@ -3,7 +3,9 @@ package miyucomics.hexical.features.player
 import dev.kosmx.playerAnim.api.layered.ModifierLayer
 import dev.kosmx.playerAnim.api.layered.modifier.MirrorModifier
 import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess
+import miyucomics.hexical.HexicalMain
 import miyucomics.hexical.features.curios.curios.FluteCurioPlayerModel
+import miyucomics.hexical.features.curios.curios.HandbellCurioItemModel
 import miyucomics.hexical.features.curios.curios.HandbellCurioPlayerModel
 import miyucomics.hexical.features.evocation.EvocationAnimation
 import miyucomics.hexical.inits.HexicalItems
@@ -16,17 +18,19 @@ object PlayerAnimatorHook : InitHook() {
 		PlayerAnimationAccess.REGISTER_ANIMATION_EVENT.register { player, stack ->
 			stack.addAnimLayer(1000, EvocationAnimation(player))
 
-			stack.addAnimLayer(100, ModifierLayer(HandbellCurioPlayerModel(player)).also {
-				it.addModifierBefore(object : MirrorModifier() {
-					override fun isEnabled() = (player.mainArm == Arm.LEFT) xor (player.getStackInHand(Hand.OFF_HAND).isOf(HexicalItems.CURIO_HANDBELL) && !player.getStackInHand(Hand.MAIN_HAND).isOf(HexicalItems.CURIO_HANDBELL))
-				})
-			})
-
 			stack.addAnimLayer(100, ModifierLayer(FluteCurioPlayerModel(player)).also {
 				it.addModifierBefore(object : MirrorModifier() {
 					override fun isEnabled() = (player.mainArm == Arm.LEFT) xor (player.getStackInHand(Hand.OFF_HAND).isOf(HexicalItems.CURIO_FLUTE) && !player.getStackInHand(Hand.MAIN_HAND).isOf(HexicalItems.CURIO_FLUTE))
 				})
 			})
+
+			val handbellAnimation = ModifierLayer(HandbellCurioPlayerModel(player)).also {
+				it.addModifierBefore(object : MirrorModifier() {
+					override fun isEnabled() = (player.mainArm == Arm.LEFT) xor (player.getStackInHand(Hand.OFF_HAND).isOf(HexicalItems.CURIO_HANDBELL) && !player.getStackInHand(Hand.MAIN_HAND).isOf(HexicalItems.CURIO_HANDBELL))
+				})
+			}
+			PlayerAnimationAccess.getPlayerAssociatedData(player).set(HandbellCurioItemModel.clientReceiver, handbellAnimation)
+			stack.addAnimLayer(100, handbellAnimation)
 		}
 	}
 }
