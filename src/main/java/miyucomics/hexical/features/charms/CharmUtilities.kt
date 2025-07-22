9396194e3fcfbed286390c491c71dcdf96924c1d
miyucomics <nbriannyc@gmail.com>
2025-07-22 10:06:08 -0400

    Reworked charms again

--- src/main/java/miyucomics/hexical/features/charms/CharmUtilities.kt
+++ src/main/java/miyucomics/hexical/features/charms/CharmUtilities.kt
@@ -30,13 +30,9 @@ object CharmUtilities {
 
 	@JvmStatic
 	fun shouldIntercept(stack: ItemStack, button: Int, sneaking: Boolean): Boolean {
-		val charmed = stack.nbt!!.getCompound("charmed")
-		val key = when (button) {
-			0 -> if (sneaking) "left_sneak" else "left"
-			1 -> if (sneaking) "right_sneak" else "right"
-			else -> return true
-		}
-		return charmed.getBoolean(key)
+		val charmed = getCompound(stack)
+		val inputs = if (sneaking) charmed.getIntArray("sneak_inputs") else charmed.getIntArray("normal_inputs")
+		return inputs.contains(button)
 	}
 
 	fun removeCharm(stack: ItemStack) {
