1e5bd7d263d1039f974855f44718b25d7deafbb1
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-07-22 09:36:16 -0400

    Random flute bobbing

--- src/main/java/miyucomics/hexical/features/curios/curios/HandbellCurioPlayerModel.kt
+++ src/main/java/miyucomics/hexical/features/curios/curios/HandbellCurioPlayerModel.kt
@@ -1,8 +1,6 @@
 package miyucomics.hexical.features.curios.curios
 
 import dev.kosmx.playerAnim.api.TransformType
-import dev.kosmx.playerAnim.api.firstPerson.FirstPersonConfiguration
-import dev.kosmx.playerAnim.api.firstPerson.FirstPersonMode
 import dev.kosmx.playerAnim.api.layered.IAnimation
 import dev.kosmx.playerAnim.core.util.Vec3f
 import miyucomics.hexical.inits.HexicalItems
@@ -25,13 +23,16 @@ class HandbellCurioPlayerModel(val player: PlayerEntity) : IAnimation {
 	override fun get3DTransform(modelName: String, type: TransformType, tickDelta: Float, original: Vec3f): Vec3f {
 		if (modelName == "rightArm" && type == TransformType.ROTATION) {
 			val time = ClientStorage.ticks + tickDelta
-			var rock = MathHelper.sin(time * 0.07f) * 2f
+			val pitch = MathHelper.sin(time * 0.08f + 0.8f) * 2f
+			val yaw = MathHelper.sin(time * 0.06f + 0.2f) * 2f
+			var roll = MathHelper.sin(time * 0.07f) * 2f
+
 			if (shakingBellTimer > 0) {
 				val progress = ((1f - shakingBellTimer + tickDelta) / 10f)
 				val eased = (1f - MathHelper.cos(progress * MathHelper.PI)) / 2f
-				rock += MathHelper.sin(eased * MathHelper.PI * 2) * 20f
+				roll += MathHelper.sin(eased * MathHelper.PI * 2) * 20f
 			}
-			return Vec3f(-90f + MathHelper.sin(time * 0.08f + 0.8f) * 2f, MathHelper.sin(time * 0.06f + 0.2f) * 2f, rock).scale(MathHelper.RADIANS_PER_DEGREE)
+			return Vec3f(-90f + pitch, yaw, roll).scale(MathHelper.RADIANS_PER_DEGREE)
 		}
 		return original
 	}
