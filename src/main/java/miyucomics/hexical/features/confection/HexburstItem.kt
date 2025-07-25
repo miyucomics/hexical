c20e3c1c35e9466077ba00df6d5d482ef5a93f88
miyucomics <nbriannyc@gmail.com>
2025-07-25 11:03:35 -0400

    Tooltips

--- src/main/java/miyucomics/hexical/features/confection/HexburstItem.kt
+++ src/main/java/miyucomics/hexical/features/confection/HexburstItem.kt
@@ -2,13 +2,17 @@ package miyucomics.hexical.features.confection
 
 import at.petrak.hexcasting.api.casting.iota.GarbageIota
 import at.petrak.hexcasting.api.casting.iota.IotaType
+import at.petrak.hexcasting.api.utils.asTranslatedComponent
 import miyucomics.hexical.misc.CastingUtils
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.LivingEntity
 import net.minecraft.item.FoodComponent
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.world.World
 
 class HexburstItem : Item(Settings().maxCount(16).food(FoodComponent.Builder().alwaysEdible().snack().build())) {
@@ -23,5 +27,9 @@ class HexburstItem : Item(Settings().maxCount(16).food(FoodComponent.Builder().a
 		else
 			GarbageIota())
 		return super.finishUsing(stack, world, user)
+	}
+
+	override fun appendTooltip(stack: ItemStack, world: World?, list: MutableList<Text>, context: TooltipContext) {
+		list.add("hexical.hexburst.iota".asTranslatedComponent(IotaType.getDisplay(stack.orCreateNbt.getCompound("iota"))).formatted(Formatting.GRAY))
 	}
 }
