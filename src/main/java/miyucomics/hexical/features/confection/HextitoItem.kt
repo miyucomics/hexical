69f2abec57b2f70ce433dd2682ce7b12ccac928b
miyucomics <nbriannyc@gmail.com>
2025-07-25 11:03:35 -0400

    Tooltips

--- src/main/java/miyucomics/hexical/features/confection/HextitoItem.kt
+++ src/main/java/miyucomics/hexical/features/confection/HextitoItem.kt
@@ -1,14 +1,23 @@
 package miyucomics.hexical.features.confection
 
 import at.petrak.hexcasting.api.casting.eval.vm.CastingVM
+import at.petrak.hexcasting.api.casting.iota.IotaType
+import at.petrak.hexcasting.api.utils.asCompound
+import at.petrak.hexcasting.api.utils.asTranslatedComponent
+import at.petrak.hexcasting.api.utils.getList
+import at.petrak.hexcasting.api.utils.styledWith
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexical.misc.HexSerialization
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.LivingEntity
 import net.minecraft.item.FoodComponent
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtElement
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.util.Hand
 import net.minecraft.world.World
 
@@ -25,5 +34,9 @@ class HextitoItem : Item(Settings().maxCount(16).food(FoodComponent.Builder().al
 			IXplatAbstractions.INSTANCE.setStaffcastImage(user, vm.image)
 		}
 		return super.finishUsing(stack, world, user)
+	}
+
+	override fun appendTooltip(stack: ItemStack, world: World?, list: MutableList<Text>, context: TooltipContext) {
+		list.add("hexical.hextito.hex".asTranslatedComponent(stack.getList("hex", NbtElement.COMPOUND_TYPE.toInt())!!.fold(Text.empty()) { acc, curr -> acc.append(IotaType.getDisplay(curr.asCompound)) }).styledWith(Formatting.GRAY))
 	}
 }
