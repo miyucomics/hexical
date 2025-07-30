3bdb0d7381f706d810c54305438530d54a784d34
miyucomics <nbriannyc@gmail.com>
2025-07-30 11:39:51 -0400

    Reduced dependency on mixins

--- src/main/java/miyucomics/hexical/mixin/CastingEnvironmentMixin.java
+++ src/main/java/miyucomics/hexical/mixin/CastingEnvironmentMixin.java
@@ -1,8 +1,6 @@
 package miyucomics.hexical.mixin;
 
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import miyucomics.hexical.features.periwinkle.WooleyedEffect;
 import miyucomics.hexical.inits.HexicalItems;
 import net.minecraft.entity.LivingEntity;
@@ -11,20 +9,22 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = CastingEnvironment.class, remap = false)
 public abstract class CastingEnvironmentMixin {
 	@Shadow public abstract @Nullable LivingEntity getCastingEntity();
 
-	@WrapMethod(method = "isEnlightened")
-	private boolean canOvercast(Operation<Boolean> original) {
+	@Inject(method = "isEnlightened", at = @At("HEAD"), cancellable = true)
+	private void canDoGreatSpells(CallbackInfoReturnable<Boolean> cir) {
 		if (this.getCastingEntity() == null)
-			return original.call();
+			return;
 		if (this.getCastingEntity() instanceof PlayerEntity player && player.getInventory().armor.get(3).isOf(HexicalItems.LEI))
-			return true;
+			cir.setReturnValue(true);
 		StatusEffectInstance wooleye = this.getCastingEntity().getStatusEffect(WooleyedEffect.INSTANCE);
-		if (wooleye == null)
-			return original.call();
-		return wooleye.getAmplifier() < 1;
+		if (wooleye != null)
+			cir.setReturnValue(wooleye.getAmplifier() < 1);
 	}
 }
