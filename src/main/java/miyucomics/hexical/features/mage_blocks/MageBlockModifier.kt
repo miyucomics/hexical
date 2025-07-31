7456964bfd1778479f61ee7aa333b6bfed2a38bd
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-07-31 17:49:37 -0400

    Scrying modifiers

--- src/main/java/miyucomics/hexical/features/mage_blocks/MageBlockModifier.kt
+++ src/main/java/miyucomics/hexical/features/mage_blocks/MageBlockModifier.kt
@@ -1,12 +1,16 @@
 package miyucomics.hexical.features.mage_blocks
 
+import com.mojang.datafixers.util.Pair
 import net.minecraft.block.BlockState
+import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtElement
+import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 interface MageBlockModifier {
 	val type: MageBlockModifierType<*>
 	fun serialize(): NbtElement
+	fun getScryingLens(): Pair<ItemStack, Text>? = null
 	fun tick(world: World, pos: BlockPos, state: BlockState) {}
 }
--- src/main/java/miyucomics/hexical/features/mage_blocks/modifiers/LifespanModifier.kt
+++ src/main/java/miyucomics/hexical/features/mage_blocks/modifiers/LifespanModifier.kt
@@ -3,13 +3,18 @@ package miyucomics.hexical.features.mage_blocks.modifiers
 import at.petrak.hexcasting.api.casting.getPositiveInt
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.utils.asInt
+import at.petrak.hexcasting.api.utils.asTranslatedComponent
+import com.mojang.datafixers.util.Pair
 import miyucomics.hexical.HexicalMain
 import miyucomics.hexical.features.mage_blocks.MageBlockModifier
 import miyucomics.hexical.features.mage_blocks.MageBlockModifierType
 import miyucomics.hexical.inits.HexicalBlocks
 import net.minecraft.block.BlockState
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.nbt.NbtElement
 import net.minecraft.nbt.NbtInt
+import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
@@ -17,6 +22,7 @@ class LifespanModifier : MageBlockModifier {
 	override val type: MageBlockModifierType<*> = TYPE
 	var lifespan = 0
 
+	override fun getScryingLens(): Pair<ItemStack, Text> = Pair(ItemStack(Items.CLOCK), "hexical.mage_block.lifespan".asTranslatedComponent(lifespan / 20f))
 	override fun serialize(): NbtElement = NbtInt.of(lifespan)
 	override fun tick(world: World, pos: BlockPos, state: BlockState) {
 		lifespan--
