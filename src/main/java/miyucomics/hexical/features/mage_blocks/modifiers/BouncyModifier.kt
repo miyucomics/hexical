67e8d9ec841d402fcd613840ab8f239a3d3c27d8
miyucomics <nbriannyc@gmail.com>
2025-07-31 16:53:57 -0400

    Extensible modifiers complete

---
+++ src/main/java/miyucomics/hexical/features/mage_blocks/modifiers/BouncyModifier.kt
@@ -0,0 +1,23 @@
+package miyucomics.hexical.features.mage_blocks.modifiers
+
+import at.petrak.hexcasting.api.casting.iota.Iota
+import miyucomics.hexical.HexicalMain
+import miyucomics.hexical.features.mage_blocks.MageBlockModifier
+import miyucomics.hexical.features.mage_blocks.MageBlockModifierType
+import net.minecraft.nbt.NbtByte
+import net.minecraft.nbt.NbtElement
+
+class BouncyModifier : MageBlockModifier {
+	override val type: MageBlockModifierType<*> = TYPE
+
+	override fun serialize(): NbtElement = NbtByte.of(false)
+
+	companion object {
+		var TYPE: MageBlockModifierType<BouncyModifier> = object : MageBlockModifierType<BouncyModifier>() {
+			override val argc: Int = 0
+			override val id = HexicalMain.id("bouncy")
+			override fun construct(args: List<Iota>) = BouncyModifier()
+			override fun deserialize(element: NbtElement) = BouncyModifier()
+		}
+	}
+}
