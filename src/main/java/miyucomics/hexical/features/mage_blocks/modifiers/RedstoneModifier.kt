31eb6391c59454c07dbf2bd5805c139c2a2fa360
miyucomics <nbriannyc@gmail.com>
2025-07-31 17:49:37 -0400

    Scrying modifiers

--- src/main/java/miyucomics/hexical/features/mage_blocks/modifiers/RedstoneModifier.kt
+++ src/main/java/miyucomics/hexical/features/mage_blocks/modifiers/RedstoneModifier.kt
@@ -3,16 +3,22 @@ package miyucomics.hexical.features.mage_blocks.modifiers
 import at.petrak.hexcasting.api.casting.getPositiveIntUnderInclusive
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.utils.asInt
+import at.petrak.hexcasting.api.utils.asTranslatedComponent
+import com.mojang.datafixers.util.Pair
 import miyucomics.hexical.HexicalMain
 import miyucomics.hexical.features.mage_blocks.MageBlockModifier
 import miyucomics.hexical.features.mage_blocks.MageBlockModifierType
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.nbt.NbtElement
 import net.minecraft.nbt.NbtInt
+import net.minecraft.text.Text
 
 class RedstoneModifier : MageBlockModifier {
 	override val type: MageBlockModifierType<*> = TYPE
 	var power = 0
 
+	override fun getScryingLens(): Pair<ItemStack, Text> = Pair(ItemStack(Items.REDSTONE), "hexical.mage_block.redstone".asTranslatedComponent(power))
 	override fun serialize(): NbtElement = NbtInt.of(power)
 
 	companion object {
