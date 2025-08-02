3688eca6b9ada9958c9784b17ecc56179a2212ec
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-08-01 22:20:14 -0400

    Seized item rendering

--- src/main/java/miyucomics/hexical/features/pedestal/PedestalBlockEntityRenderer.kt
+++ src/main/java/miyucomics/hexical/features/pedestal/PedestalBlockEntityRenderer.kt
@@ -1,6 +1,5 @@
 package miyucomics.hexical.features.pedestal
 
-import miyucomics.hexical.features.pedestal.PedestalBlockEntity.Companion.HEIGHT
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.OverlayTexture
 import net.minecraft.client.render.VertexConsumerProvider
@@ -8,16 +7,13 @@ import net.minecraft.client.render.WorldRenderer
 import net.minecraft.client.render.block.entity.BlockEntityRenderer
 import net.minecraft.client.render.model.json.ModelTransformationMode
 import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.util.math.RotationAxis
 import net.minecraft.util.math.Vec3d
-import kotlin.math.sin
 
 class PedestalBlockEntityRenderer : BlockEntityRenderer<PedestalBlockEntity> {
 	override fun render(pedestal: PedestalBlockEntity, tickDelta: Float, matrices: MatrixStack, vertices: VertexConsumerProvider, light: Int, overlay: Int) {
-//		if (pedestal.heldStack.isEmpty)
-//			return
+		if (pedestal.heldStack.isEmpty)
+			return
 
 		val time = pedestal.world!!.time + tickDelta
 		val offset = Vec3d.of(pedestal.normalVector).multiply(0.75)
@@ -25,6 +21,6 @@ class PedestalBlockEntityRenderer : BlockEntityRenderer<PedestalBlockEntity> {
 		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(time * 4))
 
 		val lightAbove = WorldRenderer.getLightmapCoordinates(pedestal.world, pedestal.pos.up())
-		MinecraftClient.getInstance().itemRenderer.renderItem(ItemStack(Items.APPLE), ModelTransformationMode.GROUND, lightAbove, OverlayTexture.DEFAULT_UV, matrices, vertices, pedestal.world, 0)
+		MinecraftClient.getInstance().itemRenderer.renderItem(pedestal.heldStack, ModelTransformationMode.GROUND, lightAbove, OverlayTexture.DEFAULT_UV, matrices, vertices, pedestal.world, 0)
 	}
 }
