6025f413294b57119db7fdb8033e6b86b4c07e78
miyucomics <nbriannyc@gmail.com>
2025-08-20 13:02:07 -0400

    Repositioned item, microoptimization

--- src/client/java/miyucomics/hexical/features/pedestal/PedestalBlockEntityRenderer.kt
+++ src/client/java/miyucomics/hexical/features/pedestal/PedestalBlockEntityRenderer.kt
@@ -9,7 +9,6 @@ import net.minecraft.client.render.model.json.ModelTransformationMode
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.math.RotationAxis
 import net.minecraft.util.math.Vec3d
-import kotlin.math.sin
 
 class PedestalBlockEntityRenderer : BlockEntityRenderer<PedestalBlockEntity> {
 	override fun render(pedestal: PedestalBlockEntity, tickDelta: Float, matrices: MatrixStack, vertices: VertexConsumerProvider, light: Int, overlay: Int) {
@@ -17,12 +16,8 @@ class PedestalBlockEntityRenderer : BlockEntityRenderer<PedestalBlockEntity> {
 			return
 
 		val time = pedestal.world!!.time + tickDelta
-		val offset = Vec3d.of(pedestal.normalVector).multiply(0.75)
+		val offset = Vec3d.of(pedestal.normalVector).multiply(PedestalBlockEntity.HEIGHT)
 		matrices.translate(offset.x + 0.5, offset.y + 0.35, offset.z + 0.5)
-
-		val bobbing = Vec3d.of(pedestal.normalVector).multiply(sin(time / 10).toDouble() / 10)
-		matrices.translate(bobbing.x, bobbing.y, bobbing.z)
-
 		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(time * 4))
 
 		val lightAbove = WorldRenderer.getLightmapCoordinates(pedestal.world, pedestal.pos.up())
