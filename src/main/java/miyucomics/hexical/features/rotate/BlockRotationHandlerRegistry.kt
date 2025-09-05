5bf463563a9a5e3e7a936c529a1e824ab4315c6d
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-09-04 20:18:19 -0400

    Finished improved block rotations

--- src/main/java/miyucomics/hexical/features/rotate/BlockRotationHandlerRegistry.kt
+++ src/main/java/miyucomics/hexical/features/rotate/BlockRotationHandlerRegistry.kt
@@ -1,13 +1,21 @@
 package miyucomics.hexical.features.rotate
 
+import miyucomics.hexical.misc.InitHook
 import net.minecraft.block.BlockState
 import net.minecraft.state.property.Properties
 import net.minecraft.util.math.Direction
 
-object BlockRotationHandlerRegistry {
+object BlockRotationHandlerRegistry : InitHook() {
     private val handlers = mutableListOf<BlockRotationHandler>()
 
-    fun init() {
+    fun modify(state: BlockState, direction: Direction) = handlers.firstOrNull { it.canHandle(state, direction) }?.handle(state, direction)
+
+    override fun init() {
+        register(object : BlockRotationHandler {
+            override fun canHandle(state: BlockState, direction: Direction) = state.properties.contains(Properties.AXIS)
+            override fun handle(state: BlockState, direction: Direction) = state.with(Properties.AXIS, direction.axis)
+        })
+
         register(object : BlockRotationHandler {
             override fun canHandle(state: BlockState, direction: Direction) = state.properties.contains(Properties.FACING)
             override fun handle(state: BlockState, direction: Direction) = state.with(Properties.FACING, direction)
@@ -19,8 +27,13 @@ object BlockRotationHandlerRegistry {
         })
 
         register(object : BlockRotationHandler {
-            override fun canHandle(state: BlockState, direction: Direction) = state.properties.contains(Properties.HORIZONTAL_FACING) && direction != Direction.UP
+            override fun canHandle(state: BlockState, direction: Direction) = state.properties.contains(Properties.HORIZONTAL_FACING) && direction != Direction.UP  && direction != Direction.DOWN
             override fun handle(state: BlockState, direction: Direction) = state.with(Properties.HORIZONTAL_FACING, direction)
+        })
+
+        register(object : BlockRotationHandler {
+            override fun canHandle(state: BlockState, direction: Direction) = state.properties.contains(Properties.VERTICAL_DIRECTION) && !(direction == Direction.UP || direction == Direction.DOWN)
+            override fun handle(state: BlockState, direction: Direction) = state.with(Properties.VERTICAL_DIRECTION, direction)
         })
     }
 
