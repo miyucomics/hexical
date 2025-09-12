1ebd953cd3250c6edb4b138eb956458279f7a9f6
miyucomics <nbriannyc@gmail.com>
2025-09-12 11:29:45 -0400

    Dye rework continued

--- src/main/java/miyucomics/hexical/features/dyes/OpTranslateDye.kt
+++ src/main/java/miyucomics/hexical/features/dyes/OpTranslateDye.kt
@@ -9,7 +9,7 @@ import net.minecraft.util.math.Vec3d
 object OpTranslateDye : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-		val dye = args.getTrueDye(0, argc).colorComponents
+		val dye = args.getColoredDye(0, argc).colorComponents
 		return Vec3d(dye[0].toDouble(), dye[1].toDouble(), dye[2].toDouble()).asActionResult
 	}
 }
