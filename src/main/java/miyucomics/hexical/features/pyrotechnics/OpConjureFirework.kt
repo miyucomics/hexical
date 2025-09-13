41c11729ec771722e4f4588d58b63bfe6bf76de8
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-09-13 11:27:58 -0400

    Fireworks altered

--- src/main/java/miyucomics/hexical/features/pyrotechnics/OpConjureFirework.kt
+++ src/main/java/miyucomics/hexical/features/pyrotechnics/OpConjureFirework.kt
@@ -4,16 +4,15 @@ import at.petrak.hexcasting.api.casting.*
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.iota.Vec3Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import at.petrak.hexcasting.api.misc.MediaConstants
-import miyucomics.hexical.features.dyes.DyeIota
 import net.minecraft.entity.projectile.FireworkRocketEntity
 import net.minecraft.item.FireworkRocketItem
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtList
-import net.minecraft.util.DyeColor
 import net.minecraft.util.math.Vec3d
 
 object OpConjureFirework : SpellAction {
@@ -24,58 +23,57 @@ object OpConjureFirework : SpellAction {
 
 		val velocity = args.getVec3(1, argc)
 		val duration = args.getIntBetween(2, 1, 3, argc)
-		val type = args.getIntBetween(3, 0, 4, argc)
+		val shape = args.getPositiveIntUnderInclusive(3, 4, argc)
 
 		val colors = args.getList(4, argc)
 		if (!colors.nonEmpty)
 			throw MishapInvalidIota.of(args[3], 4, "nonempty_list")
-		val trueColors = mutableListOf<Int>()
-		for (color in colors) {
-			if (color.type != DyeIota.TYPE)
-				throw MishapInvalidIota.of(args[3], 4, "colored_dye")
-			trueColors.add(DyeColor.byName((color as DyeIota).dye, DyeColor.WHITE)!!.fireworkColor)
+		val trueColors = colors.map {
+			if (it !is Vec3Iota)
+				throw MishapInvalidIota.of(args[3], 4, "vector_list")
+			translateVectorToColor(it.vec3)
 		}
 
-		val fades = args.getList(5, argc)
-		val trueFades = mutableListOf<Int>()
-		for (fade in fades) {
-			if (fade.type != DyeIota.TYPE)
-				throw MishapInvalidIota.of(args[4], 3, "colored_dye")
-			trueFades.add(DyeColor.byName((fade as DyeIota).dye, DyeColor.WHITE)!!.fireworkColor)
+		val fades = args.getList(5, argc).map {
+			if (it !is Vec3Iota)
+				throw MishapInvalidIota.of(args[3], 4, "vector_list")
+			translateVectorToColor(it.vec3)
 		}
 
 		val flicker = args.getBool(6, argc)
 		val trail = args.getBool(7, argc)
 
-		return SpellAction.Result(Spell(position, velocity, duration, type, trueColors, trueFades, flicker, trail), MediaConstants.SHARD_UNIT, listOf(ParticleSpray.burst(position, 1.0)))
+		return SpellAction.Result(Spell(position, velocity, duration, shape, trueColors, fades, flicker, trail), MediaConstants.SHARD_UNIT, listOf(ParticleSpray.burst(position, 1.0)))
 	}
 
-	private data class Spell(val position: Vec3d, val velocity: Vec3d, val duration: Int, val type: Int, val colors: List<Int>, val fades: List<Int>, val flicker: Boolean, val trail: Boolean) :
-		RenderedSpell {
+	private data class Spell(val position: Vec3d, val velocity: Vec3d, val duration: Int, val shape: Int, val colors: List<Int>, val fades: List<Int>, val flicker: Boolean, val trail: Boolean) : RenderedSpell {
 		override fun cast(env: CastingEnvironment) {
-			val star = NbtCompound()
-			star.putInt(FireworkRocketItem.TYPE_KEY, type)
-			if (flicker)
-				star.putByte(FireworkRocketItem.FLICKER_KEY, (1).toByte())
-			if (trail)
-				star.putByte(FireworkRocketItem.TRAIL_KEY, (1).toByte())
-			star.putIntArray(FireworkRocketItem.COLORS_KEY, colors)
-			if (fades.isNotEmpty())
-				star.putIntArray(FireworkRocketItem.FADE_COLORS_KEY, fades)
+			val fireworkNbt = NbtCompound().apply {
+				put(FireworkRocketItem.EXPLOSIONS_KEY, NbtList().apply {
+					add(NbtCompound().apply {
+						putInt(FireworkRocketItem.TYPE_KEY, shape)
+						putIntArray(FireworkRocketItem.COLORS_KEY, colors)
+						if (fades.isNotEmpty()) putIntArray(FireworkRocketItem.FADE_COLORS_KEY, fades)
+						if (flicker) putByte(FireworkRocketItem.FLICKER_KEY, 0b1)
+						if (trail) putByte(FireworkRocketItem.TRAIL_KEY, 0b1)
+					})
+				})
+				putByte(FireworkRocketItem.FLIGHT_KEY, duration.toByte())
+			}
 
-			val explosions = NbtList()
-			explosions.add(star)
+			val fireworkStack = ItemStack(Items.FIREWORK_ROCKET).also { stack ->
+				stack.orCreateNbt.put(FireworkRocketItem.FIREWORKS_KEY, fireworkNbt)
+			}
 
-			val main = NbtCompound()
-			main.put(FireworkRocketItem.EXPLOSIONS_KEY, explosions)
-			main.putByte(FireworkRocketItem.FLIGHT_KEY, duration.toByte())
-
-			val stack = ItemStack(Items.FIREWORK_ROCKET)
-			stack.orCreateNbt.put(FireworkRocketItem.FIREWORKS_KEY, main)
-
-			val firework = FireworkRocketEntity(env.world, stack, position.x, position.y, position.z, true)
-			firework.setVelocity(velocity.x, velocity.y, velocity.z)
-			env.world.spawnEntity(firework)
+			env.world.spawnEntity(FireworkRocketEntity(env.world, fireworkStack, position.x, position.y, position.z, true).apply {
+				setVelocity(velocity.x, velocity.y, velocity.z)
+			})
 		}
+	}
+
+	private fun translateVectorToColor(vector: Vec3d): Int {
+		return (vector.x.coerceIn(0.0, 1.0) * 255).toInt() shl 16 or
+		(vector.y.coerceIn(0.0, 1.0) * 255).toInt() shl 8 or
+		(vector.z.coerceIn(0.0, 1.0) * 255).toInt()
 	}
 }
