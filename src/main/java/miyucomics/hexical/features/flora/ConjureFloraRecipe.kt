ee78d9a9443984829bea57dfec42ab336ce6f8f9
miyucomics <nbriannyc@gmail.com>
2025-10-07 14:36:38 -0400

    Block state info for conjure flora

--- src/main/java/miyucomics/hexical/features/flora/ConjureFloraRecipe.kt
+++ src/main/java/miyucomics/hexical/features/flora/ConjureFloraRecipe.kt
@@ -1,7 +1,7 @@
 package miyucomics.hexical.features.flora
 
 import miyucomics.hexical.features.dyes.block.DyeingBlockSerializer
-import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.recipe.Recipe
@@ -10,7 +10,7 @@ import net.minecraft.registry.DynamicRegistryManager
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
 
-class ConjureFloraRecipe(private val id: Identifier, val block: Block, val cost: Long) : Recipe<Inventory> {
+class ConjureFloraRecipe(private val id: Identifier, val block: BlockState, val cost: Long) : Recipe<Inventory> {
 	override fun getId() = this.id
 	override fun getType() = Type.INSTANCE
 	override fun fits(width: Int, height: Int) = false
