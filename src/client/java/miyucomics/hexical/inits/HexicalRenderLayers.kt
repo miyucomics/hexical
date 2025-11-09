ec6c9a36b6a6f77275eabe5e99fae4b5e9aa18bd
miyucomics <nbriannyc@gmail.com>
2025-11-09 18:32:48 -0500

    Started changing mage block render layer to the media jar briefly

--- src/client/java/miyucomics/hexical/inits/HexicalRenderLayers.kt
+++ src/client/java/miyucomics/hexical/inits/HexicalRenderLayers.kt
@@ -1,7 +1,9 @@
-package miyucomics.hexical.features.media_jar
+package miyucomics.hexical.inits
 
 import miyucomics.hexical.HexicalMain
-import miyucomics.hexical.misc.InitHook
+import miyucomics.hexical.inits.HexicalBlocks.MAGE_BLOCK
+import miyucomics.hexical.inits.HexicalBlocks.PERIWINKLE_FLOWER
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback
 import net.minecraft.client.render.RenderLayer
 import net.minecraft.client.render.RenderPhase
@@ -9,23 +11,18 @@ import net.minecraft.client.render.VertexFormat
 import net.minecraft.client.render.VertexFormats
 import net.minecraft.util.Identifier
 
-object MediaJarShader : InitHook() {
+object HexicalRenderLayers {
 	lateinit var mediaJarRenderLayer: RenderLayer
 	val PERLIN_NOISE: Identifier = HexicalMain.id("textures/misc/perlin.png")
 
-	override fun init() {
+	fun clientInit() {
 		CoreShaderRegistrationCallback.EVENT.register { context ->
 			context.register(HexicalMain.id("media_jar"), VertexFormats.POSITION_TEXTURE_COLOR_NORMAL) { shader ->
 				mediaJarRenderLayer = RenderLayer.of(
-					"media_jar_shader",
-					VertexFormats.POSITION_TEXTURE_COLOR_NORMAL,
-					VertexFormat.DrawMode.QUADS,
-					512,
+					"media_jar_shader", VertexFormats.POSITION_TEXTURE_COLOR_NORMAL, VertexFormat.DrawMode.QUADS, 512,
 					RenderLayer.MultiPhaseParameters.builder()
 						.program(RenderPhase.ShaderProgram { shader })
-						.texture(RenderPhase.Textures.create()
-							.add(PERLIN_NOISE, false, false)
-							.build())
+						.texture(RenderPhase.Textures.create().add(PERLIN_NOISE, false, false).build())
 						.transparency(RenderPhase.NO_TRANSPARENCY)
 						.cull(RenderPhase.ENABLE_CULLING)
 						.lightmap(RenderPhase.DISABLE_LIGHTMAP)
@@ -34,5 +31,8 @@ object MediaJarShader : InitHook() {
 				)
 			}
 		}
+
+		BlockRenderLayerMap.INSTANCE.putBlock(PERIWINKLE_FLOWER, RenderLayer.getCutout())
+		BlockRenderLayerMap.INSTANCE.putBlock(MAGE_BLOCK, mediaJarRenderLayer)
 	}
 }
