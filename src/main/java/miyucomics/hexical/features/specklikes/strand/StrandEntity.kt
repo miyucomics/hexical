3a47031d77d50a92649f4e113b9b6d9e5a32fd7e
miyucomics <nbriannyc@gmail.com>
2025-11-18 14:06:45 -0500

    Strands should work now

--- src/main/java/miyucomics/hexical/features/specklikes/strand/StrandEntity.kt
+++ src/main/java/miyucomics/hexical/features/specklikes/strand/StrandEntity.kt
@@ -1,15 +1,17 @@
 package miyucomics.hexical.features.specklikes.strand
 
-import at.petrak.hexcasting.api.utils.putCompound
-import miyucomics.hexical.features.specklikes.BaseSpecklike
+import at.petrak.hexcasting.api.casting.iota.Vec3Iota
+import at.petrak.hexcasting.api.utils.asFloat
+import at.petrak.hexcasting.api.utils.putList
 import miyucomics.hexical.features.specklikes.FigureSpecklike
 import miyucomics.hexical.inits.HexicalEntities
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.data.DataTracker
-import net.minecraft.entity.data.TrackedData
-import net.minecraft.entity.data.TrackedDataHandlerRegistry
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtFloat
+import net.minecraft.nbt.NbtList
 import net.minecraft.util.math.Vec2f
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 
 class StrandEntity(entityType: EntityType<out StrandEntity>, world: World) : FigureSpecklike(entityType, world) {
@@ -17,22 +19,29 @@ class StrandEntity(entityType: EntityType<out StrandEntity>, world: World) : Fig
 
 	var clientVertices: MutableList<Vec2f> = mutableListOf()
 
-	override fun readCustomDataFromNbt(nbt: NbtCompound) {
-		super.readCustomDataFromNbt(nbt)
-		dataTracker.set(shapeDataTracker, nbt.getCompound("shape"))
+	fun getShape(): List<Vec3Iota> {
+		val list = dataTracker.get(shapeDataTracker).getList("shape", NbtElement.FLOAT_TYPE.toInt()).iterator()
+		val deserializedVertices = mutableListOf<Vec3Iota>()
+		while (list.hasNext())
+			deserializedVertices.add(Vec3Iota(Vec3d(list.next().asFloat.toDouble(), list.next().asFloat.toDouble(), 0.0)))
+		return deserializedVertices
 	}
 
-	override fun writeCustomDataToNbt(nbt: NbtCompound) {
-		super.writeCustomDataToNbt(nbt)
-		nbt.putCompound("shape", dataTracker.get(shapeDataTracker))
+	fun setShape(shape: List<Vec2f>) {
+		val compound = NbtCompound()
+		val list = NbtList()
+		for (vertex in shape) {
+			list.add(NbtFloat.of(vertex.x))
+			list.add(NbtFloat.of(vertex.y))
+		}
+		compound.putList("shape", list)
+		this.dataTracker.set(shapeDataTracker, compound)
 	}
 
-	override fun initDataTracker() {
-		super.initDataTracker()
-		dataTracker.startTracking(shapeDataTracker, NbtCompound())
-	}
-
-	companion object {
-		private val shapeDataTracker: TrackedData<NbtCompound> = DataTracker.registerData(BaseSpecklike::class.java, TrackedDataHandlerRegistry.NBT_COMPOUND)
+	override fun processShape(shape: NbtCompound) {
+		this.clientVertices.clear()
+		val list = shape.getList("shape", NbtElement.FLOAT_TYPE.toInt()).iterator()
+		while (list.hasNext())
+			clientVertices.add(Vec2f(list.next().asFloat, list.next().asFloat))
 	}
 }
