1fc1ced11f1c64a3e8e82b1beccc84ff6d0b42a2
miyucomics <nbriannyc@gmail.com>
2025-11-21 16:07:22 -0500

    Many small changes

--- src/main/java/miyucomics/hexical/features/specklikes/strand/OpDrawPattern.kt
+++ src/main/java/miyucomics/hexical/features/specklikes/strand/OpDrawPattern.kt
@@ -1,4 +1,4 @@
-package miyucomics.hexical.features.pattern_manipulation
+package miyucomics.hexical.features.specklikes.strand
 
 import at.petrak.hexcasting.api.casting.asActionResult
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
@@ -11,9 +11,5 @@ import net.minecraft.util.math.Vec3d
 
 object OpDrawPattern : ConstMediaAction {
 	override val argc = 1
-	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-		val output = mutableListOf<Vec3Iota>()
-		PatternUtils.getNormalizedStrokes(args.getPattern(0, argc)).forEach { vec -> output.add(Vec3Iota(Vec3d(vec.x.toDouble(), vec.y.toDouble(), 0.0))) }
-		return output.toList().asActionResult
-	}
+	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> = PatternUtils.getNormalizedStrokes(args.getPattern(0, argc)).map { Vec3Iota(Vec3d(it.x.toDouble(), it.y.toDouble(), 0.0)) }.asActionResult
 }
