ad9a4d96af47c843a867ca997a0f6fad13cce4a3
miyucomics <nbriannyc@gmail.com>
2026-01-07 19:40:50 -0500

    Remove zap particle for now

--- src/client/java/miyucomics/hexical/inits/HexicalParticlesClient.kt
+++ src/client/java/miyucomics/hexical/inits/HexicalParticlesClient.kt
@@ -2,13 +2,11 @@ package miyucomics.hexical.inits
 
 import miyucomics.hexical.features.confetti.ConfettiParticle
 import miyucomics.hexical.features.sparkle.SparkleParticle
-import miyucomics.hexical.features.zap.ZapParticle
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry
 
 object HexicalParticlesClient {
 	fun clientInit() {
 		ParticleFactoryRegistry.getInstance().register(HexicalParticles.CONFETTI_PARTICLE, ConfettiParticle::Factory)
 		ParticleFactoryRegistry.getInstance().register(HexicalParticles.SPARKLE_PARTICLE, SparkleParticle::Factory)
-		ParticleFactoryRegistry.getInstance().register(HexicalParticles.ZAP_PARTICLE, ZapParticle::Factory)
 	}
 }
--- src/main/java/miyucomics/hexical/inits/HexicalParticles.kt
+++ src/main/java/miyucomics/hexical/inits/HexicalParticles.kt
@@ -2,7 +2,6 @@ package miyucomics.hexical.inits
 
 import miyucomics.hexical.HexicalMain
 import miyucomics.hexical.features.sparkle.SparkleParticleEffect
-import miyucomics.hexical.features.zap.ZapParticleEffect
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes
 import net.minecraft.particle.DefaultParticleType
 import net.minecraft.particle.ParticleType
@@ -12,11 +11,9 @@ import net.minecraft.registry.Registry
 object HexicalParticles {
 	val CONFETTI_PARTICLE: DefaultParticleType = FabricParticleTypes.simple(true)
 	val SPARKLE_PARTICLE: ParticleType<SparkleParticleEffect> = FabricParticleTypes.complex(SparkleParticleEffect.Factory)
-	val ZAP_PARTICLE: ParticleType<ZapParticleEffect> = FabricParticleTypes.complex(ZapParticleEffect.Factory)
 
 	fun init() {
 		Registry.register(Registries.PARTICLE_TYPE, HexicalMain.id("confetti"), CONFETTI_PARTICLE)
 		Registry.register(Registries.PARTICLE_TYPE, HexicalMain.id("sparkle"), SPARKLE_PARTICLE)
-		Registry.register(Registries.PARTICLE_TYPE, HexicalMain.id("zap"), ZAP_PARTICLE)
 	}
 }
