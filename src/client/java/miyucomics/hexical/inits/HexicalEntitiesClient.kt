c1f92b0f6ddd88725562d3144505a97cd7cd7945
miyucomics <nbriannyc@gmail.com>
2026-01-07 19:44:09 -0500

    Removed shield code

--- src/client/java/miyucomics/hexical/inits/HexicalEntitiesClient.kt
+++ src/client/java/miyucomics/hexical/inits/HexicalEntitiesClient.kt
@@ -3,14 +3,12 @@ package miyucomics.hexical.inits
 import miyucomics.hexical.features.animated_scrolls.AnimatedScrollRenderer
 import miyucomics.hexical.features.magic_missile.MagicMissileRenderer
 import miyucomics.hexical.features.specklikes.MeshRenderer
-import miyucomics.hexical.features.specklikes.ShieldRenderer
 import miyucomics.hexical.features.specklikes.SpeckRenderer
 import miyucomics.hexical.features.specklikes.StrandRenderer
 import miyucomics.hexical.features.spike.SpikeRenderer
 import miyucomics.hexical.inits.HexicalEntities.ANIMATED_SCROLL_ENTITY
 import miyucomics.hexical.inits.HexicalEntities.MAGIC_MISSILE_ENTITY
 import miyucomics.hexical.inits.HexicalEntities.MESH_ENTITY
-import miyucomics.hexical.inits.HexicalEntities.SHIELD_ENTITY
 import miyucomics.hexical.inits.HexicalEntities.SPECK_ENTITY
 import miyucomics.hexical.inits.HexicalEntities.SPIKE_ENTITY
 import miyucomics.hexical.inits.HexicalEntities.STRAND_ENTITY
@@ -20,7 +18,6 @@ object HexicalEntitiesClient {
 	fun clientInit() {
 		EntityRendererRegistry.register(ANIMATED_SCROLL_ENTITY, ::AnimatedScrollRenderer)
 		EntityRendererRegistry.register(MAGIC_MISSILE_ENTITY, ::MagicMissileRenderer)
-		EntityRendererRegistry.register(SHIELD_ENTITY, ::ShieldRenderer)
 		EntityRendererRegistry.register(SPIKE_ENTITY, ::SpikeRenderer)
 		EntityRendererRegistry.register(SPECK_ENTITY, ::SpeckRenderer)
 		EntityRendererRegistry.register(STRAND_ENTITY, ::StrandRenderer)
