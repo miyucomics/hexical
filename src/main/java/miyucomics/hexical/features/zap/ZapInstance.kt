d4db72f0caab704aea21565a6b26150135329438
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-01-15 13:42:22 -0500

    Serialization of zaps, codecs are awesome

--- src/main/java/miyucomics/hexical/features/zap/ZapInstance.kt
+++ src/main/java/miyucomics/hexical/features/zap/ZapInstance.kt
@@ -1,6 +1,15 @@
 package miyucomics.hexical.features.zap
 
-import net.minecraft.server.world.ServerWorld
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.util.math.BlockPos
 
-data class ZapInstance(val world: ServerWorld, val position: BlockPos, val power: Int, val time: Int)
+data class ZapInstance(val position: BlockPos, val power: Int, val time: Int) {
+	companion object {
+		val CODEC: Codec<ZapInstance> = RecordCodecBuilder.create { it.group(
+			BlockPos.CODEC.fieldOf("position").forGetter(ZapInstance::position),
+			Codec.INT.fieldOf("power").forGetter(ZapInstance::power),
+			Codec.INT.fieldOf("time").forGetter(ZapInstance::time)
+		).apply(it, ::ZapInstance) }
+	}
+}
--- src/main/java/miyucomics/hexical/features/zap/ZapManager.kt
+++ src/main/java/miyucomics/hexical/features/zap/ZapManager.kt
@@ -2,35 +2,16 @@ package miyucomics.hexical.features.zap
 
 import miyucomics.hexical.misc.InitHook
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
-import net.minecraft.block.Blocks
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 
 object ZapManager : InitHook() {
-	private val instances: MutableMap<ServerWorld, HashMap<BlockPos, ZapInstance>> = HashMap()
-
-	fun triggerRedstone(world: ServerWorld, position: BlockPos, power: Int, ticks: Int) {
-		val specific = instances.getOrPut(world, ::HashMap)
-		specific[position] = ZapInstance(world, position, power, ticks)
-		triggerBlockUpdate(world, position)
-	}
-
-	@JvmStatic fun hasMagicalPower(world: ServerWorld, position: BlockPos) = instances.getOrPut(world, ::HashMap).containsKey(position)
-	@JvmStatic fun getMagicalPower(world: ServerWorld, position: BlockPos) = instances.getOrPut(world, ::HashMap)[position]!!.power
+	fun getPersistentState(world: ServerWorld): ZapState = world.persistentStateManager.getOrCreate(ZapState::createFromNbt, ::ZapState, "zaps")
+	fun zap(world: ServerWorld, position: BlockPos, power: Int, ticks: Int) = getPersistentState(world).zap(world, position, power, ticks)
+	@JvmStatic fun hasMagicalPower(world: ServerWorld, position: BlockPos) = getPersistentState(world).hasMagicalPower(position)
+	@JvmStatic fun getMagicalPower(world: ServerWorld, position: BlockPos) = getPersistentState(world).getMagicalPower(position)
 
 	override fun init() {
-		ServerTickEvents.END_WORLD_TICK.register(::tick)
-	}
-
-	private fun tick(world: ServerWorld) {
-		val specific = instances.getOrPut(world, ::HashMap)
-		specific.replaceAll { _, instance -> instance.copy(time = instance.time - 1) }
-		val toRemove = specific.filter { (_, instance) -> instance.time <= 0 }.map { (position, _) -> position }
-		toRemove.forEach(specific::remove)
-		toRemove.forEach { triggerBlockUpdate(world, it) }
-	}
-
-	private fun triggerBlockUpdate(world: ServerWorld, pos: BlockPos) {
-		world.getBlockState(pos).neighborUpdate(world, pos, Blocks.AIR, pos, false)
+		ServerTickEvents.END_WORLD_TICK.register { getPersistentState(it).tick(it) }
 	}
 }
