820e009dd19abd7d55ca4a1625432a73fc154319
miyucomics <nbriannyc@gmail.com>
2026-01-19 09:48:28 -0500

    Cleaning up

--- src/main/java/miyucomics/hexical/mixin/PatternIotaMixin.java
+++ src/main/java/miyucomics/hexical/mixin/PatternIotaMixin.java
@@ -4,20 +4,16 @@ import at.petrak.hexcasting.api.casting.eval.CastResult;
 import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.PatternIota;
-import at.petrak.hexcasting.api.casting.math.HexPattern;
 import miyucomics.hexical.features.driver_dots.DriverDotItem;
 import miyucomics.hexical.features.scarabs.ScarabBeetleItem;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = PatternIota.class, priority = 100, remap = false)
 public abstract class PatternIotaMixin {
-	@Shadow public abstract HexPattern getPattern();
-
 	@Inject(method = "execute", at = @At("HEAD"), cancellable = true)
 	void expandRuntimeMacros(CastingVM vm, ServerWorld world, SpellContinuation continuation, CallbackInfoReturnable<CastResult> cir) {
 		PatternIota pattern = (PatternIota) (Object) this;
