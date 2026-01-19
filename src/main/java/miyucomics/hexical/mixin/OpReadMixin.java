3e3f265b0a165ecb3d8639170dbb11061c58bbef
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-01-19 09:51:49 -0500

    Fixed compass curios not allowing for reading

--- src/main/java/miyucomics/hexical/mixin/OpReadMixin.java
+++ src/main/java/miyucomics/hexical/mixin/OpReadMixin.java
@@ -1,15 +1,18 @@
 package miyucomics.hexical.mixin;
 
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
+import at.petrak.hexcasting.api.casting.iota.GarbageIota;
 import at.petrak.hexcasting.api.casting.iota.Iota;
-import at.petrak.hexcasting.api.casting.iota.NullIota;
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota;
+import at.petrak.hexcasting.api.casting.mishaps.MishapBadOffhandItem;
 import at.petrak.hexcasting.common.casting.actions.rw.OpRead;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.sugar.Local;
 import miyucomics.hexical.inits.HexicalItems;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,25 +23,32 @@ import java.util.List;
 
 @Mixin(value = OpRead.class, remap = false)
 public class OpReadMixin {
+	@WrapMethod(method = "execute")
+	private List<Iota> readFromCompassCurio(List<Iota> args, CastingEnvironment env, Operation<List<Iota>> original) throws MishapBadOffhandItem {
+		try {
+			return original.call(args, env);
+		} catch (Throwable exception) { // Java does not know how to handle Kotlin not needing to specify that it throws something, so we just need to be general and just use Throwable
+			CastingEnvironment.HeldItemInfo data = env.getHeldItemToOperateOn(
+				item -> item.isOf(HexicalItems.CURIO_COMPASS)
+					&& item.hasNbt()
+					&& item.getNbt().contains("needle", NbtElement.INT_ARRAY_TYPE)
+					&& item.getNbt().getIntArray("needle").length >= 3
+			);
+			if (data == null)
+				 throw MishapBadOffhandItem.of(null, "iota.read");
+
+			LivingEntity caster = env.getCastingEntity();
+			if (caster == null)
+				return List.of(new GarbageIota());
+
+			int[] coordinates = data.stack().getNbt().getIntArray("needle");
+			return List.of(new Vec3Iota(new Vec3d(coordinates[0], coordinates[1], coordinates[2]).subtract(caster.getEyePos()).normalize()));
+		}
+	}
+
 	@Inject(method = "execute", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/addldata/ADIotaHolder;readIota(Lnet/minecraft/server/world/ServerWorld;)Lat/petrak/hexcasting/api/casting/iota/Iota;"))
 	private static void dontEatTwoHexbursts(List<? extends Iota> args, CastingEnvironment env, CallbackInfoReturnable<List<Iota>> cir, @Local ItemStack handStack) {
 		if (handStack.isOf(HexicalItems.HEXBURST_ITEM))
 			handStack.increment(1);
-	}
-
-	@Inject(method = "execute", at = @At("HEAD"), cancellable = true)
-	private void readCompass(List<? extends Iota> args, CastingEnvironment env, CallbackInfoReturnable<List<Iota>> cir) {
-		CastingEnvironment.HeldItemInfo data = env.getHeldItemToOperateOn(item -> item.isOf(HexicalItems.CURIO_COMPASS) && item.hasNbt() && item.getNbt().contains("needle"));
-		if (data == null)
-			return;
-
-		NbtCompound nbt = data.stack().getNbt();
-		LivingEntity caster = env.getCastingEntity();
-		if (caster == null || nbt == null)
-			cir.setReturnValue(List.of(new NullIota()));
-
-		assert nbt != null;
-		int[] rawCoordinates = nbt.getIntArray("needle");
-		cir.setReturnValue(List.of(new Vec3Iota(new Vec3d(rawCoordinates[0], rawCoordinates[1], rawCoordinates[2]).subtract(caster.getEyePos()).normalize())));
 	}
 }
