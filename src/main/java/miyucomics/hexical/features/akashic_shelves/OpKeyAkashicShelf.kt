fc81a4be1df53fc93ced25ff81e83f4c1ffcee1c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-02-03 09:29:31 -0400

    Refactored shelves to be in a more descriptive package

--- src/main/java/miyucomics/hexical/features/akashic_shelves/OpKeyAkashicShelf.kt
+++ src/main/java/miyucomics/hexical/features/akashic_shelves/OpKeyAkashicShelf.kt
@@ -1,25 +1,24 @@
-package miyucomics.hexical.features.akashic
+package miyucomics.hexical.features.akashic_shelves
 
+import at.petrak.hexcasting.api.casting.asActionResult
 import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getBlockPos
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.IotaType
 import at.petrak.hexcasting.api.casting.iota.NullIota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadBlock
 import at.petrak.hexcasting.common.blocks.akashic.BlockEntityAkashicBookshelf
 import at.petrak.hexcasting.common.lib.HexBlocks
 
-object OpReadAkashicShelf : ConstMediaAction {
+object OpKeyAkashicShelf : ConstMediaAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
 		val position = args.getBlockPos(0, argc)
 		env.assertPosInRange(position)
-
 		val block = env.world.getBlockState(position)
 		if (!block.isOf(HexBlocks.AKASHIC_BOOKSHELF))
 			throw MishapBadBlock.of(position, "akashic_bookshelf")
-		val nbt = (env.world.getBlockEntity(position) as BlockEntityAkashicBookshelf).iotaTag ?: return listOf(NullIota())
-		return listOf(IotaType.deserialize(nbt, env.world))
+		val pattern = (env.world.getBlockEntity(position) as BlockEntityAkashicBookshelf).pattern ?: return listOf(NullIota())
+		return pattern.asActionResult
 	}
 }
