e58444d7951cb440056301914bbf57211d2d20b6
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-02-14 09:54:40 -0400

    Removed unnecessary passing around

--- src/main/java/miyucomics/hexical/features/specklikes/strand/OpDrawPattern.kt
+++ src/main/java/miyucomics/hexical/features/specklikes/strand/OpDrawPattern.kt
@@ -6,10 +6,21 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getPattern
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota
-import miyucomics.hexical.misc.PatternUtils
+import net.minecraft.util.math.Vec2f
 import net.minecraft.util.math.Vec3d
+import kotlin.math.max
 
 object OpDrawPattern : ConstMediaAction {
 	override val argc = 1
-	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> = PatternUtils.getNormalizedStrokes(args.getPattern(0, argc)).map { Vec3Iota(Vec3d(it.x.toDouble(), it.y.toDouble(), 0.0)) }.asActionResult
+	override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
+		val pattern = args.getPattern(0, argc)
+		val lines = pattern.toLines(1f, pattern.getCenter(1f).negate()).toMutableList()
+		val scaling = max(
+			lines.maxBy { vector -> vector.x }.x - lines.minBy { vector -> vector.x }.x,
+			lines.maxBy { vector -> vector.y }.y - lines.minBy { vector -> vector.y }.y
+		)
+		for (i in lines.indices)
+			lines[i] = Vec2f(lines[i].x, -lines[i].y).multiply(1 / scaling)
+		return lines.map { Vec3Iota(Vec3d(it.x.toDouble(), it.y.toDouble(), 0.0)) }.asActionResult
+	}
 }
