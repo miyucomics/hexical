b4f44cb1e56e457337ab24840cd37da30b9a457c
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-02-20 16:51:42 -0400

    Abandoned recipe systems for dyeing spell, just good old fashioned custom data format now

---
+++ src/main/java/miyucomics/hexical/datagen/generators/HexicalDyeingGenerator.kt
@@ -0,0 +1,13 @@
+package miyucomics.hexical.datagen.generators
+
+import miyucomics.hexical.datagen.providers.dyeing.DyeingProvider
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.minecraft.data.DataProvider
+import net.minecraft.data.DataWriter
+import java.util.concurrent.CompletableFuture
+
+class HexicalDyeingGenerator(val output: FabricDataOutput) : DataProvider {
+	override fun getName() = "Hexical Dyeing"
+	override fun run(writer: DataWriter): CompletableFuture<*> =
+		CompletableFuture.allOf(*DyeingProvider.tasks.map { it(writer, output) }.toTypedArray())
+}
