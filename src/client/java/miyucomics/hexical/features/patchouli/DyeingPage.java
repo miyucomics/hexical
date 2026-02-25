fc7094bf0bd8e89d13540361fed9b59f856746c5
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-02-25 14:09:07 -0400

    Decently presentable dye page now

--- src/client/java/miyucomics/hexical/features/patchouli/DyeingPage.java
+++ src/client/java/miyucomics/hexical/features/patchouli/DyeingPage.java
@@ -1,9 +1,12 @@
 package miyucomics.hexical.features.patchouli;
 
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import org.joml.Vector2i;
 import vazkii.patchouli.api.IVariable;
 import vazkii.patchouli.client.book.BookContentsBuilder;
 import vazkii.patchouli.client.book.BookEntry;
@@ -18,41 +21,51 @@ import java.util.stream.Stream;
 @SuppressWarnings("unused")
 public class DyeingPage extends PageWithText {
 	IVariable uncolored, white, orange, magenta, light_blue, yellow, lime, pink, gray, light_gray, cyan, purple, blue, brown, green, red, black;
-	private final transient List<ItemStack> validOptions = new ArrayList<>();
+	private transient List<ItemStack> validOptions;
+	private final transient List<Pair<ItemStack, Vector2i>> renders = new ArrayList<>();
+	private transient int spiralMinY = 0;
+	private transient int spiralMaxY = 0;
+
+	private static final double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
+	private static final double GOLDEN_ANGLE = MathHelper.TAU / (GOLDEN_RATIO * GOLDEN_RATIO);
+	private static final double SCALE = 12.0;
 
 	@Override
 	public void build(World level, BookEntry entry, BookContentsBuilder builder, int pageNum) {
 		super.build(level, entry, builder, pageNum);
-		validOptions.addAll(
-			Stream.of(uncolored, white, orange, magenta, light_blue, yellow, lime, pink, gray, light_gray, cyan, purple, blue, brown, green, red, black)
-				.filter(Objects::nonNull)
-				.map(var -> var.as(ItemStack.class))
-				.filter(stack -> !stack.isEmpty())
-				.toList()
-		);
+		List<ItemStack> validOptions = Stream.of(uncolored, white, orange, magenta, light_blue, yellow, lime, pink, gray, light_gray, cyan, purple, blue, brown, green, red, black)
+			.filter(Objects::nonNull)
+			.map(var -> var.as(ItemStack.class))
+			.toList();
+
+		int index = 0;
+		spiralMinY = 0;
+		spiralMaxY = 0;
+		int numberOfOptions = validOptions.size();
+		float radius = Math.max(10, numberOfOptions * 18 / MathHelper.TAU);
+
+		renders.clear();
+		for (ItemStack stack : validOptions) {
+			double angle = (double) index * MathHelper.TAU / numberOfOptions;
+			int x = (int) (Math.cos(angle) * radius);
+			int y = (int) (Math.sin(angle) * radius);
+			renders.add(new Pair<>(stack, new Vector2i(x - 8, y - 8)));
+			spiralMinY = Math.min(spiralMinY, y);
+			spiralMaxY = Math.max(spiralMaxY, y);
+			index++;
+		}
 	}
 
 	@Override
 	public void render(DrawContext graphics, int mouseX, int mouseY, float pticks) {
-		int index = 0;
-		int numberOfOptions = validOptions.size();
-		final double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
-
-		final double SCALE = 12.0;
-		for (ItemStack stack : validOptions) {
-			double goldenAngle = index * MathHelper.TAU / (GOLDEN_RATIO * GOLDEN_RATIO);
-			double radius = SCALE * Math.sqrt(index + 1);
-			int x = GuiBook.PAGE_WIDTH / 2 + (int) (Math.cos(goldenAngle) * radius) - 8;
-			int y = 45 + (int) (Math.sin(goldenAngle) * radius);
-			graphics.drawItem(stack, x, y);
-			index++;
-		}
-
+		MatrixStack matrices = graphics.getMatrices();
+		for (Pair<ItemStack, Vector2i> pair : this.renders)
+			parent.renderItemStack(graphics, pair.getRight().x + GuiBook.PAGE_WIDTH / 2, pair.getRight().y - spiralMinY + 5, mouseX, mouseY, pair.getLeft());
 		super.render(graphics, mouseX, mouseY, pticks);
 	}
 
 	@Override
 	public int getTextHeight() {
-		return 0;
+		return spiralMaxY - spiralMinY + 20;
 	}
 }
