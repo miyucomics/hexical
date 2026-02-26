17704a3d1592ada03318a5a9713bd730adf49439
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-02-26 16:11:12 -0400

    Removed extraneous variables

--- src/client/java/miyucomics/hexical/features/patchouli/DyeingPage.java
+++ src/client/java/miyucomics/hexical/features/patchouli/DyeingPage.java
@@ -1,7 +1,6 @@
 package miyucomics.hexical.features.patchouli;
 
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.MathHelper;
@@ -18,17 +17,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-@SuppressWarnings("unused")
 public class DyeingPage extends PageWithText {
 	IVariable uncolored, white, orange, magenta, light_blue, yellow, lime, pink, gray, light_gray, cyan, purple, blue, brown, green, red, black;
-	private transient List<ItemStack> validOptions;
 	private final transient List<Pair<ItemStack, Vector2i>> renders = new ArrayList<>();
-	private transient int spiralMinY = 0;
-	private transient int spiralMaxY = 0;
-
-	private static final double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
-	private static final double GOLDEN_ANGLE = MathHelper.TAU / (GOLDEN_RATIO * GOLDEN_RATIO);
-	private static final double SCALE = 12.0;
+	private transient int minY = 0;
+	private transient int maxY = 0;
 
 	@Override
 	public void build(World level, BookEntry entry, BookContentsBuilder builder, int pageNum) {
@@ -39,8 +32,8 @@ public class DyeingPage extends PageWithText {
 			.toList();
 
 		int index = 0;
-		spiralMinY = 0;
-		spiralMaxY = 0;
+		minY = 0;
+		maxY = 0;
 		int numberOfOptions = validOptions.size();
 		float radius = Math.max(10, numberOfOptions * 18 / MathHelper.TAU);
 
@@ -50,22 +43,21 @@ public class DyeingPage extends PageWithText {
 			int x = (int) (Math.cos(angle) * radius);
 			int y = (int) (Math.sin(angle) * radius);
 			renders.add(new Pair<>(stack, new Vector2i(x - 8, y - 8)));
-			spiralMinY = Math.min(spiralMinY, y);
-			spiralMaxY = Math.max(spiralMaxY, y);
+			minY = Math.min(minY, y);
+			maxY = Math.max(maxY, y);
 			index++;
 		}
 	}
 
 	@Override
 	public void render(DrawContext graphics, int mouseX, int mouseY, float pticks) {
-		MatrixStack matrices = graphics.getMatrices();
 		for (Pair<ItemStack, Vector2i> pair : this.renders)
-			parent.renderItemStack(graphics, pair.getRight().x + GuiBook.PAGE_WIDTH / 2, pair.getRight().y - spiralMinY + 5, mouseX, mouseY, pair.getLeft());
+			parent.renderItemStack(graphics, pair.getRight().x + GuiBook.PAGE_WIDTH / 2, pair.getRight().y - minY + 5, mouseX, mouseY, pair.getLeft());
 		super.render(graphics, mouseX, mouseY, pticks);
 	}
 
 	@Override
 	public int getTextHeight() {
-		return spiralMaxY - spiralMinY + 20;
+		return maxY - minY + 20;
 	}
 }
