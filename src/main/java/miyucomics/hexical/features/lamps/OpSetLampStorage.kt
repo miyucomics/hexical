8667463aa00da5114af87532c9cf92efea94e3a7
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-03-17 16:58:43 -0400

    No storing player truenames forever in an arch lamp

--- src/main/java/miyucomics/hexical/features/lamps/OpSetLampStorage.kt
+++ src/main/java/miyucomics/hexical/features/lamps/OpSetLampStorage.kt
@@ -4,12 +4,16 @@ import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.Iota
+import miyucomics.hexical.inits.HexicalItems
+import miyucomics.hexical.misc.CastingUtils
 
 object OpSetLampStorage : SpellAction {
 	override val argc = 1
 	override fun execute(args: List<Iota>, env: CastingEnvironment): SpellAction.Result {
 		if (env !is LampCastEnv)
 			throw NeedsLampMishap()
+		if (env.stack.isOf(HexicalItems.ARCH_LAMP_ITEM))
+			CastingUtils.assertNoTruename(args[0], env)
 		return SpellAction.Result(Spell(args[0]), 0, listOf())
 	}
 
