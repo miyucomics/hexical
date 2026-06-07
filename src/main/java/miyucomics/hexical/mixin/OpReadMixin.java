0cabc3154edc528f9e9a89bd6c92a310b0d6b880
Teal Wolf 25 <teal@tw25.net>
2026-06-07 06:44:50 -0400

    Fix mixin fail

--- src/main/java/miyucomics/hexical/mixin/OpReadMixin.java
+++ src/main/java/miyucomics/hexical/mixin/OpReadMixin.java
@@ -8,16 +8,11 @@ import at.petrak.hexcasting.api.casting.mishaps.MishapBadOffhandItem;
 import at.petrak.hexcasting.common.casting.actions.rw.OpRead;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.sugar.Local;
 import miyucomics.hexical.inits.HexicalItems;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
@@ -44,11 +39,5 @@ public class OpReadMixin {
 			@SuppressWarnings("DataFlowIssue") int[] coordinates = data.stack().getNbt().getIntArray("needle");
 			return List.of(new Vec3Iota(new Vec3d(coordinates[0], coordinates[1], coordinates[2]).subtract(caster.getEyePos()).normalize()));
 		}
-	}
-
-	@Inject(method = "execute", at = @At(value = "INVOKE", target = "Lat/petrak/hexcasting/api/addldata/ADIotaHolder;readIota(Lnet/minecraft/server/world/ServerWorld;)Lat/petrak/hexcasting/api/casting/iota/Iota;"))
-	private static void dontEatTwoHexbursts(List<? extends Iota> args, CastingEnvironment env, CallbackInfoReturnable<List<Iota>> cir, @Local(name = "handStack") ItemStack handStack) {
-		if (handStack.isOf(HexicalItems.HEXBURST_ITEM))
-			handStack.increment(1);
 	}
 }
